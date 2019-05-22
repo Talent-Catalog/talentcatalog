21d052574c8fddd0468de5bb1b1a8ec80e4e8a24
Dan Zwolenski <dan@codecamp.com.au>
2019-05-22 16:23:06 +1000

    Added the ability to create new Candidates via the admin portal.

--- server/src/main/resources/db/migration/V1__init.sql
+++ server/src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,7 @@
 create table candidate
 (
   id                      bigserial not null primary key,
-  canidate_number         text,
+  candidate_number        text,
   first_name              text,
   last_name               text
 );
