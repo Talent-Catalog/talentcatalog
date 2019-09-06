474f14e36971e97dd4f5346c4f559292ecf75f5d
Dean El-Mouslimani <deanmouslimani@hotmail.com>
2019-09-06 17:14:27 +1000

    Fleshing out registration flow - EOD commit

--- ui/candidate-portal/src/app/components/register/work-experience/registration-work-experience.component.ts
+++ ui/candidate-portal/src/app/components/register/work-experience/registration-work-experience.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {countries} from "../../../model/countries";
 
 @Component({
   selector: 'app-registration-work-experience',
@@ -7,9 +10,34 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RegistrationWorkExperienceComponent implements OnInit {
 
-  constructor() { }
+
+  form: FormGroup;
+  countries: string[];
+
+  constructor(private fb: FormBuilder,
+              private router: Router) { }
 
   ngOnInit() {
+    this.countries = countries;
+    this.form = this.fb.group({
+      companyName: ['', Validators.required],
+      country: ['', Validators.required],
+      role: ['', Validators.required],
+      startDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      isFullTime: [false, Validators.required],
+      isPaid: [false, Validators.required],
+      roleDescription: ['', Validators.required]
+    })
+  }
+
+  addMore() {
+    // TODO
+  }
+
+  save() {
+    // TODO save
+    this.router.navigate(['register', 'education']);
   }
 
 }
