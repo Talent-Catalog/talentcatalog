984a564129ec9e1a5cc3136bae941d1caab7cf64
Caroline Cameron <carolinecameron_34@hotmail.com>
2019-09-10 11:53:21 +1000

    Worked on the adding new work experience, we as filling out the empty pages.

--- ui/candidate-portal/src/app/components/register/school/registration-school.component.ts
+++ ui/candidate-portal/src/app/components/register/school/registration-school.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-registration-school',
