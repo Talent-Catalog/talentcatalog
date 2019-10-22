44c13992cea3a78c71e7f8a1db9acb59a889fd17
jthatcher <England76^>
2019-10-22 16:15:04 +1100

    Deployment as bundle

--- bundle/all/src/main/java/org/tbbtalent/TbbTalentApplication.java
+++ bundle/all/src/main/java/org/tbbtalent/TbbTalentApplication.java
@@ -1,11 +1,7 @@
 package org.tbbtalent;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.RedirectViewControllerRegistration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -17,26 +13,7 @@ public class TbbTalentApplication extends WebMvcConfigurerAdapter {
         SpringApplication.run(TbbTalentApplication.class, args);
     }
 
-//    @Bean
-//    @ConditionalOnProperty(name="flyway.repair", havingValue="true")
-//    public FlywayMigrationStrategy fixFlyway() {
-//        return new FlywayMigrationStrategy() {
-//            @Override
-//            public void migrate(Flyway flyway) {
-//                try {
-//                    System.out.println("************* Starting flyway repair ***********************");
-//                    flyway.repair();
-//                    System.out.println("************* Finished flyway repair ***********************");
-//                    flyway.migrate();
-//                } catch (Exception e) {
-//                    System.out.println("ERROR: unable to repair flyway");
-//                    e.printStackTrace();
-//                }
-//            }
-//        };
-//    }
-
-    @Override
+   @Override
     public void addViewControllers (ViewControllerRegistry registry) {
         RedirectViewControllerRegistration r =
                 registry.addRedirectViewController("/", "/candidate");
