edcf85b3c8c1ab8cdc52f9ac549beaeff1306bea
John Cameron <john@cameronfoundation.org>
2020-02-13 15:50:24 +1100

    Simplified, more data driven, tabs on Home page

--- ui/admin-portal/src/app/components/home/home.component.ts
+++ ui/admin-portal/src/app/components/home/home.component.ts
@@ -3,6 +3,11 @@ import {NgbTabChangeEvent} from "@ng-bootstrap/ng-bootstrap";
 import {SavedSearchType} from "../../model/saved-search";
 import {LocalStorageService} from "angular-2-local-storage";
 
+interface TabInfo {
+  savedSearchType?: SavedSearchType;
+  title: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -13,19 +18,20 @@ export class HomeComponent implements OnInit {
   private activeId: string;
   private lastTabKey: string = 'HomeLastTab';
 
-  savedSearchType: SavedSearchType;
-
-  savedSearchTypeTabs: SavedSearchType[] = [
-    SavedSearchType.profession,
-    SavedSearchType.job,
-    SavedSearchType.other
+  tabInfos: TabInfo[] = [
+    {savedSearchType: SavedSearchType.profession,
+      title: 'Professions'},
+    {savedSearchType: SavedSearchType.job,
+      title: 'Jobs'},
+    {savedSearchType: SavedSearchType.other,
+      title: 'Other'},
+    {title: 'All'},
   ];
 
   constructor(
     private localStorageService: LocalStorageService
   ) { }
 
-  //todo process url for any hint of what tab shoudl be selected.
   ngOnInit() {
     this.selectDefaultTab();
   }
@@ -35,10 +41,7 @@ export class HomeComponent implements OnInit {
   }
 
   onTabChanged(event: NgbTabChangeEvent) {
-    this.savedSearchType = SavedSearchType[event.nextId];
     this.activeId = event.nextId;
-    //todo Save savedSearchType to local storage and default to that tab if
-    //no other tab specifically requested (eg on URL)
-    this.localStorageService.set(this.lastTabKey, this.savedSearchType);
+    this.localStorageService.set(this.lastTabKey, this.activeId);
   }
 }
