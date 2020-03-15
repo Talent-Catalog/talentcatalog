56a7499172817e082937fccac1806f0646369c17
john <jadcpub@gmail.com>
2020-03-15 11:17:33 +0000

    Make new fixed and reviewable fields not null and defaulting to false. Set all existing fields to true.

--- server/src/main/resources/db/migration/V1_33__add_saved_search_fixed_and_reviewable.sql
+++ server/src/main/resources/db/migration/V1_33__add_saved_search_fixed_and_reviewable.sql
@@ -1,2 +1,5 @@
-alter table saved_search add column fixed boolean;
-alter table saved_search add column reviewable boolean;
+alter table saved_search add column fixed boolean default false not null;
+alter table saved_search add column reviewable boolean default false not null;
+
+update saved_search set fixed = true where saved_search.fixed = false;
+update saved_search set reviewable = true where saved_search.reviewable = false;
