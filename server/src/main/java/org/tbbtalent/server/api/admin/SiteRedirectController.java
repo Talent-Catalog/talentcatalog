96bc5ed9cbbb5f324f2da4f3aa93ac1241fcd1da
john <jadcpub@gmail.com>
2020-03-15 17:39:36 +0000

    Old url redirects to a unknown candidate should now given an error message.

--- server/src/main/java/org/tbbtalent/server/api/admin/SiteRedirectController.java
+++ server/src/main/java/org/tbbtalent/server/api/admin/SiteRedirectController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.tbbtalent.server.model.Candidate;
 import org.tbbtalent.server.service.CandidateService;
 
 @Controller()
@@ -25,12 +24,6 @@ public class SiteRedirectController {
 
     @RequestMapping(value = "/view-resume")
     public String redirectOldResumeUrl(@RequestParam("id") String candidateNumber) {
-        Candidate candidate = candidateService.findByCandidateNumber(candidateNumber);
-        if (candidate != null){
-            return "redirect:" + adminUrl + "/candidates/" + candidateNumber;
-        } else {
-            return "redirect:" + adminUrl + "/candidates";
-        }
-
+      return "redirect:" + adminUrl + "/candidates/" + candidateNumber;
     }
 }
