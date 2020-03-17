527194cf6098123e2ffb01bb9ac45e9b8a0e4a8a
john <jadcpub@gmail.com>
2020-03-17 13:14:25 +0000

    Merge duplicate nationality translations.

---
+++ server/src/main/resources/db/migration/V1_35__merge_duplicate_nationality_translations.sql
@@ -0,0 +1,24 @@
+-- Syrian
+update candidate set nationality_id = 9396 where nationality_id = 6863;
+delete from nationality where id = 6863;
+delete from translation where object_type = 'nationality' and object_id = 6863;
+
+-- Iraqi
+update candidate set nationality_id = 9317 where nationality_id = 265;
+delete from nationality where id = 265;
+delete from translation where object_type = 'nationality' and object_id = 265;
+
+-- Palestinian
+update candidate set nationality_id = 9362 where nationality_id = 7342;
+delete from nationality where id = 7342;
+delete from translation where object_type = 'nationality' and object_id = 7342;
+
+-- Somali
+update candidate set nationality_id = 9386 where nationality_id = 267;
+delete from nationality where id = 267;
+delete from translation where object_type = 'nationality' and object_id = 267;
+
+-- Sudanese
+update candidate set nationality_id = 9391 where nationality_id = 353;
+delete from nationality where id = 353;
+delete from translation where object_type = 'nationality' and object_id = 353;
