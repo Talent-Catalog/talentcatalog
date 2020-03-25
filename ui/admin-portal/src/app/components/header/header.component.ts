a3b304dc1eec95f901cd1ab52b779dfb5f3f318b
John Cameron <john@cameronfoundation.org>
2020-03-25 15:31:36 +1100

    Log user

--- ui/admin-portal/src/app/components/header/header.component.ts
+++ ui/admin-portal/src/app/components/header/header.component.ts
@@ -90,6 +90,7 @@ export class HeaderComponent implements OnInit {
         tap(() => this.searching = false)
       );
     this.loggedInUser = this.authService.getLoggedInUser();
+    console.log(this.loggedInUser);
   }
 
   renderCandidateRow(candidate: Candidate) {
