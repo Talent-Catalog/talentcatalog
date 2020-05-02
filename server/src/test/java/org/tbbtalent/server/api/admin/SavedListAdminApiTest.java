4f0019a80fe856beccf98e52054f19763172e747
John Cameron <john@cameronfoundation.org>
2020-05-02 17:30:40 +1000

    Fix unit testing config. Need to use Jupiter (5.4.0) classes.

--- server/src/test/java/org/tbbtalent/server/api/admin/SavedListAdminApiTest.java
+++ server/src/test/java/org/tbbtalent/server/api/admin/SavedListAdminApiTest.java
@@ -4,7 +4,7 @@
 
 package org.tbbtalent.server.api.admin;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
