767962a3d92181e81d1bc06cc6e369dd2269aa5a
John Cameron <john@cameronfoundation.org>
2020-05-02 22:09:53 +1000

    A couple of working unit tests!

--- server/src/test/java/org/tbbtalent/server/api/admin/SavedListAdminApiTest.java
+++ server/src/test/java/org/tbbtalent/server/api/admin/SavedListAdminApiTest.java
@@ -4,41 +4,56 @@
 
 package org.tbbtalent.server.api.admin;
 
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.servlet.MockMvc;
+import org.tbbtalent.server.model.User;
+import org.tbbtalent.server.repository.SavedListRepository;
+import org.tbbtalent.server.repository.UserRepository;
+import org.tbbtalent.server.request.list.UpdateSavedListRequest;
+import org.tbbtalent.server.security.UserContext;
+import org.tbbtalent.server.service.SavedListService;
+import org.tbbtalent.server.service.impl.SavedListServiceImpl;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
-/**
- * TODO JC Doc
- *
- * @author John Cameron
- */
 @SpringBootTest
-@AutoConfigureMockMvc
 class SavedListAdminApiTest {
+    
     @Autowired
-    private MockMvc mockMvc;
+    SavedListRepository savedListRepository;
+    
+    @Autowired
+    UserRepository userRepository;
+    
+    private SavedListAdminApi savedListAdminApi;
+
+    @BeforeEach
+    void initUseCase() {
+        User testUser = userRepository.findByUsernameIgnoreCase("camerojo"); 
+        
+        UserContext userContext = Mockito.mock(UserContext.class);
+        when(userContext.getLoggedInUser()).thenReturn(testUser);
+        
+        SavedListService savedListService = new SavedListServiceImpl(savedListRepository, userContext);
+        savedListAdminApi = new SavedListAdminApi(savedListService);
+    }
 
     @Test
-    public void shouldReturnCreatedList() throws Exception {
-        
-        this.mockMvc.perform(
-                post("/api/admin/saved-list")
-                        .param("name","TestList")
-                        .param("fixed", "false")
-        )
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.name").value("TestList"))
-                .andExpect(jsonPath("$.fixed").value("false"))
-                .andExpect(jsonPath("$.id").isNumber());
+    void createNewSavedList() {
+        UpdateSavedListRequest request = new UpdateSavedListRequest();
+        request.setName("TestList");
+        request.setFixed(false);
+
+        Map<String, Object> result = savedListAdminApi.create(request);
+        assertNotNull(result);
+        Object name = result.get("name");
+        assertEquals("TestList", name);
     }
-    
 }
---
+++ server/src/test/java/org/tbbtalent/server/api/admin/SavedListAdminApiWebLayerTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2020 Talent Beyond Boundaries. All rights reserved.
+ */
+
+package org.tbbtalent.server.api.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class SavedListAdminApiWebLayerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldBeRejectedWithAuthorizationError() throws Exception {
+        
+        this.mockMvc.perform(
+                post("/api/admin/saved-list")
+                        .param("name","TestList")
+                        .param("fixed", "false")
+        )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("TestList"))
+                .andExpect(jsonPath("$.fixed").value("false"))
+                .andExpect(jsonPath("$.id").isNumber());
+    }
+}
