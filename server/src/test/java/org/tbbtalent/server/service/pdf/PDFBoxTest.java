cafc1369e00f60a52a59881593004c2bbfe6d827
Caroline Cameron <caroline.iris.cameron@gmail.com>
2020-05-14 16:35:35 +1000

    Got PDF text extraction tests working

---
+++ server/src/test/java/org/tbbtalent/server/service/pdf/PDFBoxTest.java
@@ -0,0 +1,102 @@
+package org.tbbtalent.server.service.pdf;
+
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.EventType;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfCanvasProcessor;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.kernel.pdf.canvas.parser.data.IEventData;
+import com.itextpdf.kernel.pdf.canvas.parser.data.TextRenderInfo;
+import com.itextpdf.kernel.pdf.canvas.parser.filter.TextRegionEventFilter;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.FilteredEventListener;
+import com.itextpdf.kernel.pdf.canvas.parser.listener.LocationTextExtractionStrategy;
+
+import com.itextpdf.kernel.pdf.canvas.parser.listener.SimpleTextExtractionStrategy;
+import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.io.RandomAccessFile;
+import org.apache.pdfbox.pdfparser.PDFParser;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class PDFBoxTest {
+
+    @Test
+    void testPDFBoxMethods() throws IOException {
+        File file = new File("src/test/resources/CV.pdf");
+
+        assertTrue(file.exists());
+
+        //FIRST WAY USING PDFBOX
+
+        //LOAD FILE
+        String parsedText;
+        PDFParser parser = new PDFParser(new RandomAccessFile(file, "r"));
+        parser.parse();
+
+        //EXTRACT TEXT
+        COSDocument cosDoc = parser.getDocument();
+        PDFTextStripper pdfStripper = new PDFTextStripper();
+        pdfStripper.setSortByPosition(true);
+        PDDocument pdDoc = new PDDocument(cosDoc);
+        parsedText = pdfStripper.getText(pdDoc);
+
+        assertNotEquals("", parsedText);
+
+        PrintWriter pw = new PrintWriter("src/test/pdf.txt");
+        pw.print(parsedText);
+        pw.close();
+
+        assertNotNull(pw);
+
+        //SECOND WAY USING PDFBOX
+
+        PDFTextStripper tStripper = new PDFTextStripper();
+        tStripper.setSortByPosition(true);
+        PDDocument document = PDDocument.load(new File("src/test/resources/CV.pdf"));
+        String pdfFileInText = "";
+        if (!document.isEncrypted()) {
+            pdfFileInText = tStripper.getText(document);
+        }
+        System.out.println(pdfFileInText.trim());
+
+        assertNotEquals("", pdfFileInText);
+
+    }
+
+    @Test
+    void testITextMethods() throws IOException {
+        String src = "src/test/resources/CV.pdf";
+        PdfDocument pdfDoc = new PdfDocument(new PdfReader(src));
+
+        assertNotNull(pdfDoc);
+
+        String str;
+        StringBuffer txt = new StringBuffer();
+
+        for (int i=1; i<= pdfDoc.getNumberOfPages(); i++){
+            str = PdfTextExtractor.getTextFromPage(pdfDoc.getPage(i), new LocationTextExtractionStrategy());
+            txt.append(str);
+        }
+
+        assertNotEquals("", txt);
+
+        try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("src/test/pdf2.txt")))) {
+            writer.write(String.valueOf(txt));
+        }
+
+        pdfDoc.close();
+    }
+}
+
