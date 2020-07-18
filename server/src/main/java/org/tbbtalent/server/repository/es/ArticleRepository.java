56ab0ccb11b724422f9426e85dce72b26a1a5c5d
John Cameron <john@cameronfoundation.org>
2020-07-19 09:50:25 +1000

    Testing elasticsearch

--- server/src/main/java/org/tbbtalent/server/repository/es/ArticleRepository.java
+++ server/src/main/java/org/tbbtalent/server/repository/es/ArticleRepository.java
@@ -7,7 +7,6 @@ package org.tbbtalent.server.repository.es;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.tbbtalent.server.model.es.Article;
 
 /**
@@ -15,10 +14,15 @@ import org.tbbtalent.server.model.es.Article;
  *
  * @author John Cameron
  */
-public interface ArticleRepository extends ElasticsearchRepository<Article, String> {
+public interface ArticleRepository 
+        extends ElasticsearchRepository<Article, String> 
+{
+
+    Iterable<Article> findByAuthorsName(String name);
 
     Page<Article> findByAuthorsName(String name, Pageable pageable);
 
-    @Query("{\"bool\": {\"must\": [{\"match\": {\"authors.name\": \"?0\"}}]}}")
-    Page<Article> findByAuthorsNameUsingCustomQuery(String name, Pageable pageable);
+    //todo For some reason this won't run - stops tart up.
+//    @Query("{\"bool\": {\"must\": [{\"match\": {\"authors.name\": \"?0\"}}]}}")
+//    Page<Article> findByAuthorsNameUsingCustomQuery(String name, Pageable pageable);
 }
--- server/src/test/java/org/tbbtalent/server/repository/es/ArticleRepositoryTest.java
+++ server/src/test/java/org/tbbtalent/server/repository/es/ArticleRepositoryTest.java
@@ -4,29 +4,91 @@
 
 package org.tbbtalent.server.repository.es;
 
-import java.util.Arrays;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.tbbtalent.server.model.es.Article;
-import org.tbbtalent.server.model.es.Author;
-
 /**
  * TODO JC Doc
  *
  * @author John Cameron
  */
-@SpringBootTest
+//@SpringBootTest
 public class ArticleRepositoryTest {
 
-    @Autowired
-    private ArticleRepository articleRepository;
+//    @Autowired
+//    private ElasticsearchRestTemplate elasticsearchTemplate;
+//
+//    @Autowired
+//    private ArticleRepository articleRepository;
+//
+//    private final Author johnSmith = new Author("John Smith");
+//    private final Author johnDoe = new Author("John Doe");
+//    
+//    @BeforeEach
+//    void before() {
+//        Article article = new Article("Spring Data Elasticsearch");
+//        article.setAuthors(asList(johnSmith, johnDoe));
+//        article.setTags("elasticsearch", "spring data");
+//        articleRepository.save(article);
+//
+//        article = new Article("Search engines");
+//        article.setAuthors(asList(johnDoe));
+//        article.setTags("search engines", "tutorial");
+//        articleRepository.save(article);
+//
+//        article = new Article("Second Article About Elasticsearch");
+//        article.setAuthors(asList(johnSmith));
+//        article.setTags("elasticsearch", "spring data");
+//        articleRepository.save(article);
+//
+//        article = new Article("Elasticsearch Tutorial");
+//        article.setAuthors(asList(johnDoe));
+//        article.setTags("elasticsearch");
+//        articleRepository.save(article);
+//    }
+//
+//    @AfterEach
+//    public void after() {
+//        articleRepository.deleteAll();
+//    }
+//
+//    @Test
+//    public void givenPersistedArticles_whenSearchByAuthorsName_thenRightFound() {
+//        final Page<Article> articleByAuthorName = articleRepository
+//                .findByAuthorsName(johnSmith.getName(), 
+//                        PageRequest.of(0, 10));
+//        assertEquals(2L, articleByAuthorName.getTotalElements());
+//    }
+//
+//
+//    @Test
+//    public void givenPersistedArticles_whenUseRegexQuery_thenRightArticlesFound() {
+//        final Query searchQuery = new NativeSearchQueryBuilder()
+//                .withFilter(regexpQuery("title", ".*data.*"))
+//                .build();
+//
+//        final SearchHits<Article> articles = elasticsearchTemplate
+//                .search(searchQuery, Article.class, IndexCoordinates.of("blog"));
+//
+//        assertEquals(1, articles.getTotalHits());
+//    }
+//
+//    @Test
+//    public void givenSavedDoc_whenTitleUpdated_thenCouldFindByUpdatedTitle() {
+//        final Query searchQuery = new NativeSearchQueryBuilder()
+//                .withQuery(fuzzyQuery("title", "serch"))
+//                .build();
+//        final SearchHits<Article> articles = elasticsearchTemplate
+//                .search(searchQuery, Article.class, IndexCoordinates.of("blog"));
+//
+//        assertEquals(1, articles.getTotalHits());
+//
+//        final Article article = articles.getSearchHit(0)
+//                .getContent();
+//        final String newTitle = "Getting started with Search Engines";
+//        article.setTitle(newTitle);
+//        articleRepository.save(article);
+//
+//        assertEquals(newTitle, articleRepository.findById(article.getId())
+//                .get()
+//                .getTitle());
+//    }
     
-    @Test
-    void testCreate() {
-        Article article = new Article("Spring Data Elasticsearch");
-        article.setAuthors(Arrays.asList(new Author("John Smith"), new Author("John Doe")));
-        articleRepository.save(article);
-    }
 }
