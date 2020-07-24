96dc299a388241fa68b4577752a8766385c77aba
Caroline Cameron <caroline.iris.cameron@gmail.com>
2020-07-24 14:39:56 +1000

    Changed name of showAll to seeMore

--- ui/admin-portal/src/app/components/util/candidate-source/candidate-source.component.ts
+++ ui/admin-portal/src/app/components/util/candidate-source/candidate-source.component.ts
@@ -26,8 +26,15 @@ import {copyToClipboard} from "../../../util/clipboard";
 })
 export class CandidateSourceComponent implements OnInit, OnChanges {
 
+  // The data processing is dependant on these variables
+  // The candidate source passed in
   @Input() candidateSource: CandidateSource;
-  @Input() showAll: boolean;
+  // If the extra data can be loaded for the dropdowns (get request)
+  @Input() canLoad: boolean = true;
+  // Handles the toggle state of the candidate source (show less/show more)
+  @Input() seeMore: boolean;
+
+  // The font awesome icon buttons are dependant on these variables
   @Input() showLink: boolean = true;
   @Input() showMore: boolean = true;
   @Input() showOpen: boolean = true;
@@ -37,8 +44,7 @@ export class CandidateSourceComponent implements OnInit, OnChanges {
   @Input() showEdit: boolean = false;
   @Input() showDelete: boolean = false;
   @Input() showAudit: boolean = false;
-  // Load the extra data for the dropdowns (multiselect names)
-  @Input() loadAll: boolean = true;
+
   @Output() openSource = new EventEmitter<CandidateSource>();
   @Output() selectSource = new EventEmitter<CandidateSource>();
   @Output() copySource = new EventEmitter<CandidateSource>();
@@ -64,14 +70,15 @@ export class CandidateSourceComponent implements OnInit, OnChanges {
 
   ngOnChanges (changes: SimpleChanges){
     // WHEN candidateSource changes IF showAll fetch the savedSearch object which has the multi select Names to display (not just Ids).
-    if (this.showAll && changes && changes.candidateSource && changes.candidateSource.previousValue !== changes.candidateSource.currentValue){
+    if (this.seeMore && changes && changes.candidateSource && changes.candidateSource.previousValue !== changes.candidateSource.currentValue){
       this.getSavedSearch(this.candidateSource.id);
     }
   }
 
-  toggleShowAll() {
-    this.showAll = !this.showAll;
-    if (this.showAll) {
+  toggleShowMore() {
+    this.seeMore = !this.seeMore;
+    // Get extra data from saved search if needed (canLoad:true)
+    if (this.canLoad) {
       this.loading = true;
       this.getSavedSearch(this.candidateSource.id);
     }
