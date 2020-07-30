32649042788abf2f2f01e7e0ab8e915eac4ca3a0
John Cameron <john@cameronfoundation.org>
2020-07-30 15:24:19 +1000

    Restore environment.prod.ts files - they are used during production builds Added comments explaining their use.

---
+++ ui/admin-portal/src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+// This file replaces environment.ts during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: true,
+  apiUrl: '/api/admin',
+  systemApiUrl: '/api/system',
+  s3BucketUrl: 'https://s3.us-east-1.amazonaws.com/files.tbbtalent.org'
+};
