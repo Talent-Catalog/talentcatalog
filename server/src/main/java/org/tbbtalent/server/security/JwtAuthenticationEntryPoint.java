b65fdca08f6df8457e3314a1417a27716fbc03f5
John Cameron <john@cameronfoundation.org>
2020-07-31 10:53:15 +1000

    Display the url which caused unauthorized errors.

--- server/src/main/java/org/tbbtalent/server/security/JwtAuthenticationEntryPoint.java
+++ server/src/main/java/org/tbbtalent/server/security/JwtAuthenticationEntryPoint.java
@@ -1,15 +1,16 @@
 package org.tbbtalent.server.security;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 @Component
@@ -20,7 +21,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest httpServletRequest,
                          HttpServletResponse httpServletResponse,
                          AuthenticationException e) throws IOException, ServletException {
-        log.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        log.error("Responding with unauthorized error. Message - {} - caused by {}", 
+                e.getMessage(), httpServletRequest.getRequestURI());
         httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
     }
 }
