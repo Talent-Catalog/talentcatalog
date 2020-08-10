b919286c5913761ea6c652eb80dccae6ad5cc0af
John Cameron <john@cameronfoundation.org>
2020-08-10 15:50:26 +1000

    Add recaptcha check to candidate Reset Password

--- server/src/main/java/org/tbbtalent/server/request/user/SendResetPasswordEmailRequest.java
+++ server/src/main/java/org/tbbtalent/server/request/user/SendResetPasswordEmailRequest.java
@@ -1,14 +1,13 @@
 package org.tbbtalent.server.request.user;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class SendResetPasswordEmailRequest {
-
     private String email;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    private String reCaptchaV3Token;
 }
--- ui/candidate-portal/src/app/components/account/reset-password/reset-password.component.ts
+++ ui/candidate-portal/src/app/components/account/reset-password/reset-password.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {UserService} from '../../../services/user.service';
+import {ReCaptchaV3Service} from "ng-recaptcha";
+import {SendResetPasswordEmailRequest} from "../../../model/candidate";
 
 @Component({
   selector: 'app-reset-password',
@@ -15,6 +17,7 @@ export class ResetPasswordComponent implements OnInit {
   updated: boolean;
 
   constructor(private fb: FormBuilder,
+              private reCaptchaV3Service: ReCaptchaV3Service,
               private userService: UserService) {
   }
 
@@ -27,18 +30,8 @@ export class ResetPasswordComponent implements OnInit {
     });
   }
 
-  sendResetEmail() {
-    this.updated = false;
-    this.error = null;
-    this.userService.sendResetPassword(this.form.value).subscribe(
-      () => {
-        this.resetForm();
-        this.updated = true;
-      },
-      (error) => {
-        this.error = error;
-      }
-    );
+  get email(): string {
+    return this.form.value.email;
   }
 
   resetForm() {
@@ -51,4 +44,33 @@ export class ResetPasswordComponent implements OnInit {
     }
   }
 
+  sendResetEmail() {
+    this.updated = false;
+    this.error = null;
+
+    const action = 'resetPassword';
+    this.reCaptchaV3Service.execute(action).subscribe(
+      (token) => this.sendResetWithToken(token),
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private sendResetWithToken(token: string) {
+    const req: SendResetPasswordEmailRequest = new SendResetPasswordEmailRequest();
+    req.email = this.email;
+    req.reCaptchaV3Token = token;
+
+    this.userService.sendResetPassword(req).subscribe(
+      () => {
+        this.resetForm();
+        this.updated = true;
+      },
+      (error) => {
+        this.error = error;
+      }
+    );
+
+  }
 }
