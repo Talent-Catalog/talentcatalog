04a7e5aa3aa1ec5520949ae460c2a9ac353964f5
Caroline Cameron <caroline.iris.cameron@gmail.com>
2020-08-26 17:17:52 +1000

    Added candidate icon back after revert

--- ui/admin-portal/src/app/components/candidates/view/view-candidate.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/view-candidate.component.ts
@@ -4,23 +4,23 @@ import {Candidate} from '../../../model/candidate';
 import {ActivatedRoute, Router} from '@angular/router';
 import {NgbModal, NgbTabChangeEvent} from '@ng-bootstrap/ng-bootstrap';
 import {DeleteCandidateComponent} from './delete/delete-candidate.component';
-import {EditCandidateStatusComponent} from "./status/edit-candidate-status.component";
-import {Title} from "@angular/platform-browser";
-import {AuthService} from "../../../services/auth.service";
-import {User} from "../../../model/user";
-import {IDropdownSettings} from "ng-multiselect-dropdown";
-import {ListItem} from "ng-multiselect-dropdown/multiselect.model";
-import {CreateListComponent} from "../../list/create/create-list.component";
-import {
-  IHasSetOfCandidates,
-  SavedList,
-  SearchSavedListRequest
-} from "../../../model/saved-list";
-import {SavedListService} from "../../../services/saved-list.service";
-import {CandidateSavedListService} from "../../../services/candidate-saved-list.service";
-import {SavedListCandidateService} from "../../../services/saved-list-candidate.service";
-import {forkJoin} from "rxjs";
-import {LocalStorageService} from "angular-2-local-storage";
+import {EditCandidateStatusComponent} from './status/edit-candidate-status.component';
+import {Title} from '@angular/platform-browser';
+import {AuthService} from '../../../services/auth.service';
+import {User} from '../../../model/user';
+import {IDropdownSettings} from 'ng-multiselect-dropdown';
+import {ListItem} from 'ng-multiselect-dropdown/multiselect.model';
+import {CreateListComponent} from '../../list/create/create-list.component';
+import {IHasSetOfCandidates, SavedList, SearchSavedListRequest} from '../../../model/saved-list';
+import {SavedListService} from '../../../services/saved-list.service';
+import {CandidateSavedListService} from '../../../services/candidate-saved-list.service';
+import {SavedListCandidateService} from '../../../services/saved-list-candidate.service';
+import {forkJoin} from 'rxjs';
+import {CandidateAttachmentService} from '../../../services/candidate-attachment.service';
+import {AttachmentType, CandidateAttachment} from '../../../model/candidate-attachment';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {environment} from '../../../../environments/environment';
+import {LocalStorageService} from 'angular-2-local-storage';
 
 @Component({
   selector: 'app-view-candidate',
@@ -42,6 +42,11 @@ export class ViewCandidateComponent implements OnInit {
 
   selectedLists: SavedList[] = [];
   lists: SavedList[] = [];
+  attachmentForm: FormGroup;
+  attachments: CandidateAttachment[];
+  cvs: CandidateAttachment[];
+  s3BucketUrl = environment.s3BucketUrl;
+
   /* MULTI SELECT */
   dropdownSettings: IDropdownSettings = {
     idField: 'id',
@@ -56,11 +61,13 @@ export class ViewCandidateComponent implements OnInit {
               private candidateSavedListService: CandidateSavedListService,
               private localStorageService: LocalStorageService,
               private savedListCandidateService: SavedListCandidateService,
+              private candidateAttachmentService: CandidateAttachmentService,
               private route: ActivatedRoute,
               private router: Router,
               private modalService: NgbModal,
               private titleService: Title,
-              private authService: AuthService) { }
+              private authService: AuthService,
+              private fb: FormBuilder) { }
 
   ngOnInit() {
     this.refreshCandidateInfo();
@@ -86,6 +93,7 @@ export class ViewCandidateComponent implements OnInit {
         } else {
           this.setCandidate(candidate);
           this.loadLists();
+          this.getAttachments();
         }
       }, error => {
         this.loadingError = true;
@@ -118,6 +126,47 @@ export class ViewCandidateComponent implements OnInit {
         this.loading = false;
       }
     );
+  }
+
+  getAttachments() {
+    this.attachments = [];
+
+    this.attachmentForm = this.fb.group({
+      candidateId: [this.candidate.id],
+      pageSize: 10,
+      pageNumber: 0,
+      sortDirection: 'DESC',
+      sortFields: [['createdDate']]
+    });
+
+    this.loading = true;
+    this.candidateAttachmentService.search(this.attachmentForm.value).subscribe(
+      results => {
+        this.attachments = results.content;
+        this.cvs = results.content.filter(attachment => attachment.cv === true)
+        this.loading = false;
+      },
+      error => {
+        this.error = error;
+        this.loading = false;
+      })
+    ;
+
+  }
+
+  getAttachmentUrl(att: CandidateAttachment) {
+    if (att.type === AttachmentType.file) {
+      return this.s3BucketUrl + '/candidate/' + (att.migrated ? 'migrated' : this.candidate.candidateNumber) + '/' + att.location;
+    }
+    return att.location;
+  }
+
+  openCVs() {
+    for (let i = 0; i < this.cvs.length; i++) {
+      const newTab = window.open();
+      const url = this.getAttachmentUrl(this.cvs[i]);
+      newTab.location.href = url;
+    }
   }
 
   deleteCandidate() {
