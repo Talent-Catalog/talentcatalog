e87390e828dcf6db89dbaba971fbbe27d508d8ad
John Cameron <john@cameronfoundation.org>
2020-09-05 08:59:15 +1000

    Improved SF url extraction

--- server/src/test/java/org/tbbtalent/server/TestExtractSFIDFromUrl.java
+++ server/src/test/java/org/tbbtalent/server/TestExtractSFIDFromUrl.java
@@ -7,6 +7,7 @@ package org.tbbtalent.server;
 import org.junit.jupiter.api.Test;
 import org.tbbtalent.server.service.db.impl.SalesforceServiceImpl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
@@ -21,8 +22,12 @@ public class TestExtractSFIDFromUrl {
         String url = "https://talentbeyondboundaries.lightning.force.com/lightning/r/Opportunity/0061N00000gO3zXQAS/view";
         String id = SalesforceServiceImpl.extractIdFromSfUrl(url);
         assertNotNull(id);
+        String objectType = SalesforceServiceImpl.extractObjectTypeFromSfUrl(url);
+        assertEquals("Opportunity", objectType);
         url = "https://talentbeyondboundaries.lightning.force.com/lightning/r/Opportunity/0061N00000gO3zXQAS";
         id = SalesforceServiceImpl.extractIdFromSfUrl(url);
         assertNotNull(id);
+        objectType = SalesforceServiceImpl.extractObjectTypeFromSfUrl(url);
+        assertEquals("Opportunity", objectType);
     } 
 }
