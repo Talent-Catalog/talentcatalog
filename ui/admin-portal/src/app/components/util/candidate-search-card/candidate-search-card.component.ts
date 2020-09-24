e9ea224b039f03b9dac7c54737918bb970e55374
Caroline Cameron <caroline.iris.cameron@gmail.com>
2020-09-24 16:08:59 +1000

    Adding in autosave

--- ui/admin-portal/src/app/components/util/candidate-search-card/candidate-search-card.component.ts
+++ ui/admin-portal/src/app/components/util/candidate-search-card/candidate-search-card.component.ts
@@ -2,6 +2,7 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges
 import {Candidate} from '../../../model/candidate';
 import {User} from '../../../model/user';
 import {FormBuilder, FormGroup} from '@angular/forms';
+import {catchError, debounceTime, switchMap, takeUntil} from 'rxjs/operators';
 
 @Component({
   selector: 'app-candidate-search-card',
@@ -31,6 +32,49 @@ export class CandidateSearchCardComponent implements OnInit, OnChanges {
     if (changes && changes.candidate && changes.candidate.previousValue !== changes.candidate.currentValue) {
       // TODO switch to general tab?
     }
+  }
+
+  private autoSaveNote(timeout: number) {
+    this.form.valueChanges.pipe(
+
+      //Only pass values on if there has been inactivity for the given timeout
+      debounceTime(timeout),
+
+      //Do a save of the received form values.
+      switchMap(formValue => {
+          //Update the candidateIntakeData to keep it in sync with the server
+          //saved version.
+          //Object assign just copies the formValue fields across, leaving any
+          //other fields in candidateIntakeData unchanged.
+          //Object.assign(this.candidateIntakeData, formValue);
+          // this.error = null;
+          // this.saving = true;
+          console.log(formValue)
+          //return this.candidateService.updateIntakeData(this.candidate.id, formValue);
+        }
+      ),
+
+      //We catch errors, copying them to this.error, but then just continuing
+      catchError((error, caught) => {
+        // this.saving = false;
+        // this.error = error;
+        return caught;
+      }),
+
+      //Subscription will continue until the given Observable emits.
+      //See ngOnDestroy
+      takeUntil(this.unsubscribe)
+    ).subscribe(
+
+      //Save has completed successfully
+      () => this.saving = false,
+
+      //Theoretically never get here because we catch errors in the pipe
+      (error) => {
+        this.saving = false;
+        this.error = error;
+      }
+    )
   }
 
   close() {
