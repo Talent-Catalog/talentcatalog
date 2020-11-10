36a8dc2fbe3d024602506ea948ccae5320c13c79
John Cameron <john@cameronfoundation.org>
2020-11-10 15:11:13 +1100

    Added field for saving the columns displayed in candidate lists and search results.

---
+++ server/src/main/resources/db/migration/V1_84__add_displayed_fields.sql
@@ -0,0 +1,5 @@
+alter table saved_list add column displayed_fields_long text;
+alter table saved_list add column displayed_fields_short text;
+alter table saved_search add column displayed_fields_long text;
+alter table saved_search add column displayed_fields_short text;
+
