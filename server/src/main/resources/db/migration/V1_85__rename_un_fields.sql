916093775d0a5fb1d2b070731ba6293d970d382a
Caroline Cameron <caroline.iris.cameron@gmail.com>
2020-11-20 11:21:38 +1100

    New flyway for unhcr and unrwa rename and refactor

---
+++ server/src/main/resources/db/migration/V1_85__rename_un_fields.sql
@@ -0,0 +1,13 @@
+alter table candidate drop column unhcr_status;
+alter table candidate drop column unhcr_number;
+
+alter table candidate rename column un_registered to unhcr_status;
+alter table candidate rename column un_registration_number to unhcr_number;
+
+alter table candidate rename column unrwa_registered to unrwa_status;
+alter table candidate drop column unrwa_was_registered;
+
+alter table candidate alter column unhcr_status type text;
+
+update candidate set unhcr_status = 'RegisteredAsylum' where unhcr_status = 'true';
+update candidate set unhcr_status = 'NotRegistered' where unhcr_status = 'false';
