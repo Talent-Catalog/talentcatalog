0dac2c97604dbbe4cb8b8bc7490816a4633a6a51
John Cameron <john@cameronfoundation.org>
2020-12-21 16:12:14 +1100

    Doc only

--- ui/candidate-portal/src/app/components/register/register.component.ts
+++ ui/candidate-portal/src/app/components/register/register.component.ts
@@ -19,8 +19,12 @@ export class RegisterComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
 
-    //Need to delay changing language otherwise you get ExpressionChangedAfterItHasBeenCheckedError
+    //Note that we deliberately use a snapshot rather than a subscribe
+    //so that we pick up the lang query even if there is a redirect.
+    //eg like login redirects to /home if already authenticated.
+    //With a subscribe, the query can be lost in the redirect.
     const lang = this.route.snapshot.queryParams['lang'];
+    //Need to delay changing language otherwise you get ExpressionChangedAfterItHasBeenCheckedError
     setTimeout(
       () => this.languageService.changeLanguage(lang), 1000
     )
