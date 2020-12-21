335fb022af439091c96af74ebcb21bac65d818ac
John Cameron <john@cameronfoundation.org>
2020-12-21 17:49:40 +1100

    Doc only - explaining use of X-Language header to track a user's selected language.

--- server/src/main/java/org/tbbtalent/server/security/LanguageFilter.java
+++ server/src/main/java/org/tbbtalent/server/security/LanguageFilter.java
@@ -10,18 +10,28 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.tbbtalent.server.model.db.User;
 
-
+/**
+ * Sets an authenticated user's selected language based on the X-Language
+ * header in the HTTP request.
+ * <p/>
+ * This matches up with the language.interceptor.ts class on the 
+ * browser (Angular) side.
+ */
 public class LanguageFilter extends OncePerRequestFilter {
 
     private static final Logger logger = LoggerFactory.getLogger(LanguageFilter.class);
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
             String selectedLanguage = request.getHeader("X-Language");
             if (StringUtils.isBlank(selectedLanguage)) {
