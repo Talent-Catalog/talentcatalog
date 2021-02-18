b1bcb1d56815d5b4a904156a0ab00ea7fd2585ef
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-02-18 16:59:32 +1100

    Have file upload style and display dependant on device

--- ui/candidate-portal/src/app/components/common/file-upload/file-upload.component.ts
+++ ui/candidate-portal/src/app/components/common/file-upload/file-upload.component.ts
@@ -63,6 +63,8 @@ export class FileUploadComponent implements OnInit {
   }
 
   ngOnInit() {
+    const device = navigator.userAgent;
+    console.log(device)
   }
 
   handleImage(webcamImage: WebcamImage) {
