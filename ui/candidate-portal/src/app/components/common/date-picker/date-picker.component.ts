5dcf9fa95f0c73be36fbf7a8c7852955c70e327a
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-03-12 13:10:12 +1100

    Added min/max dates to candidate portal component and restricted for dob

--- ui/candidate-portal/src/app/components/common/date-picker/date-picker.component.ts
+++ ui/candidate-portal/src/app/components/common/date-picker/date-picker.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {FormControl} from "@angular/forms";
-import {LanguageService} from "../../../services/language.service";
+import {NgbDateStruct} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-date-picker',
@@ -9,12 +9,24 @@ import {LanguageService} from "../../../services/language.service";
 })
 export class DatePickerComponent implements OnInit {
   @Input() control: FormControl;
-  date;
+  // If don't want to allow selection of a future date, set to false.
+  @Input() allowFuture: boolean = true;
+  date: string;
+  today: Date;
+  maxDate: NgbDateStruct;
+  minDate: NgbDateStruct;
 
-  constructor(private languageService: LanguageService) { }
+  constructor() { }
 
   ngOnInit(): void {
     this.date = this.control.value;
+    this.minDate = {year: 1930, month: 1, day: 1}
+    this.today = new Date();
+    if (this.allowFuture) {
+      this.maxDate = null;
+    } else {
+      this.maxDate = {year: this.today.getFullYear(), month: this.today.getMonth() + 1, day: this.today.getDate()};
+    }
   }
 
   update() {
