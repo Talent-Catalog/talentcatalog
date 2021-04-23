4de6e337d0ad0521b7e2424db5f639f341180866
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-04-23 13:59:36 +1000

    Add notes for rural No

--- ui/admin-portal/src/app/components/candidates/intake/rural/rural.component.ts
+++ ui/admin-portal/src/app/components/candidates/intake/rural/rural.component.ts
@@ -41,11 +41,19 @@ export class RuralComponent extends IntakeComponentBase implements OnInit {
     });
   }
 
-  get ruralInterest(): boolean {
-    let interested: boolean;
-    if (this.form.value.intRecruitRural) {
-      interested = this.form.value.intRecruitRural === 'Yes' || this.form.value.intRecruitRural === 'Unsure';
+  // get ruralInterest(): boolean {
+  //   let interested: boolean;
+  //   if (this.form.value.intRecruitRural) {
+  //     interested = this.form.value.intRecruitRural === 'Yes' || this.form.value.intRecruitRural === 'Unsure';
+  //   }
+  //   return interested;
+  // }
+
+  get hasNotes(): boolean {
+    if (this.form.value?.intRecruitRural == null || this.form.value?.intRecruitRural === 'NoResponse') {
+      return false;
+    } else {
+      return true;
     }
-    return interested;
   }
 }
