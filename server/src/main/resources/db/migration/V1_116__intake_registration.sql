b791030dcbb06efea1d04ad62535737bc12732b4
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-05-05 13:53:33 +1000

    Added fields for redo of registration section

---
+++ server/src/main/resources/db/migration/V1_116__intake_registration.sql
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2021 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+alter table candidate drop column host_entry_year;
+
+alter table candidate_dependant add column registered_number text;
+alter table candidate_dependant add column registered_notes text;
+alter table candidate_dependant rename column notes to health_notes;
+
+update candidate set unhcr_old_status = null;
+alter table candidate rename column unhcr_old_status to unhcr_not_reg_status;
+
+update candidate set unhcr_status = null;
