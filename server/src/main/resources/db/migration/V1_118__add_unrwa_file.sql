07e811d52209699406ca3a31a792141ff119ca73
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-05-06 10:38:02 +1000

    Updated flyways to remove changing unhcr status change

--- server/src/main/resources/db/migration/V1_118__add_unrwa_file.sql
+++ server/src/main/resources/db/migration/V1_118__add_unrwa_file.sql
@@ -1,6 +1,3 @@
-update candidate set unhcr_status = null where unhcr_status = 'NotRegistered';
-update candidate set unhcr_status = null where unhcr_status = 'NA';
-
 alter table candidate drop column unhcr_not_reg_notes;
 
 alter table candidate add column unrwa_file integer;
