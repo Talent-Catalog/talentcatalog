20edd6fcd895c52e7254abcf2aa3bdbfb5788ac3
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-08-25 12:01:47 +1000

    Don't set status to ineligible if afghan in afghanistan

--- server/src/main/java/org/tbbtalent/server/service/db/impl/CandidateServiceImpl.java
+++ server/src/main/java/org/tbbtalent/server/service/db/impl/CandidateServiceImpl.java
@@ -2142,8 +2142,9 @@ public class CandidateServiceImpl implements CandidateService {
     private String checkStatusValidity(long countryReq, long nationalityReq, Candidate candidate) {
         String newStatus = "";
         // If candidate pending, but they updating country & nationality as same. Change to ineligible.
+        // EXCEPTION: UNLESS AFGHAN IN AFGHANISTAN
         if (candidate.getStatus() == CandidateStatus.pending) {
-            if (countryReq == nationalityReq) {
+            if (countryReq == nationalityReq && countryReq != 6180) {
                 newStatus = "ineligible";
             }
         // If candidate ineligible & it has country & nationality the same (causing the status) BUT the request
@@ -2151,6 +2152,10 @@ public class CandidateServiceImpl implements CandidateService {
         // that the cause of the ineligible status was due to country & nationality being the same, and not another reason.
         } else if (candidate.getStatus() == CandidateStatus.ineligible) {
             if (candidate.getCountry() == candidate.getNationality() && countryReq != nationalityReq) {
+                newStatus = "pending";
+            }
+            // EXCEPTION: IF AFGHAN IN AFGHANISTAN SET PENDING
+            if (countryReq == 6180 && nationalityReq == 6180) {
                 newStatus = "pending";
             }
         }
