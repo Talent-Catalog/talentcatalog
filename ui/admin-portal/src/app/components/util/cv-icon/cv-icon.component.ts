b0ed945f127b6cd38c92df5c6872553a9304303c
Caroline Cameron <caroline.iris.cameron@gmail.com>
2021-10-04 13:11:24 +1100

    Commiting to change branch

--- ui/admin-portal/src/app/components/util/cv-icon/cv-icon.component.ts
+++ ui/admin-portal/src/app/components/util/cv-icon/cv-icon.component.ts
@@ -15,10 +15,7 @@
  */
 
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {
-  CandidateAttachment,
-  SearchCandidateAttachmentsRequest
-} from '../../../model/candidate-attachment';
+import {CandidateAttachment, SearchCandidateAttachmentsRequest} from '../../../model/candidate-attachment';
 import {CandidateAttachmentService} from '../../../services/candidate-attachment.service';
 import {Candidate} from '../../../model/candidate';
 
@@ -72,9 +69,15 @@ export class CvIconComponent implements OnInit {
   openCVs() {
     this.loading = true;
     this.candidateAttachmentService.downloadAttachments(this.candidate, this.cvs).subscribe(
-      () => this.loading = false,
-      (err: string) => {this.loading = false; this.error = err}
-    );
+      () => {
+        this.loading = false;
+        console.log("hi! loading is false")
+      },
+      (error) => {
+        this.loading = false;
+        this.error = error;
+      });
+    console.log("done")
   }
 
 }
