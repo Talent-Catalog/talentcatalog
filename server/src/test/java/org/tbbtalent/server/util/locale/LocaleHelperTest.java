8c863bdeb2a68183353300b2b5e8dd6b2524eb19
john <jadcpub@gmail.com>
2021-11-13 16:47:25 +0000

    Added LocaleHelper which uses the standard Java Locale class to provide translations of Countries and Languages

---
+++ server/src/test/java/org/tbbtalent/server/util/locale/LocaleHelperTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2021 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tbbtalent.server.util.locale;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.tbbtalent.server.util.locale.LocaleHelper.CodeTranslation;
+
+class LocaleHelperTest {
+    LocaleHelper helper;
+
+    @BeforeEach
+    void setUp() {
+        helper = new LocaleHelper();
+    }
+
+    @AfterEach
+    void tearDown() {
+        helper = null;
+    }
+
+    @Test
+    void getCountryNameTranslations() {
+        List<CodeTranslation> cts = helper.getCountryNameTranslations("fa");
+        assertNotNull(cts);
+    }
+
+    @Test
+    void getLanguageNameTranslations() {
+        List<CodeTranslation> cts = helper.getLanguageNameTranslations("fa");
+        assertNotNull(cts);
+    }
+}
