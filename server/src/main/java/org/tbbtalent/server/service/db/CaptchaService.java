0dd180b6be4c15ed887b2231807da4c420d5742a
john <jadcpub@gmail.com>
2021-12-12 11:34:15 +0100

    Doc only

--- server/src/main/java/org/tbbtalent/server/service/db/CaptchaService.java
+++ server/src/main/java/org/tbbtalent/server/service/db/CaptchaService.java
@@ -5,12 +5,12 @@
  * the terms of the GNU Affero General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
- * You should have received a copy of the GNU Affero General Public License 
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
@@ -20,16 +20,23 @@ import org.tbbtalent.server.exception.ReCaptchaInvalidException;
 
 /**
  * Service for managing Google reCaptcha validation.
- * Used to prevent credential stuffing attacks. 
+ * Used to prevent credential stuffing attacks.
  * Automated data entry - eg for registration will be ignored.
  * <p>
  *     Based on https://www.baeldung.com/spring-security-registration-captcha
+ * </p>
+ * <p>
+ *     reCaptcha - see https://www.google.com/recaptcha/admin/
+ *     Talent Catalog is currently managed out of Google account john@cameronfoundation.org,
+ *     with jcameron@talentbeyondboundaries.org as a notifiable email.
+ *     The one reCaptcha can serve muultiple domains associated with the same site - eg
+ *     tbbtalent.org, unhcrtalent.org, iomtalent.org, localhost, 127.0.0.1 etc
  * </p>
  *
  * @author John Cameron
  */
 public interface CaptchaService {
-    void processCaptchaV3Token(final String token, String action) 
+    void processCaptchaV3Token(final String token, String action)
             throws ReCaptchaInvalidException;
 
     String getReCaptchaSite();
