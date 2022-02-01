e8f01c46c3deb931e1001eaf575b120b4a63a97d
John Cameron <john@cameronfoundation.org>
2022-02-01 12:50:47 +1100

    Order candidate attachments in descending order (and show time uploaded) candidate portal

--- server/src/main/java/org/tbbtalent/server/repository/db/CandidateAttachmentRepository.java
+++ server/src/main/java/org/tbbtalent/server/repository/db/CandidateAttachmentRepository.java
@@ -5,12 +5,12 @@
  * the terms of the GNU Affero General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
- * You should have received a copy of the GNU Affero General Public License 
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
@@ -31,7 +31,8 @@ public interface CandidateAttachmentRepository extends JpaRepository<CandidateAt
     @Query(" select distinct a from CandidateAttachment a "
             + " left join a.createdBy u "
             + " left join a.candidate c "
-            + " where c.id = :candidateId ")
+            + " where c.id = :candidateId "
+            + " order by a.createdDate desc")
     List<CandidateAttachment> findByCandidateIdLoadAudit(@Param("candidateId") Long candidateId);
 
     Page<CandidateAttachment> findByCandidateId(Long candidateId, Pageable request);
