fea3915267c9203873c89bb0f892b55b49dae18c
John Cameron <john@cameronfoundation.org>
2022-02-22 16:14:35 +1100

    Added ErrorInterceptor to turn exceptions into simple error messages.

--- ui/public-portal/src/app/app.module.ts
+++ ui/public-portal/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CvLandingComponent } from './components/cv-landing/cv-landing.component';
 import { CvDisplayComponent } from './components/cv-display/cv-display.component';
-import {HttpClientModule} from "@angular/common/http";
+import {ErrorInterceptor} from './services/error.interceptor';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -19,7 +19,10 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule,
 
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ui/public-portal/src/app/services/error.interceptor.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {Router} from '@angular/router';
+
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private router: Router ) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(catchError(err => {
+      console.log(err);
+      let error: string;
+      if (err.error !== null && err.error.message) {
+        error = err.error.message;
+      } else if (err.message !== null) {
+        error = err.message;
+      } else {
+        error = err.status + " " + err.statusText;
+      }
+      return throwError(error);
+    }));
+  }
+}
