c7535f4d8762a52daaed7a815b5e7f262fb79bff
John Cameron <john@cameronfoundation.org>
2022-03-01 12:55:53 +1100

    Added task_saved_list to store unidirectional many to many relationship supporting having tasks associated with saved lists.

---
+++ server/src/main/resources/db/migration/V1_173__add_task_saved_list.sql
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+-- Tasks associated with saved lists
+-- Note that this is a unidirectional many to many relationship, so we don't need an index
+-- on task_id (don't need to efficiently show all saved lists associated with a task). We just to
+-- be able to efficiently retrieve all tasks associated with a saved list - and the primary key
+-- can provide that index. See https://stackoverflow.com/questions/3048154/indexes-and-multi-column-primary-keys
+-- See also https://thorben-janssen.com/ultimate-guide-association-mappings-jpa-hibernate/#Unidirectional_Many-to-Many_Associations
+create table task_saved_list
+(
+    task_id bigint not null references task,
+    saved_list_id bigint not null references saved_list,
+    primary key (saved_list_id, task_id)
+);
