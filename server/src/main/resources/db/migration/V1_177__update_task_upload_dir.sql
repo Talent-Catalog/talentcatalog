f93bea24700889cbff14e0259acffb794500fc66
John Cameron <john@cameronfoundation.org>
2022-03-03 15:54:14 +1100

    Add other renames to Language and Medical

--- server/src/main/resources/db/migration/V1_177__update_task_upload_dir.sql
+++ server/src/main/resources/db/migration/V1_177__update_task_upload_dir.sql
@@ -15,3 +15,5 @@
  */
 
 update task set upload_subfolder_name = 'Engagement' where upload_subfolder_name = 'TBB Forms';
+update task set upload_subfolder_name = 'Language' where upload_subfolder_name = 'English';
+update task set upload_subfolder_name = 'Medical' where upload_subfolder_name = 'Medicals';
