13c10b2fd02d969f6bb88221dfcc61f9d7a57cea
John Cameron <john@cameronfoundation.org>
2022-03-07 15:23:12 +1100

    Rename related_task to related_task_id

--- server/src/main/resources/db/migration/V1_180__candidate_property.sql
+++ server/src/main/resources/db/migration/V1_180__candidate_property.sql
@@ -18,7 +18,7 @@ create table candidate_property (
   candidate_id      bigint not null references candidate,
   name              text not null,
   value             text,
-  related_task      bigint references task,
+  related_task_id   bigint references task,
 
 --   Name must be unique for a candidate. ie a Candidate cannot have two properties with the same name
   primary key (candidate_id, name)
