10bfad1d6cfce0dbd54c2c0450a78a0eb21436cb
John Cameron <john@cameronfoundation.org>
2022-03-11 15:45:01 +1100

    Change to just add new display name column - so that I can keep the non null for the name column.
    Initialize display name's to name.
    
    Make name column unique

--- server/src/main/resources/db/migration/V1_184__task_display_name.sql
+++ server/src/main/resources/db/migration/V1_184__task_display_name.sql
@@ -14,3 +14,7 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 alter table task add column display_name text;
+
+update task set display_name = name;
+
+create unique index task_name_uindex on task (name);
