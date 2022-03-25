02ba7806c13d4530a9151de0239b33cd50243e12
Caroline Cameron <caroline.iris.cameron@gmail.com>
2022-03-25 11:23:00 +1100

    Add Aus Physio Council form task

---
+++ server/src/main/resources/db/migration/V1_191__add_apc_form_task.sql
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2022 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+insert into task (name, display_name, task_type, description, days_to_complete, upload_subfolder_name,
+                  upload_type, uploadable_file_types, help_link,
+                  created_by, created_date)
+values ('apcInterestForm', 'Australian Physiotherapy Council - Expression of Interest','UploadTask', 'Please fill out the Expression of Interest form for the Australian PhysioTherapy Council, the form can be downloaded by clicking on the Task Help button. Once completed, please upload the filled out form below.',
+        7, 'Experience', 'apcInterestForm', null, 'https://drive.google.com/file/d/1gIhD8vnJtHN-LmYtRmaZ7n4U_3-x-FPl/view?usp=sharing',
+        (select id from users where username = 'SystemAdmin'), now());
