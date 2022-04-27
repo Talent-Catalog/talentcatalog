28fdb16fb228b96ae45e6c8dc41ded8dd5a2ed87
Caroline Cameron <caroline.iris.cameron@gmail.com>
2022-04-27 12:10:58 +1000

    Add explicit allowed answers column and task

---
+++ server/src/main/resources/db/migration/V1_195__add_explicit_allowed_answers.sql
@@ -0,0 +1,7 @@
+alter table task add column explicit_allowed_answers text;
+
+insert into task (name, display_name, task_type, description, days_to_complete, explicit_allowed_answers,
+                  created_by, created_date)
+values ('studiedEnglishUniversity','Did you study in English at University?', 'QuestionTask', 'Please select:',
+        7, 'Yes,No',
+        (select id from users where username = 'SystemAdmin'), now());
