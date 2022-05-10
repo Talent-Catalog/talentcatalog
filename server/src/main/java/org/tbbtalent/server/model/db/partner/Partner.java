c4914fc24505b14c1e3cc92fc8b0d50b2024fd7b
John Cameron <john@cameronfoundation.org>
2022-05-10 16:38:45 +1000

    Define basic partner interfaces

---
+++ server/src/main/java/org/tbbtalent/server/model/db/partner/Partner.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tbbtalent.server.model.db.partner;
+
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.tbbtalent.server.model.db.Auditable;
+import org.tbbtalent.server.model.db.Status;
+
+/**
+ * A partner is an organization that works to implement displaced talent mobility.
+ * <p/>
+ * All Talent Catalog users are associated with just one partner at a time.
+ *
+ * @author John Cameron
+ */
+public interface Partner extends Auditable {
+
+    /**
+     * Unique id identifying this partner
+     * @return partner id
+     */
+    Long getId();
+
+    /**
+     * Partner's logo.
+     * <p/>
+     * Optional - if none is specified this call returns null, and the calling code may default
+     * to using a default logo.
+     * @return Link to partner's logo.
+     */
+    @Nullable
+    String getLogo();
+
+    /**
+     * Name of partner.
+     * @return Name of partner - eg "Talent Beyond Boundaries"
+     */
+    @NonNull
+    String getName();
+
+    /**
+     * Status of partner. Partners may be inactivated or deleted.
+     * @return Status of partner
+     */
+    @NonNull
+    Status getStatus();
+
+    /**
+     * Url of partner's website (optional)
+     * @return Website url, null if none - eg "https://talentbeyondboundaries.org/"
+     */
+    @Nullable
+    String getWebsiteUrl();
+
+}
---
+++ server/src/main/java/org/tbbtalent/server/model/db/partner/SourcePartner.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tbbtalent.server.model.db.partner;
+
+import java.util.Set;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+import org.tbbtalent.server.model.db.Country;
+
+/**
+ * A source partner works with displaced talent candidate users, encouraging them to register with
+ * the Talent Catalog, and then assisting them to find international employment and ultimately to
+ * relocate to new lives in a destination country.
+ * <p/>
+ * Each back end/admin users of the Talent Catalog is an employee of a source partner.
+ * The branding of the Talent Catalog displayed to a back end user is defined by the user's
+ * source partner.
+ * <p/>
+ * Each candidate user is associated at any given time with a single source partner.
+ * The branding of the Talent Catalog displayed to a candidate is defined by the candidate's
+ * source partner.
+ *
+ * @author John Cameron
+ */
+public interface SourcePartner extends Partner {
+
+    /**
+     * Url of registration landing page.
+     * <p/>
+     * Typically this page is located somewhere on the partner's website, providing information
+     * about the partner and the Talent Catalog. It will contain a button or some other link
+     * directing the user to the {@link #getRegistrationUrl()} which is the entry point for this
+     * application.
+     * <p/>
+     * For example, Talent Beyond Boundaries currently has a registration landing page at
+     * https://www.talentbeyondboundaries.org/talentcatalog/register
+     * @return Url of landing page. Null if none.
+     */
+    @Nullable
+    String getRegistrationLandingPage();
+
+    /**
+     * Optional branded candidate login url associated with this source partner.
+     * <p/>
+     * This is optional. If specified it typically is a url branded to reflect the source partner.
+     * For example, for Talent Beyond Boundaries the url is currently based on https://tbbtalent.org/.
+     * <p/>
+     * All source partner candidate login url's, even though they may be different, point to this
+     * application.
+     * <p/>
+     * If no special branded url has been specified this call returns null, and the calling code
+     * will default to using the registration url of the default source partner.
+     * @return Candidate login url.
+     */
+    @Nullable
+    String getRegistrationUrl();
+
+    /**
+     * Source countries that this source partner operates in.
+     * <p/>
+     * If countries are specified, this can be used to assist in the assignment of a newly
+     * registered candidate to a source partner - based on the country where the candidate is
+     * currently located.
+     * @return Associated source countries - not null but may be empty
+     */
+    @NonNull
+    Set<Country> getSourceCountries();
+
+    /**
+     * True if this partner is the default source partner - associated with candidates who are
+     * not clearly associated with any other source partner.
+     * <p/>
+     * Only one source partner at any given time can be the default.
+     * @return True if this is the default source partner
+     */
+    boolean isDefaultSourcePartner();
+}
