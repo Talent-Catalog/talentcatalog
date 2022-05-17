8b4b2f6915bde626b599183b5dbd9cabd25f9bf7
John Cameron <john@cameronfoundation.org>
2022-05-17 21:59:36 +1000

    More generic message for NoSuchObjectException

--- server/src/main/java/org/tbbtalent/server/exception/NoSuchObjectException.java
+++ server/src/main/java/org/tbbtalent/server/exception/NoSuchObjectException.java
@@ -5,12 +5,12 @@
  * the terms of the GNU Affero General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
- * You should have received a copy of the GNU Affero General Public License 
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
@@ -22,8 +22,8 @@ public class NoSuchObjectException extends ServiceException {
         super("missing_object", "Missing " + objectClass.getSimpleName() + " with ID " + id);
     }
 
-    public NoSuchObjectException(Class objectClass, String email) {
-        super("missing_object", "Missing " + objectClass.getSimpleName() + " with email " + email);
+    public NoSuchObjectException(Class objectClass, String idStr) {
+        super("missing_object", "Missing " + objectClass.getSimpleName() + " matching " + idStr);
     }
 
     public NoSuchObjectException(String message) {
