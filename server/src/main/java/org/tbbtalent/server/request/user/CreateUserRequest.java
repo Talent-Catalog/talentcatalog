12536593195d430e5933cbbccfefdd85afff97e9
John Cameron <john@cameronfoundation.org>
2022-05-30 18:57:57 +1000

    Refactor - ordering and using Lombok

--- server/src/main/java/org/tbbtalent/server/request/user/CreateUserRequest.java
+++ server/src/main/java/org/tbbtalent/server/request/user/CreateUserRequest.java
@@ -19,77 +19,39 @@ package org.tbbtalent.server.request.user;
 import java.util.List;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
 import org.tbbtalent.server.model.db.Country;
 import org.tbbtalent.server.model.db.Role;
 
+@Getter
+@Setter
 public class CreateUserRequest {
 
     @NotBlank
+    private String email;
+
+    @NotBlank
     private String firstName;
+
     @NotBlank
     private String lastName;
-    @NotBlank
-    private String username;
-    @NotBlank
-    private String password;
-    @NotBlank
-    private String email;
+
+    private Long partnerId;
+
+    private Boolean readOnly;
+
     @NotNull
     private Role role;
 
     //TODO JC Just ids
     private List<Country> sourceCountries;
 
-    private Boolean readOnly;
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
 
     private Boolean usingMfa;
-
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() { return password; }
-
-    public void setPassword(String password) { this.password = password; }
-
-    public String getEmail() { return email; }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public Role getRole() { return role; }
-
-    public void setRole(Role role) { this.role = role; }
-
-    public List<Country> getSourceCountries() { return sourceCountries; }
-
-    public void setSourceCountries(List<Country> sourceCountries) { this.sourceCountries = sourceCountries; }
-
-    public Boolean getReadOnly() { return readOnly; }
-
-    public void setReadOnly(Boolean readOnly) { this.readOnly = readOnly; }
-
-    public Boolean getUsingMfa() { return usingMfa; }
-
-    public void setUsingMfa(Boolean usingMfa) { this.usingMfa = usingMfa; }
 }
