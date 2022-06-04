cce7641a35a51bfba64074fc566e8332ceb6e31d
John Cameron <john@cameronfoundation.org>
2022-06-04 22:22:14 +1000

    Set all abbreviations.
    Make partner abbreviation field not null

---
+++ server/src/main/resources/db/migration/V1_209__add_partners_abbreviations.sql
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+update partner set abbreviation = 'IOM' where 'name' = 'IOM';
+update partner set abbreviation = 'HIAS' where 'name' = 'HIAS';
+update partner set abbreviation = 'UNHCR' where 'name' = 'UNHCR';
+update partner set abbreviation = 'RP' where 'name' = 'Refuge Point';
+update partner set abbreviation = 'Dignity' where 'name' = 'Dignity for Children';
+update partner set abbreviation = 'CRS' where 'name' = 'Catholic Relief Services';
+update partner set abbreviation = 'TBB' where 'name' = 'Talent Beyond Boundaries';
