ab1018bcc16b0cec1f82c0c9f5f3b7762dd73a78
John Cameron <john@cameronfoundation.org>
2022-10-30 14:43:19 +1100

    Add Limit 1 to submission list copy to get past bad data

--- server/src/main/resources/db/migration/V1_229__copy_submission_list_ids_from_job.sql
+++ server/src/main/resources/db/migration/V1_229__copy_submission_list_ids_from_job.sql
@@ -17,4 +17,4 @@
 -- Copy submission list from old job table to corresponding SalesforceJobOpp
 update salesforce_job_opp set submission_list_id =
       (select submission_list_id from job j where salesforce_job_opp.id =
-            (select sf_job_opp_id from saved_list sl where sl.id = j.submission_list_id))
+            (select sf_job_opp_id from saved_list sl where sl.id = j.submission_list_id) limit 1)
