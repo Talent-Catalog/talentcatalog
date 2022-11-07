b287188d4aafacfcda737cd2470ad58c0b0607b4
John Cameron <john@cameronfoundation.org>
2022-11-07 18:34:37 +1100

    Capture local file containing jd or joi

--- ui/admin-portal/src/app/components/job/view/uploads/view-job-uploads/view-job-uploads.component.ts
+++ ui/admin-portal/src/app/components/job/view/uploads/view-job-uploads/view-job-uploads.component.ts
@@ -1,5 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Job} from "../../../../../model/job";
+import {FileSelectorComponent} from "../../../../util/file-selector/file-selector.component";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
+
+enum DocType {
+  jd = "job description",
+  joi = "job intake"
+}
 
 @Component({
   selector: 'app-view-job-uploads',
@@ -13,24 +20,39 @@ export class ViewJobUploadsComponent implements OnInit {
   error: any;
   saving: boolean;
 
-  constructor() { }
+  constructor(
+    private modalService: NgbModal,
+  ) { }
 
   ngOnInit(): void {
   }
 
-  editJd() {
+  get DocType() {
+    return DocType;
+  }
+
+  editJobLink(docType: DocType) {
     //todo
   }
 
-  editJoi() {
-    //todo
+  uploadJobDoc(docType: DocType) {
+    const fileSelectorModal = this.modalService.open(FileSelectorComponent, {
+      centered: true,
+      backdrop: 'static'
+    })
+
+    fileSelectorModal.componentInstance.maxFiles = 1;
+    fileSelectorModal.componentInstance.closeButtonLabel = "Upload";
+    fileSelectorModal.componentInstance.title = "Select file containing the " + docType;
+
+    fileSelectorModal.result
+    .then((selectedFiles: File[]) => {
+      this.doUpload(docType, selectedFiles);
+    })
+    .catch(() => {});
   }
 
-  uploadJd() {
-    //todo
-  }
-
-  uploadJoi() {
-    //todo
+  private doUpload(docType: DocType, selectedFiles: File[]) {
+    //todo upload file and if successful set the link and name
   }
 }
