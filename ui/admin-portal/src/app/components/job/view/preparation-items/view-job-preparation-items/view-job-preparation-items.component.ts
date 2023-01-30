d11fde96b19e34da247d72858ad17b9e72e89dcd
John Cameron <john@cameronfoundation.org>
2023-01-30 14:34:25 +1100

    Don't allow job to be published without minimum info.
    Refactored JobPrepItems up into view job component.

--- ui/admin-portal/src/app/components/job/view/preparation-items/view-job-preparation-items/view-job-preparation-items.component.ts
+++ ui/admin-portal/src/app/components/job/view/preparation-items/view-job-preparation-items/view-job-preparation-items.component.ts
@@ -1,71 +1,19 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges
-} from '@angular/core';
-import {Job} from "../../../../../model/job";
-import {
-  JobPrepDueDate,
-  JobPrepItem,
-  JobPrepJD,
-  JobPrepJobSummary,
-  JobPrepSuggestedCandidates,
-  JobPrepSuggestedSearches
-} from "../../../../../model/job-prep-item";
-import {
-  CandidateSourceCandidateService
-} from "../../../../../services/candidate-source-candidate.service";
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {JobPrepItem} from "../../../../../model/job-prep-item";
 
 @Component({
   selector: 'app-view-job-preparation-items',
   templateUrl: './view-job-preparation-items.component.html',
   styleUrls: ['./view-job-preparation-items.component.scss']
 })
-export class ViewJobPreparationItemsComponent implements OnInit, OnChanges {
-  @Input() job: Job;
+export class ViewJobPreparationItemsComponent implements OnInit {
+  @Input() jobPrepItems: JobPrepItem[];
   @Output() itemSelected = new EventEmitter();
 
   selectedItem: JobPrepItem;
   error: any;
 
-  private jobPrepSuggestedCandidates = new JobPrepSuggestedCandidates();
-
-  jobPrepItems: JobPrepItem[] = [
-    new JobPrepJobSummary(),
-    new JobPrepJD(),
-    //todo temporary comment out: new JobPrepJOI(),
-    new JobPrepSuggestedSearches(),
-    this.jobPrepSuggestedCandidates,
-    new JobPrepDueDate(),
-  ];
-
-  constructor(private candidateSourceService: CandidateSourceCandidateService) {
-  }
-
   ngOnInit(): void {
-  }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.job) {
-      this.checkSubmissionListContents();
-      this.jobPrepItems.forEach(j => j.job = this.job)
-    }
-  }
-
-  private checkSubmissionListContents() {
-    const submissionList = this.job?.submissionList;
-    if (submissionList == null) {
-      this.jobPrepSuggestedCandidates.empty = true;
-    } else {
-      this.candidateSourceService.isEmpty(submissionList).subscribe(
-        (empty) => this.jobPrepSuggestedCandidates.empty = empty,
-        (error) => this.error = error
-      )
-    }
   }
 
   onItemSelected(item: JobPrepItem) {
