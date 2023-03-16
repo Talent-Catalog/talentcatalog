2f0bff92f1bb740128027f1e8124d3ee2ad306d6
Caroline Cameron <caroline.iris.cameron@gmail.com>
2023-03-17 10:51:43 +1100

    Add some employer details to top of JOI

--- ui/admin-portal/src/app/components/job/view/tab/job-intake-tab/job-intake-tab.component.ts
+++ ui/admin-portal/src/app/components/job/view/tab/job-intake-tab/job-intake-tab.component.ts
@@ -10,7 +10,6 @@ import {AuthService} from "../../../../../services/auth.service";
 })
 export class JobIntakeTabComponent extends JobIntakeComponentTabBase {
 
-  countriesExample: String[] = ['Australia', 'Canada'];
   constructor(
     authService: AuthService,
     jobService: JobService
