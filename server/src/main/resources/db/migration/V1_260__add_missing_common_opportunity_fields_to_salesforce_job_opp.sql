77817349d75aa8bf3fa0247f2e10691a17e8cbf4
John Cameron <john@cameronfoundation.org>
2023-06-20 09:48:15 +1000

    Rename salesforce_job_opp field from sf_job_opp_id to sf_id

--- server/src/main/resources/db/migration/V1_260__add_missing_common_opportunity_fields_to_salesforce_job_opp.sql
+++ server/src/main/resources/db/migration/V1_260__add_missing_common_opportunity_fields_to_salesforce_job_opp.sql
@@ -17,4 +17,4 @@
 alter table salesforce_job_opp add column closing_comments text;
 alter table salesforce_job_opp add column next_step text;
 alter table salesforce_job_opp add column next_step_due_date date;
-alter table salesforce_job_opp add column sf_id text;
+alter table salesforce_job_opp rename column sf_job_opp_id to sf_id;
