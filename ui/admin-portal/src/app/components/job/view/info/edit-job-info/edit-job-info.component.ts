26e531f7c848a69687721890f43a6df46eacb252
John Cameron <john@cameronfoundation.org>
2023-06-27 17:39:21 +1000

    Prepare for displaying recruiter partner, removing contact email and adding conatct user.

--- ui/admin-portal/src/app/components/job/view/info/edit-job-info/edit-job-info.component.ts
+++ ui/admin-portal/src/app/components/job/view/info/edit-job-info/edit-job-info.component.ts
@@ -21,8 +21,7 @@ import {JobService} from "../../../../../services/job.service";
 import {Job, UpdateJobRequest} from "../../../../../model/job";
 import {forkJoin} from "rxjs";
 import {PartnerService} from "../../../../../services/partner.service";
-import {Partner, PartnerType} from "../../../../../model/partner";
-import {SearchPartnerRequest, SearchUserRequest} from "../../../../../model/base";
+import {SearchUserRequest} from "../../../../../model/base";
 import {UserService} from "../../../../../services/user.service";
 import {User} from "../../../../../model/user";
 import {AuthService} from "../../../../../services/auth.service";
@@ -34,13 +33,10 @@ import {AuthService} from "../../../../../services/auth.service";
 })
 export class EditJobInfoComponent implements OnInit {
 
-  jobId: number;
-
   job: Job;
 
   jobForm: FormGroup;
 
-  recruiters: Partner[];
   users: User[];
 
   error;
@@ -59,19 +55,15 @@ export class EditJobInfoComponent implements OnInit {
     this.error = null;
     this.loading = true;
     const userRequest: SearchUserRequest = {
+      partnerId: this.job.recruiterPartner.id, //todo recruiter is currently always null.
       sortFields: ["firstName", "lastName"],
       sortDirection: "ASC"
     };
-    const partnerRequest: SearchPartnerRequest = {partnerType: PartnerType.RecruiterPartner};
     forkJoin({
-      'job': this.jobService.get(this.jobId),
-      'partners': this.partnerService.search(partnerRequest),
       'users': this.userService.search(userRequest),
     }).subscribe(results => {
       this.loading = false;
-      this.recruiters = results['partners'];
       this.users = results['users'].map(u => {u.name = u.firstName + " " + u.lastName; return u});
-      this.job = results['job'];
       this.createForm();
     }, error => {
       this.loading = false;
@@ -82,10 +74,8 @@ export class EditJobInfoComponent implements OnInit {
   private createForm() {
     this.jobForm = this.fb.group({
       submissionDueDate: [this.job.submissionDueDate],
-      contactEmail: [this.job.contactEmail],
       contactUser: [this.job.contactUser],
       recruiterPartner: [this.job.recruiterPartner]
-      //  todo other fields
     });
   }
 
@@ -96,12 +86,12 @@ export class EditJobInfoComponent implements OnInit {
   onSave() {
     this.error = null;
     this.saving = true;
-    //todo need to add contactEmail, recruiter  etc
+    //todo need to add contactUser  etc
     const request: UpdateJobRequest = {
       submissionDueDate: this.submissionDueDate
     }
 
-    this.jobService.update(this.jobId, request).subscribe(
+    this.jobService.update(this.job.id, request).subscribe(
       (job) => {
         this.closeModal(job);
         this.saving = false;
