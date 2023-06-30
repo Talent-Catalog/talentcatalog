64478747d38274503a2a9abb2c667b1a01e14575
John Cameron <john@cameronfoundation.org>
2023-06-30 17:54:28 +1000

    Redo Flyways

--- server/src/main/resources/db/migration/V1_265__replace_null_recruiter_partners.sql
+++ server/src/main/resources/db/migration/V1_265__replace_null_recruiter_partners.sql
@@ -15,5 +15,5 @@
  */
 
 update salesforce_job_opp set recruiter_partner_id =
-                                  (select id from partner where default_source_partner = true)
+                                  (select id from partner where default_destination_partner = true)
 where recruiter_partner_id is null;
