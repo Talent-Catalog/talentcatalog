b77b84e9d7ea761c0bc21fd97f132ec00efddf7c
john <jadcpub@gmail.com>
2023-07-13 15:54:23 +1000

    Initialize source_partner and job_creator fields of defaults

--- server/src/main/resources/db/migration/V1_269__add_partner_source_partner_and_job_creator_fields.sql
+++ server/src/main/resources/db/migration/V1_269__add_partner_source_partner_and_job_creator_fields.sql
@@ -19,3 +19,6 @@ alter table partner add column job_creator boolean default false not null;
 
 update partner set source_partner = true where partner_type = 'SourcePartner';
 
+update partner set source_partner = true where default_source_partner = true;
+update partner set job_creator = true where default_destination_partner = true;
+
