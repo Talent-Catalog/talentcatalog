8149efb64ca7444219ee4ce3b5f857e8940c2ef7
John Cameron <john@cameronfoundation.org>
2023-07-24 11:47:02 +1000

    Update flyway to also update publishedBy from createdBy where appropriate

--- server/src/main/resources/db/migration/V1_270__clean_up_jobs.sql
+++ server/src/main/resources/db/migration/V1_270__clean_up_jobs.sql
@@ -15,6 +15,10 @@
  */
 
 -- For old jobs that did not have a publishedDate, set the published date to the created date
--- the publishedDate field was introduced 2022-10-31
+-- and publishedBy to createdBy
+-- The publishedDate field was introduced 2022-10-31
 update salesforce_job_opp set published_date = created_date
     where published_date is null and created_date <= '2022-10-31'::date;
+
+update salesforce_job_opp set published_by = created_by
+    where published_by is null and created_by is not null and created_date <= '2022-10-31'::date;
