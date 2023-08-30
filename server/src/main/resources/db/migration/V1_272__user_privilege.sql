db4984a8a3b662ff958d1bb2b2a39a64659f9f11
john <jadcpub@gmail.com>
2023-08-30 14:38:54 +0100

    Add user_privilege table

---
+++ server/src/main/resources/db/migration/V1_272__user_privilege.sql
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2021 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+create table user_privilege (
+  user_id      bigint not null references users,
+  privilege    text not null,
+
+--   Name must be unique for a user. ie a user cannot have the same privilege twice
+  primary key (user_id, privilege)
+);
