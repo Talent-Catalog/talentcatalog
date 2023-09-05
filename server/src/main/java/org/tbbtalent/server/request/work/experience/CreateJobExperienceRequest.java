bc7b5ee525de6551541feed6cfa60c67bbd921bb
sadatmalik <sadat.malik@me.com>
2023-09-05 14:18:52 +0100

    use lombok getter + setter annotations

--- server/src/main/java/org/tbbtalent/server/request/work/experience/CreateJobExperienceRequest.java
+++ server/src/main/java/org/tbbtalent/server/request/work/experience/CreateJobExperienceRequest.java
@@ -19,79 +19,40 @@ package org.tbbtalent.server.request.work.experience;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
 import org.tbbtalent.server.util.html.HtmlSanitizer;
 
+@Getter
+@Setter
 public class CreateJobExperienceRequest {
 
     private Long candidateId;
+
     @NotBlank
     private String companyName;
+
     @NotNull
     private Long countryId;
+
     @NotNull
     private Long candidateOccupationId;
+
     @NotBlank
     private String role;
+
     private LocalDate startDate;
     private LocalDate endDate;
     private Boolean fullTime;
     private Boolean paid;
+
     @NotBlank
     private String description;
-
-    public Long getCandidateId() {
-        return candidateId;
-    }
-
-    public void setCandidateId(Long candidateId) {
-        this.candidateId = candidateId;
-    }
-
-    public void setCountryId(Long countryId) {
-        this.countryId = countryId;
-    }
-
-    public String getCompanyName() { return companyName; }
-
-    public void setCompanyName(String companyName) { this.companyName = companyName; }
-
-    public Long getCountryId() {
-        return countryId;
-    }
 
     public void setCountry(Long countryId) {
         this.countryId = countryId;
     }
-
-    public Long getCandidateOccupationId() {
-        return candidateOccupationId;
-    }
-
-    public void setCandidateOccupationId(Long candidateOccupationId) {
-        this.candidateOccupationId = candidateOccupationId;
-    }
-
-    public String getRole() { return role; }
-
-    public void setRole(String role) { this.role = role; }
-
-    public LocalDate getStartDate() { return startDate; }
-
-    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
-
-    public LocalDate getEndDate() { return endDate; }
-
-    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
-
-    public Boolean getFullTime() { return fullTime; }
-
-    public void setFullTime(Boolean fullTime) { this.fullTime = fullTime; }
-
-    public Boolean getPaid() { return paid; }
-
-    public void setPaid(Boolean paid) { this.paid = paid; }
-
-    public String getDescription() { return description; }
 
     public void setDescription(String description) {
         this.description = HtmlSanitizer.sanitize(description);
