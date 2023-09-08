a13deefde32439652215131351a7a803a948e75a
john <jadcpub@gmail.com>
2023-09-08 13:24:10 +0100

    Add Angular rxStomp support

---
+++ ui/admin-portal/src/app/my-rx-stomp.config.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2023 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+import {RxStompConfig} from '@stomp/rx-stomp';
+
+export const myRxStompConfig: RxStompConfig = {
+  // Which server?
+  //todo 9090
+  //todo websocket twice?
+  brokerURL: 'ws://localhost:9090/websocket/websocket',
+
+  // Headers
+  // Typical keys: login, passcode, host
+  //todo connection?
+  // connectHeaders: {
+  //   login: 'guest',
+  //   passcode: 'guest',
+  // },
+
+  // How often to heartbeat?
+  // Interval in milliseconds, set to 0 to disable
+  heartbeatIncoming: 0, // Typical value 0 - disabled
+  heartbeatOutgoing: 20000, // Typical value 20000 - every 20 seconds
+
+  // Wait in milliseconds before attempting auto reconnect
+  // Set to 0 to disable
+  // Typical value 500 (500 milli seconds)
+  reconnectDelay: 200,
+
+  // Will log diagnostics on console
+  // It can be quite verbose, not recommended in production
+  // Skip this key to stop logging to console
+  debug: (msg: string): void => {
+    console.log(new Date(), msg);
+  },
+};
