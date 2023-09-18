af485c48e5d783fae7e6344e009d4a10d784b610
samschlicht <samschlicht@gmail.com>
2023-09-18 19:00:55 +1000

    Added migration file for my PR #300 - removed John's previous entry per his request

--- server/src/main/resources/db/migration/V1_272__remove_verified_field_from_candidate_occupation.sql
+++ server/src/main/resources/db/migration/V1_272__remove_verified_field_from_candidate_occupation.sql
@@ -13,4 +13,4 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
-alter table users add column job_creator boolean not null default false;
+alter table candidate_occupation drop column verified;
