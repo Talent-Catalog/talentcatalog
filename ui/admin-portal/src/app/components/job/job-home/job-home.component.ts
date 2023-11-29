4346a4ad591cd81c01118e96fe387e107977c07f
sadatmalik <sadat.malik@me.com>
2023-11-29 09:25:55 +0000

    new component for all jobs related sub-menu tabs

---
+++ ui/admin-portal/src/app/components/job/job-home/job-home.component.ts
@@ -0,0 +1,27 @@
+import {Component} from '@angular/core';
+import {SavedSearchService} from "../../../services/saved-search.service";
+import {LocalStorageService} from "angular-2-local-storage";
+import {AuthorizationService} from "../../../services/authorization.service";
+import {AuthenticationService} from "../../../services/authentication.service";
+import {HomeComponent} from "../../candidates/home.component";
+
+@Component({
+  selector: 'app-job-home',
+  templateUrl: './job-home.component.html',
+  styleUrls: ['./job-home.component.scss']
+})
+export class JobHomeComponent extends HomeComponent {
+
+  constructor(
+    protected localStorageService: LocalStorageService,
+    protected savedSearchService: SavedSearchService,
+    protected authService: AuthorizationService,
+    protected authenticationService: AuthenticationService
+  ) {
+    super(localStorageService, savedSearchService, authService, authenticationService);
+    this.lastTabKey = 'JobsHomeLastTab';
+    this.lastCategoryTabKey = 'JobsHomeLastCategoryTab';
+    this.defaultTabId = 'NewJob';
+  }
+
+}
