f9269fb7c61ffff23bc1740be735ea39bf06a351
sadatmalik <sadat.malik@me.com>
2023-11-30 17:05:49 +0000

    resolve intelliJ warnings - use lombok getter + setter

--- server/src/main/java/org/tctalent/server/request/education/major/CreateEducationMajorRequest.java
+++ server/src/main/java/org/tctalent/server/request/education/major/CreateEducationMajorRequest.java
@@ -18,9 +18,12 @@ package org.tctalent.server.request.education.major;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-
+import lombok.Getter;
+import lombok.Setter;
 import org.tctalent.server.model.db.Status;
 
+@Getter
+@Setter
 public class CreateEducationMajorRequest {
 
     @NotBlank
@@ -29,19 +32,4 @@ public class CreateEducationMajorRequest {
     @NotNull
     private Status status;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
 }
