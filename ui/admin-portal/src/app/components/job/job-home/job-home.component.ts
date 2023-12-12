961303890a25dfe8c8cbf53bc47ba2f9d8302153
john <jadcpub@gmail.com>
2023-12-12 14:54:07 +1100

    Simplify my cases tab name where possible

--- ui/admin-portal/src/app/components/job/job-home/job-home.component.ts
+++ ui/admin-portal/src/app/components/job/job-home/job-home.component.ts
@@ -4,6 +4,7 @@ import {LocalStorageService} from "angular-2-local-storage";
 import {AuthorizationService} from "../../../services/authorization.service";
 import {AuthenticationService} from "../../../services/authentication.service";
 import {HomeComponent} from "../../candidates/home.component";
+import {SearchOppsBy} from "../../../model/base";
 
 @Component({
   selector: 'app-job-home',
@@ -24,4 +25,21 @@ export class JobHomeComponent extends HomeComponent {
     this.defaultTabId = 'LiveJobs';
   }
 
+  /**
+   * Compute my cases tab name - only inserting Job or Source into name if it is needed
+   * to clarify different types in the case where we a dealing with a partner who is both a
+   * job creator and source partner (eg TBB)
+   * @param searchOppsBy Type of case search.
+   */
+  myCasesTabName(searchOppsBy: SearchOppsBy): string {
+    const partnerName = this.loggedInPartner?.abbreviation;
+    let tabName;
+    if (this.isJobCreator() && this.isSourcePartner()) {
+      const extra = searchOppsBy === SearchOppsBy.mineAsJobCreator ? " Job" : " Source";
+      tabName = partnerName + extra + " Cases"
+    } else {
+      tabName = partnerName + " Cases"
+    }
+    return tabName;
+  }
 }
