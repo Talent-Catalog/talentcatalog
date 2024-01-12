04756d822e8d8c774fe09978e340ffd47039228c
John Cameron <john@cameronfoundation.org>
2024-01-12 17:32:08 +1100

    Simplify/fix multi chat read status

--- ui/admin-portal/src/app/components/chat/chat-read-status/chat-read-status.component.ts
+++ ui/admin-portal/src/app/components/chat/chat-read-status/chat-read-status.component.ts
@@ -1,9 +1,14 @@
 import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
-import {combineLatest, Observable, Subscription} from "rxjs";
+import {combineLatest, Subscription} from "rxjs";
 import {ChatService} from "../../../services/chat.service";
 import {JobChat} from "../../../model/chat";
 import {map} from "rxjs/operators";
 
+/**
+ * Component which takes an array of chats and sets the unreadIndicator to unread ('*") if
+ * any of the chats is unread.
+ * Otherwise it sets the unread indicator to blank.
+ */
 @Component({
   selector: 'app-chat-read-status',
   templateUrl: './chat-read-status.component.html',
@@ -46,14 +51,15 @@ export class ChatReadStatusComponent implements OnInit, OnChanges, OnDestroy {
         )
       } else if (this.chats.length > 1) {
 
-        let x: Observable<boolean>[] = [];
-        for (const chat of this.chats) {
-          x.push(this.chatService.getChatReadStatusObservable(chat));
-        }
-        const chatReadStatus$ = combineLatest(x).pipe(
-          //TODO TEST this
-          //For this to return isRead to be true for all chats, none can be false
-          map(statuses => statuses.find(isRead => false) == null)
+        //Construct array of chat read statues from array of chats
+        let chatReadStatuses$ = this.chats.map(
+          (chat) => this.chatService.getChatReadStatusObservable(chat));
+
+        //Combine the latest values of all the statuses and return a single status which is true
+        //only if all are true (ie none are false)
+        const chatReadStatus$ = combineLatest(chatReadStatuses$).pipe(
+          //For isRead to be true, no chats can be false (unread)
+          map(statuses =>  statuses.find(isRead => isRead == false) == null)
         );
         this.subscription = chatReadStatus$.subscribe(
           (chatIsRead) => this.setIndicator(chatIsRead)
