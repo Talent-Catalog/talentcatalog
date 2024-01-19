b23f0486ea2e8a39f69c2c529c3f2122bbe84f01
John Cameron <john@cameronfoundation.org>
2024-01-19 15:38:15 +1100

    Allow last_read_post_id to be null

--- server/src/main/resources/db/migration/V1_290__create_job_chat_user_table.sql
+++ server/src/main/resources/db/migration/V1_290__create_job_chat_user_table.sql
@@ -18,6 +18,6 @@ create table job_chat_user
 (
     job_chat_id bigint not null references job_chat,
     user_id bigint not null references users,
-    last_read_post_id bigint not null references chat_post,
+    last_read_post_id bigint references chat_post,
     primary key (job_chat_id, user_id)
 );
