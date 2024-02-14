aa72af83dcf48657f21cc23d6bdcc82cdff4e2e9
John Cameron <john@cameronfoundation.org>
2024-02-14 13:16:30 +1100

    Updated Flyway

--- server/src/main/resources/db/migration/V1_293__add_job_chat_candidate_id_field.sql
+++ server/src/main/resources/db/migration/V1_293__add_job_chat_candidate_id_field.sql
@@ -24,5 +24,13 @@ create index job_chat_type_idx on job_chat(type);
 
 -- Populate new field with candidate id taken from candidate associated with candidate opp
 -- associated with existing candidate_opp_id field
-update job_chat set candidate_id = opp.candidate_id from candidate_opportunity opp
+update job_chat
+set candidate_id = opp.candidate_id, job_id = opp.job_opp_id
+from candidate_opportunity opp
 where candidate_opp_id is not null and candidate_opp_id = opp.id;
+
+-- Remove all candidate prospect chats they will be regenerated, with one prospect chat being
+-- shared across opportunities
+delete from job_chat_user where (select type from job_chat where job_chat_id = id) = 'CandidateProspect';
+delete from chat_post where (select type from job_chat where job_chat_id = id) = 'CandidateProspect';
+delete from job_chat where type = 'CandidateProspect';
