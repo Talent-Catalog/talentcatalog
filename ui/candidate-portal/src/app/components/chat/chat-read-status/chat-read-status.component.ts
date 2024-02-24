b9c21e1fd509c6c6ef6b0f8e999b05d01460eef9
John Cameron <john@cameronfoundation.org>
2024-02-24 11:07:14 +1100

    Doc only

--- ui/candidate-portal/src/app/components/chat/chat-read-status/chat-read-status.component.ts
+++ ui/candidate-portal/src/app/components/chat/chat-read-status/chat-read-status.component.ts
@@ -60,7 +60,7 @@ export class ChatReadStatusComponent implements OnInit, OnChanges, OnDestroy {
         //Construct single observable to monitor.
         const chatReadStatus$
           = this.chatService.combineChatReadStatuses(this.chats);
-        console.log('ChatReadStatus subscribing to chats' +
+        console.log('ChatReadStatus subscribing to chats ' +
           this.chats.map(chat => chat.id).join(','))
         this.subscribeToObservable(chatReadStatus$)
       }
