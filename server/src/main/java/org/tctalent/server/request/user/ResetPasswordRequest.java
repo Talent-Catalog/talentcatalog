4435b781efbdd54f161a50d5da427f9a87a0ae01
sadatmalik <sadat.malik@me.com>
2024-02-26 14:53:44 +0000

    lombok

--- server/src/main/java/org/tctalent/server/request/user/ResetPasswordRequest.java
+++ server/src/main/java/org/tctalent/server/request/user/ResetPasswordRequest.java
@@ -16,34 +16,14 @@
 
 package org.tctalent.server.request.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResetPasswordRequest {
 
     private String token;
     private String password;
     private String passwordConfirmation;
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-	public String getPasswordConfirmation() {
-		return passwordConfirmation;
-	}
-
-	public void setPasswordConfirmation(String passwordConfirmation) {
-		this.passwordConfirmation = passwordConfirmation;
-	}
-
 }
