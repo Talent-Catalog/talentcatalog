c22a53dae1f4f60ce95cdb2d3b910a717fa454c6
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-02-29 13:40:27 +1100

    Keep cand portal consistent with admin portal changes

--- ui/candidate-portal/src/app/components/chat/create-update-post/create-update-post.component.ts
+++ ui/candidate-portal/src/app/components/chat/create-update-post/create-update-post.component.ts
@@ -19,7 +19,6 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {RxStompService} from "../../../services/rx-stomp.service";
 import {JobChat, Post} from "../../../model/chat";
 import Quill from 'quill';
-import {ImageHandler, Options} from "ngx-quill-upload";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {ChatPostService} from "../../../services/chat-post.service";
 import {FileSelectorComponent} from "../../util/file-selector/file-selector.component";
@@ -43,19 +42,7 @@ export class CreateUpdatePostComponent implements OnInit {
     private rxStompService: RxStompService,
     private modalService: NgbModal,
     private chatPostService: ChatPostService
-  ) {
-    Quill.register('modules/imageHandler', ImageHandler);
-    this.moduleOptions = {
-      // Doc for setting module toolbar options: https://quilljs.com/docs/modules/toolbar/
-      // Doc for setting up image handler: https://www.npmjs.com/package/ngx-quill-upload
-      imageHandler: {
-        upload: (file) => {
-          return this.doUpload(file);// your uploaded image URL as Promise<string>
-        },
-        accepts: ['png', 'jpg', 'jpeg'] // Extensions to allow for images (Optional) | Default - ['jpg', 'jpeg', 'png']
-      } as Options,
-    }
-  }
+  ) {}
 
   ngOnInit() {
     this.postForm = this.fb.group({
@@ -70,52 +57,30 @@ export class CreateUpdatePostComponent implements OnInit {
   // Note: The image handler must return a Promise<String> so we need to convert our http request observable to a promise.
   // The doc suggests using the toPromise() method on the subscription, however this is to be deprecated in new versions of Rxjs.
   // Alternative option is create a new promise and resolve with the url string.
-  private doUpload(file: File): Promise<String> {
-    return new Promise ((resolve, reject) => {
-      const formData: FormData = new FormData();
-      formData.append('file', file);
+  private doUpload(file: File) {
+    const formData: FormData = new FormData();
+    formData.append('file', file);
 
-      this.error = null;
-      this.saving = true;
-      // Upload image to the job's Google Drive folder (subfolder: ChatUploads).
-      // The url string will then be returned through the Promise, and embedded into the editor.
-      this.chatPostService.uploadFile(this.chat.id, formData).subscribe(
-        urlDto => {
-          this.saving = false;
-          resolve(urlDto.url);
-        },
-        (error) => {
-          reject(error)
-          this.error = error
-          this.saving = false;
-        });
-    })
+    this.error = null;
+    this.saving = true;
+    // Upload image to the job's Google Drive folder (subfolder: ChatUploads).
+    // The url string will then be returned through the Promise, and embedded into the editor.
+    this.chatPostService.uploadFile(this.chat.id, formData).subscribe(
+      urlDto => {
+        if (file.type.startsWith("image")) {
+          this.contentControl.patchValue(this.postForm.value.content + " <img src='" + urlDto.url + "'>")
+        } else {
+          this.contentControl.patchValue(this.postForm.value.content + " <a href=" + urlDto.url + ">link to file</a>")
+        }
+        this.saving = false;
+      },
+      (error) => {
+        this.error = error
+        this.saving = false;
+      });
   }
 
-  uploadFile() {
-    const fileSelectorModal = this.modalService.open(FileSelectorComponent, {
-      centered: true,
-      backdrop: 'static'
-    })
-
-    fileSelectorModal.componentInstance.maxFiles = 1;
-    fileSelectorModal.componentInstance.closeButtonLabel = "Upload";
-    fileSelectorModal.componentInstance.title = "Select file to upload";
-    fileSelectorModal.componentInstance.validExtensions = ['pdf', 'doc', 'docx', 'txt'];
-
-    fileSelectorModal.result
-    .then((selectedFiles: File[]) => {
-      if (selectedFiles.length > 0) {
-        this.doUpload(selectedFiles[0]).then((url: String) => {
-          this.contentControl.setValue(this.contentControl.value + " <a href=" + url + ">link to file</a>")
-        });
-      }
-    })
-    .catch(() => {
-    });
-  }
-
-  get contentControl() { return this.postForm.get('content'); }
+  get contentControl() { return this.postForm.controls.content; }
 
   onSend() {
     if (this.chat) {
@@ -129,5 +94,25 @@ export class CreateUpdatePostComponent implements OnInit {
       //Clear content.
       this.contentControl.patchValue(null);
     }
+  }
+
+  uploadFile() {
+    const fileSelectorModal = this.modalService.open(FileSelectorComponent, {
+      centered: true,
+      backdrop: 'static'
+    })
+
+    fileSelectorModal.componentInstance.maxFiles = 1;
+    fileSelectorModal.componentInstance.closeButtonLabel = "Upload";
+    fileSelectorModal.componentInstance.title = "Select file to upload";
+
+    fileSelectorModal.result
+      .then((selectedFiles: File[]) => {
+        if (selectedFiles.length > 0) {
+          this.doUpload(selectedFiles[0]);
+        }
+      })
+      .catch(() => {
+      });
   }
 }
