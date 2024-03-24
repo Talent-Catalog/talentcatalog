33da5d7c06f461d806a9668651bcc7deec971fd2
john <jadcpub@gmail.com>
2024-03-24 13:11:16 +1100

    Add focus to help_link table

--- server/src/main/resources/db/migration/V1_307__add_help_link_next_step_info.sql
+++ server/src/main/resources/db/migration/V1_307__add_help_link_next_step_info.sql
@@ -13,6 +13,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
+alter table help_link add column focus text;
 alter table help_link add column next_step_name text;
 alter table help_link add column next_step_text text;
 alter table help_link add column next_step_days integer;
