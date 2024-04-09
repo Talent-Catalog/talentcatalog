b99bfcef1e7661b66b96bba4bb349c06764d7565
John Cameron <john@cameronfoundation.org>
2024-04-09 17:23:21 +1000

    Insert canada helplinks

---
+++ server/src/main/resources/db/migration/V1_311__add_canada_help_links.sql
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2021 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Create Salesforce account and employer opportunity','briefing','null','https://tchelp.tettra.site/canada-1/operations-manual-1#create-salesforce-account-and-employer-opportunity');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule call with employer','briefing','null','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-call-with-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Pitch to employer','pitching','null','https://tchelp.tettra.site/canada-1/operations-manual-1#pitch-to-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Pitch follow up','pitching','null','https://tchelp.tettra.site/canada-1/operations-manual-1#pitch-follow-up');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Employer follow up','pitching','null','https://tchelp.tettra.site/canada-1/operations-manual-1#employer-follow-up--');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Assess roles','pitching','null','https://tchelp.tettra.site/canada-1/operations-manual-1#assess-roles');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Draft MOU','identifyingRoles','null','https://tchelp.tettra.site/canada-1/operations-manual-1#draft-mou-');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Send employer survey if the employer decides not to move forward','noJobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#send-employer-survey-if-the-employer-decides-not-to-move-forward');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Send employer survey if the employer decides not to move forward','noInterest','null','https://tchelp.tettra.site/canada-1/operations-manual-1#send-employer-survey-if-the-employer-decides-not-to-move-forward');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Send employer survey if the employer decides not to move forward','noSuitableCandidates','null','https://tchelp.tettra.site/canada-1/operations-manual-1#send-employer-survey-if-the-employer-decides-not-to-move-forward');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Sign MOU','identifyingRoles','null','https://tchelp.tettra.site/canada-1/operations-manual-1#sign-mou');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Register the job on the Talent Catalog','candidateSearch','null','https://tchelp.tettra.site/canada-1/operations-manual-1#register-the-job-on-the-talent-catalog');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Sample candidate identification','candidateSearch','null','https://tchelp.tettra.site/canada-1/operations-manual-1#sample-candidate-identification');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Candidate identification','candidateSearch','null','https://tchelp.tettra.site/canada-1/operations-manual-1#candidate-identification');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Candidate identification','prospect','null','https://tchelp.tettra.site/canada-1/operations-manual-1#candidate-identification');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Talent Catalog list of candidates','null','visaEligibility','https://tchelp.tettra.site/canada-1/operations-manual-1#talent-catalog-list-of-candidates');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Talent Catalog list of candidates','visaEligibility','null','https://tchelp.tettra.site/canada-1/operations-manual-1#talent-catalog-list-of-candidates');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Talent Catalog list of candidates','noSuitableCandidates','null','https://tchelp.tettra.site/canada-1/operations-manual-1#talent-catalog-list-of-candidates');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Prepare CV','null','cvPreparation','https://tchelp.tettra.site/canada-1/operations-manual-1#prepare-cv');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Prepare CV','cvPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#prepare-cv');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('CV approval (by candidate)','null','cvPreparation','https://tchelp.tettra.site/canada-1/operations-manual-1#cv-approval-by-candidate');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('CV approval (by candidate)','cvPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#cv-approval-by-candidate');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa eligibility','null','visaEligibility','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-eligibility');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa eligibility','visaEligibility','null','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-eligibility');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Publish and share CV list with employer','null','cvReview','https://tchelp.tettra.site/canada-1/operations-manual-1#publish-and-share-cv-list-with-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Publish and share CV list with employer','cvReview','null','https://tchelp.tettra.site/canada-1/operations-manual-1#publish-and-share-cv-list-with-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Identify candidates for interview / request feedback','null','twoWayPreparation','https://tchelp.tettra.site/canada-1/operations-manual-1#identify-candidates-for-interview--request-feedback');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Identify candidates for interview / request feedback','null','noInterview','https://tchelp.tettra.site/canada-1/operations-manual-1#identify-candidates-for-interview--request-feedback');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Identify candidates for interview / request feedback','recruitmentProcess','null','https://tchelp.tettra.site/canada-1/operations-manual-1#identify-candidates-for-interview--request-feedback');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Initiate required workflows','null','twoWayPreparation','https://tchelp.tettra.site/canada-1/operations-manual-1#initiate-required-workflows');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Initiate required workflows','null','noInterview','https://tchelp.tettra.site/canada-1/operations-manual-1#initiate-required-workflows');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Initiate required workflows','recruitmentProcess','null','https://tchelp.tettra.site/canada-1/operations-manual-1#initiate-required-workflows');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule employer interview','null','twoWayReview','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-employer-interview');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule employer interview','recruitmentProcess','null','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-employer-interview');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Request feedback','null','twoWayReview','https://tchelp.tettra.site/canada-1/operations-manual-1#request-feedback');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Request feedback','recruitmentProcess','null','https://tchelp.tettra.site/canada-1/operations-manual-1#request-feedback');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Review and Process Draft Job Offer','null','offer','https://tchelp.tettra.site/canada-1/operations-manual-1#review-and-process-draft-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Review and Process Draft Job Offer','null','noJobOffer','https://tchelp.tettra.site/canada-1/operations-manual-1#review-and-process-draft-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Review and Process Draft Job Offer','jobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#review-and-process-draft-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Review and Process Draft Job Offer','noJobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#review-and-process-draft-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Employer decision: Job offer or no job offer','null','offer','https://tchelp.tettra.site/canada-1/operations-manual-1#employer-decision-job-offer-or-no-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Employer decision: Job offer or no job offer','null','noJobOffer','https://tchelp.tettra.site/canada-1/operations-manual-1#employer-decision-job-offer-or-no-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Employer decision: Job offer or no job offer','jobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#employer-decision-job-offer-or-no-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Employer decision: Job offer or no job offer','noJobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#employer-decision-job-offer-or-no-job-offer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Update candidates information','null','offer','https://tchelp.tettra.site/canada-1/operations-manual-1#update-candidates-information');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Update candidates information','null','noJobOffer','https://tchelp.tettra.site/canada-1/operations-manual-1#update-candidates-information');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Update candidates information','jobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#update-candidates-information');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Update candidates information','noJobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#update-candidates-information');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule the Informed-Decision Making Session','null','offer','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-the-informed-decision-making-session');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule the Informed-Decision Making Session','null','noJobOffer','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-the-informed-decision-making-session');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule the Informed-Decision Making Session','jobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-the-informed-decision-making-session');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Schedule the Informed-Decision Making Session','noJobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#schedule-the-informed-decision-making-session');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Informed Decision Making (IDM)','null','offer','https://tchelp.tettra.site/canada-1/operations-manual-1#informed-decision-making-idm');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Informed Decision Making (IDM)','jobOffer','null','https://tchelp.tettra.site/canada-1/operations-manual-1#informed-decision-making-idm');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Creation of Slack Channel; Initiate trusted partner referral, and send language testing booking instructions','null','acceptance','https://tchelp.tettra.site/canada-1/operations-manual-1#creation-of-slack-channel-initiate-trusted-partner-referral-and-send-language-testing-booking-instructions');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Creation of Slack Channel; Initiate trusted partner referral, and send language testing booking instructions','visaPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#creation-of-slack-channel-initiate-trusted-partner-referral-and-send-language-testing-booking-instructions');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Introduce Relocation Team to employer','null','acceptance','https://tchelp.tettra.site/canada-1/operations-manual-1#introduce-relocation-team-to-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Introduce Relocation Team to employer','visaPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#introduce-relocation-team-to-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Language test completion','null','acceptance','https://tchelp.tettra.site/canada-1/operations-manual-1#language-test-completion');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Language test completion','visaPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#language-test-completion');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Trusted Partner Referral interview','null','visaPreparation','https://tchelp.tettra.site/canada-1/operations-manual-1#trusted-partner-referral-interview');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Trusted Partner Referral interview','visaPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#trusted-partner-referral-interview');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa preparation','null','visaPreparation','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-preparation');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa preparation','visaPreparation','null','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-preparation');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa submission','null','visaProcessing','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-submission');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Approval in principle: Relocation planning with employer','null','relocating','https://tchelp.tettra.site/canada-1/operations-manual-1#approval-in-principle-relocation-planning-with-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Approval in principle: Relocation planning with employer','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#approval-in-principle-relocation-planning-with-employer');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa granted','null','relocating','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-granted');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Visa granted','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#visa-granted');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Flight and relocation arrangements','null','relocating','https://tchelp.tettra.site/canada-1/operations-manual-1#flight-and-relocation-arrangements');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Flight and relocation arrangements','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#flight-and-relocation-arrangements');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Departure email','null','relocating','https://tchelp.tettra.site/canada-1/operations-manual-1#departure-email');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Departure email','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#departure-email');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Arrival and orientation','null','relocated','https://tchelp.tettra.site/canada-1/operations-manual-1#arrival-and-orientation');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Arrival and orientation','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#arrival-and-orientation');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Monitoring and evaluation: Employer check-in','null','relocated','https://tchelp.tettra.site/canada-1/operations-manual-1#monitoring-and-evaluation-employer-check-in');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Monitoring and evaluation: Employer check-in','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#monitoring-and-evaluation-employer-check-in');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Update candidates information','null','relocated','https://tchelp.tettra.site/canada-1/operations-manual-1#update-candidates-information-1');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Update candidates information','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#update-candidates-information-1');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Monitoring and evaluation: Candidate check-in','null','relocated','https://tchelp.tettra.site/canada-1/operations-manual-1#monitoring-and-evaluation-candidate-check-in');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Monitoring and evaluation: Candidate check-in','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#monitoring-and-evaluation-candidate-check-in');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Monitoring and evaluation: Employer survey','null','relocated','https://tchelp.tettra.site/canada-1/operations-manual-1#monitoring-and-evaluation-employer-survey');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Monitoring and evaluation: Employer survey','postHireEngagement','null','https://tchelp.tettra.site/canada-1/operations-manual-1#monitoring-and-evaluation-employer-survey');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Hiring completed','null','settled','https://tchelp.tettra.site/canada-1/operations-manual-1#hiring-completed');
+INSERT INTO help_link(label,job_stage,case_stage,link) VALUES ('Hiring completed','hiringCompleted','null','https://tchelp.tettra.site/canada-1/operations-manual-1#hiring-completed');
