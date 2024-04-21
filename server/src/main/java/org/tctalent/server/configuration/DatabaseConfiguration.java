7cbf0b842787ce7f74e9df01add17cbbbfede3ed
andrewtodd <andrew@seraco.io>
2024-04-21 16:01:39 +1000

    Update naming strategy.

--- server/src/main/java/org/tctalent/server/configuration/DatabaseConfiguration.java
+++ server/src/main/java/org/tctalent/server/configuration/DatabaseConfiguration.java
@@ -16,17 +16,16 @@
 
 package org.tctalent.server.configuration;
 
+import com.zaxxer.hikari.HikariDataSource;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.sql.DataSource;
-
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy;
-import org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -35,66 +34,68 @@ import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import com.zaxxer.hikari.HikariDataSource;
-
 /**
- * Based on https://springframework.guru/how-to-configure-multiple-data-sources-in-a-spring-boot-application/
+ * Based on
+ * https://springframework.guru/how-to-configure-multiple-data-sources-in-a-spring-boot-application/
  *
  * @author John Cameron
  */
 @Configuration
 @EnableJpaRepositories(
-        basePackages = "org.tctalent.server.repository.db",
-        entityManagerFactoryRef = "dbEntityManagerFactory",
-        transactionManagerRef= "dbTransactionManager"
+    basePackages = "org.tctalent.server.repository.db",
+    entityManagerFactoryRef = "dbEntityManagerFactory",
+    transactionManagerRef = "dbTransactionManager"
 )
 public class DatabaseConfiguration {
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource")
-    public DataSourceProperties dbDataSourceProperties() {
-        return new DataSourceProperties();
-    }
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("spring.datasource.configuration")
-    public DataSource dbDataSource() {
-        return dbDataSourceProperties().initializeDataSourceBuilder()
-                .type(HikariDataSource.class).build();
-    }
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource")
+  public DataSourceProperties dbDataSourceProperties() {
+    return new DataSourceProperties();
+  }
 
-    @Primary
-    @Bean(name = "dbEntityManagerFactory")
-    public LocalContainerEntityManagerFactoryBean
-    dbEntityManagerFactory(EntityManagerFactoryBuilder builder) {
+  @Bean
+  @Primary
+  @ConfigurationProperties("spring.datasource.configuration")
+  public DataSource dbDataSource() {
+    return dbDataSourceProperties().initializeDataSourceBuilder()
+        .type(HikariDataSource.class).build();
+  }
 
-        //Note that this is necessary because with multiple data sources the
-        //default naming strategies are not picked up.
-        //If you don't do this database fields like first_name no longer map to
-        //entity names like firstName.
-        //See https://stackoverflow.com/questions/40509395/cant-set-jpa-naming-strategy-after-configuring-multiple-data-sources-spring-1
-        Map<String, String> jpaProperties = new HashMap<>();
-        jpaProperties.put("hibernate.physical_naming_strategy", SpringPhysicalNamingStrategy.class.getName());
-        jpaProperties.put("hibernate.implicit_naming_strategy", SpringImplicitNamingStrategy.class.getName());
+  @Primary
+  @Bean(name = "dbEntityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean
+  dbEntityManagerFactory(EntityManagerFactoryBuilder builder) {
 
-        return builder
-                .dataSource(dbDataSource())
-                .packages(
-                        "org.tctalent.server.service.db",
-                        "org.tctalent.server.model.db"
-                )
-                .properties(jpaProperties)
-                .build();
-    }
+    //Note that this is necessary because with multiple data sources the
+    //default naming strategies are not picked up.
+    //If you don't do this database fields like first_name no longer map to
+    //entity names like firstName.
+    //See https://stackoverflow.com/questions/40509395/cant-set-jpa-naming-strategy-after-configuring-multiple-data-sources-spring-1
+    Map<String, String> jpaProperties = new HashMap<>();
+    jpaProperties.put("hibernate.physical_naming_strategy",
+        CamelCaseToUnderscoresNamingStrategy.class.getName());
+    jpaProperties.put("hibernate.implicit_naming_strategy",
+        SpringImplicitNamingStrategy.class.getName());
 
-    @Primary
-    @Bean
-    public PlatformTransactionManager dbTransactionManager(
-            final @Qualifier("dbEntityManagerFactory")
-                    LocalContainerEntityManagerFactoryBean
-                    dbEntityManagerFactory) {
-        return new JpaTransactionManager(dbEntityManagerFactory.getObject());
-    }
+    return builder
+        .dataSource(dbDataSource())
+        .packages(
+            "org.tctalent.server.service.db",
+            "org.tctalent.server.model.db"
+        )
+        .properties(jpaProperties)
+        .build();
+  }
+
+  @Primary
+  @Bean
+  public PlatformTransactionManager dbTransactionManager(
+      final @Qualifier("dbEntityManagerFactory")
+      LocalContainerEntityManagerFactoryBean
+          dbEntityManagerFactory) {
+    return new JpaTransactionManager(dbEntityManagerFactory.getObject());
+  }
 
 }
