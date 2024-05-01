ef85f9d391a8a5fc5105deec820e89433767ad91
EhsanEhrari <ehrarie@gmail.com>
2024-05-01 13:45:28 +0430

    ViewJobDescriptionComponent
    should safely load job description URL into iframe [Done]

--- ui/admin-portal/src/app/components/job/view/description/view-job-description/view-job-description.component.spec.ts
+++ ui/admin-portal/src/app/components/job/view/description/view-job-description/view-job-description.component.spec.ts
@@ -1,15 +1,17 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {ViewJobDescriptionComponent} from './view-job-description.component';
-
-describe('ViewJobDescriptionComponent', () => {
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ViewJobDescriptionComponent } from './view-job-description.component';
+import { Job } from '../../../../../model/job';
+import {SafePipe} from "../../../../../pipes/safe.pipe";
+import {NgModule} from "@angular/core";
+import {BrowserDynamicTestingModule} from "@angular/platform-browser-dynamic/testing";
+ fdescribe('ViewJobDescriptionComponent', () => {
   let component: ViewJobDescriptionComponent;
   let fixture: ComponentFixture<ViewJobDescriptionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ViewJobDescriptionComponent ]
-    })
+      declarations: [ ViewJobDescriptionComponent,SafePipe ],
+     })
     .compileComponents();
   });
 
@@ -21,5 +23,39 @@ describe('ViewJobDescriptionComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should safely load job description URL into iframe', () => {
+    const job: Job = {
+      closed: false,
+      contactUser: undefined,
+      country: undefined,
+      employerEntity: undefined,
+      exclusionList: undefined,
+      hiringCommitment: "",
+      jobCreator: undefined,
+      jobOppIntake: undefined,
+      jobSummary: "",
+      name: "",
+      opportunityScore: "",
+      publishedBy: undefined,
+      publishedDate: undefined,
+      stage: undefined,
+      starringUsers: [],
+      submissionDueDate: undefined,
+      submissionList: undefined,
+      suggestedList: undefined,
+      suggestedSearches: [],
+      won: false
+      // mock job data
+    };
+
+    component.job = job;
+
+    fixture.detectChanges();
+
+    const iframeElement: HTMLIFrameElement = fixture.nativeElement.querySelector('iframe');
+    expect(iframeElement).toBeTruthy();
+    expect(iframeElement.src).toBe(component.jobDescription);
   });
 });
