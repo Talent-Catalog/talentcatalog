3b84aa7c9ecc9dab0c9fbd9088410bb6594b7c38
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-05-08 12:29:08 +1000

    Reverted changed imports

--- server/src/test/java/org/tctalent/server/service/db/impl/JobServiceHelperTest.java
+++ server/src/test/java/org/tctalent/server/service/db/impl/JobServiceHelperTest.java
@@ -16,10 +16,10 @@
 
 package org.tctalent.server.service.db.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class JobServiceHelperTest {
 
