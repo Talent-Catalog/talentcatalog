ddf9ec55e728fd71e3e43671acfc7a245f70b6e5
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-05-22 12:44:17 +1000

    Extend the new base class

--- ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/au/job/visa-job-check-au.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/au/job/visa-job-check-au.component.ts
@@ -1,65 +1,34 @@
-import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
-import {
-  calculateAge,
-  Candidate,
-  CandidateIntakeData,
-  CandidateVisa,
-  CandidateVisaJobCheck,
-  getIeltsScoreTypeString
-} from "../../../../../../../model/candidate";
+import {Component} from '@angular/core';
+import {calculateAge, getIeltsScoreTypeString} from "../../../../../../../model/candidate";
 import {OccupationService} from "../../../../../../../services/occupation.service";
 import {CandidateOccupationService} from "../../../../../../../services/candidate-occupation.service";
 import {CandidateOccupation} from "../../../../../../../model/candidate-occupation";
 import {CandidateEducationService} from "../../../../../../../services/candidate-education.service";
-import {CandidateEducation} from "../../../../../../../model/candidate-education";
-import {describeFamilyInDestination} from "../../../../../../../model/candidate-destination";
 import {Occupation} from "../../../../../../../model/occupation";
-import {NgbAccordion} from "@ng-bootstrap/ng-bootstrap";
+import {CandidateVisaJobService} from "../../../../../../../services/candidate-visa-job.service";
+import {VisaJobCheckBase} from "../../../../../../util/visa/visaJobCheckBase";
 
 @Component({
   selector: 'app-visa-job-check-au',
   templateUrl: './visa-job-check-au.component.html',
   styleUrls: ['./visa-job-check-au.component.scss']
 })
-export class VisaJobCheckAuComponent implements OnInit, AfterViewInit {
-  @Input() selectedJobCheck: CandidateVisaJobCheck;
-  @Input() candidate: Candidate;
-  @Input() candidateIntakeData: CandidateIntakeData;
-  @Input() visaCheckRecord: CandidateVisa;
-
-  @ViewChild('visaJobAus') visaJobAus: NgbAccordion;
-
-  candOccupations: CandidateOccupation[];
-  candQualifications: CandidateEducation[];
+export class VisaJobCheckAuComponent extends VisaJobCheckBase {
   occupations: Occupation[];
   yrsExp: CandidateOccupation;
-  familyInAus: string;
   candidateAge: number;
 
   error: string;
+  loading: boolean;
 
-  constructor(private candidateEducationService: CandidateEducationService,
-              private candidateOccupationService: CandidateOccupationService,
-              private occupationService: OccupationService) {}
+  constructor(candidateEducationService: CandidateEducationService,
+              candidateOccupationService: CandidateOccupationService,
+              candidateVisaJobService: CandidateVisaJobService,
+              private occupationService: OccupationService) {
+    super(candidateEducationService, candidateOccupationService, candidateVisaJobService);
+  }
 
   ngOnInit() {
-    // Get the candidate occupations
-    this.candidateOccupationService.get(this.candidate.id).subscribe(
-      (response) => {
-        this.candOccupations = response;
-      }, (error) => {
-        this.error = error;
-      }
-    )
-    // Get the candidate qualifications
-    this.candidateEducationService.list(this.candidate.id).subscribe(
-        (response) => {
-        this.candQualifications = response;
-      }, (error) => {
-        this.error = error;
-      }
-    )
-
     // Get the list of all occupations
     this.occupationService.listOccupations().subscribe(
       (results) => {
@@ -67,29 +36,8 @@ export class VisaJobCheckAuComponent implements OnInit, AfterViewInit {
       }
     )
 
-    this.familyInAus = describeFamilyInDestination(this.visaCheckRecord?.country.id, this.candidateIntakeData);
     const dobDate = new Date(this.candidate.dob);
     this.candidateAge = calculateAge(dobDate);
-  }
-
-  ngAfterViewInit() {
-    this.visaJobAus.expandAll();
-  }
-
-
-  get currentYear(): string {
-    return new Date().getFullYear().toString();
-  }
-
-  get birthYear(): string {
-    return this.candidate?.dob.toString().slice(0, 4);
-  }
-
-  get selectedOccupations(): CandidateOccupation {
-    if (this.candOccupations) {
-      this.yrsExp = this.candOccupations?.find(occ => occ.occupation.id === this.selectedJobCheck?.occupation?.id);
-      return this.yrsExp;
-    }
   }
 
   get ieltsScoreType(): string {
