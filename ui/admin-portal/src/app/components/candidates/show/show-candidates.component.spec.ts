31759eae96c1de20da12100066538a6dfce0ac7a
EhsanEhrari <ehrarie@gmail.com>
2024-05-22 14:31:47 +0430

    CandidateShowComponent

--- ui/admin-portal/src/app/components/candidates/show/show-candidates.component.spec.ts
+++ ui/admin-portal/src/app/components/candidates/show/show-candidates.component.spec.ts
@@ -14,29 +14,53 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { ShowCandidatesComponent } from "./show-candidates.component";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import {FormBuilder, FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {CommonModule, DatePipe, TitleCasePipe} from "@angular/common";
-import { Router } from "@angular/router";
-import { RouterTestingModule } from "@angular/router/testing";
-import {NgbModal, NgbPaginationModule, NgbTypeaheadModule} from "@ng-bootstrap/ng-bootstrap";
-import { LocalStorageModule, LocalStorageService } from "angular-2-local-storage";
-import { SavedSearchService } from "../../../services/saved-search.service";
+import {ShowCandidatesComponent} from "./show-candidates.component";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
 import {SortedByComponent} from "../../util/sort/sorted-by.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
+import {LocalStorageModule, LocalStorageService} from "angular-2-local-storage";
+import {
+  NgbModal,
+  NgbOffcanvas,
+  NgbPaginationModule,
+  NgbTypeaheadModule
+} from "@ng-bootstrap/ng-bootstrap";
+import {DatePipe, TitleCasePipe} from "@angular/common";
+import {CandidateService} from "../../../services/candidate.service";
 import {
   CandidateSourceDescriptionComponent
 } from "../../util/candidate-source-description/candidate-source-description.component";
 import {AutosaveStatusComponent} from "../../util/autosave-status/autosave-status.component";
+import {CandidateSourceService} from "../../../services/candidate-source.service";
+import {SavedSearchService} from "../../../services/saved-search.service";
+import {AuthorizationService} from "../../../services/authorization.service";
+import {AuthenticationService} from "../../../services/authentication.service";
+import {SalesforceService} from "../../../services/salesforce.service";
 import {MockCandidateSource} from "../../../MockData/MockCandidateSource";
-import {MockCandidate} from "../../../MockData/MockCandidate";
-import {MockSavedList} from "../../../MockData/MockSavedList";
+import {MockUser} from "../../../MockData/MockUser";
+import {SavedSearchType} from "../../../model/saved-search";
+import {of, throwError} from "rxjs";
 
 fdescribe('CandidateShowComponent', () => {
   let component: ShowCandidatesComponent;
   let fixture: ComponentFixture<ShowCandidatesComponent>;
   let formBuilder: FormBuilder;
+
+
+  // Create mock service instances using jasmine.createSpyObj
+  const mockCandidateService = jasmine.createSpyObj('CandidateService', ['someMethod']);
+  const mockCandidateSourceService = jasmine.createSpyObj('CandidateSourceService', ['someMethod']);
+  const mockSavedSearchService = jasmine.createSpyObj('SavedSearchService', ['clearSelection', 'doSearch','isSavedSearch']);
+  const mockLocalStorageService = jasmine.createSpyObj('LocalStorageService', ['get', 'set']);
+  const mockNgbModal = jasmine.createSpyObj('NgbModal', ['open']);
+  const mockNgbOffcanvas = jasmine.createSpyObj('NgbOffcanvas', ['open', 'dismiss']);
+  const mockAuthorizationService = jasmine.createSpyObj('AuthorizationService', ['canAssignTask','canAccessSalesforce','canPublishList','canUpdateSalesforce','canManageCandidateTasks']);
+  const mockAuthenticationService = jasmine.createSpyObj('AuthenticationService', ['getLoggedInUser']);
+  const mockSalesforceService = jasmine.createSpyObj('SalesforceService', ['joblink']);
+  const mockLocation = jasmine.createSpyObj('Location', ['back']);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ShowCandidatesComponent, SortedByComponent, AutosaveStatusComponent,CandidateSourceDescriptionComponent],
@@ -52,9 +76,16 @@ fdescribe('CandidateShowComponent', () => {
         FormBuilder,
         DatePipe,
         TitleCasePipe,
-        NgbModal,
-        SavedSearchService, // Add any other services that are directly injected into the component
-        LocalStorageService,
+        { provide: CandidateService, useValue: mockCandidateService },
+        { provide: CandidateSourceService, useValue: mockCandidateSourceService },
+        { provide: SavedSearchService, useValue: mockSavedSearchService },
+        { provide: LocalStorageService, useValue: mockLocalStorageService },
+        { provide: NgbModal, useValue: mockNgbModal },
+        { provide: NgbOffcanvas, useValue: mockNgbOffcanvas },
+        { provide: AuthorizationService, useValue: mockAuthorizationService },
+        { provide: AuthenticationService, useValue: mockAuthenticationService },
+        { provide: SalesforceService, useValue: mockSalesforceService },
+        { provide: Location, useValue: mockLocation }
       ]
     })
     .compileComponents();
@@ -66,10 +97,7 @@ fdescribe('CandidateShowComponent', () => {
     component = fixture.componentInstance;
     // Inject the FormBuilder
     formBuilder = TestBed.inject(FormBuilder);
-    // Mock candidate data
-    const mockCandidate = {
-      taskAssignments: [{ status: 'active' }, { status: 'completed' }] // Example task assignments
-    };
+
     // Initialize the formGroup
     component.searchForm = formBuilder.group({
       keyword: [''],
@@ -77,12 +105,33 @@ fdescribe('CandidateShowComponent', () => {
       statusesDisplay: [[]]
     });
     component.candidateSource =  new MockCandidateSource();
-    component.currentCandidate = new MockCandidate();
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should clear selection for saved search and perform search', () => {
+    component.loggedInUser = new MockUser();
 
+    const mockCandidateSource = new MockCandidateSource();
+    (mockCandidateSource as any).savedSearchType = SavedSearchType.other; // Add savedSearchType property dynamically
+    component.candidateSource = mockCandidateSource;
+    mockSavedSearchService.clearSelection.and.returnValue(of(true));
+    spyOn(component, 'doSearch');
+    component.clearSelection();
+
+    expect(mockSavedSearchService.clearSelection).toHaveBeenCalledWith(1, { userId: 1 });
+    expect(component.doSearch).toHaveBeenCalledWith(true);
+  });
+  it('should handle error when clearing selection for saved search', () => {
+    component.loggedInUser = new MockUser();
+    mockSavedSearchService.clearSelection.and.returnValue(throwError('error'));
+    const mockCandidateSource = new MockCandidateSource();
+    (mockCandidateSource as any).savedSearchType = SavedSearchType.other; // Add savedSearchType property dynamically
+    component.candidateSource = mockCandidateSource;
+    component.clearSelection();
+    expect(mockSavedSearchService.clearSelection).toHaveBeenCalledWith(1, { userId: 1 });
+    expect(component.error).toBe('error');
+  });
 });
