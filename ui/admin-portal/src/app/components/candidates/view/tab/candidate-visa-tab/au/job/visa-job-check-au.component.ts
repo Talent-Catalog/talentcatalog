48d5851b1a953dea3becde7912a5facc7bd902f1
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-05-27 12:23:19 +1000

    Undo base class as it was causing errors in the build (inputs not being found)

--- ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/au/job/visa-job-check-au.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/au/job/visa-job-check-au.component.ts
@@ -1,33 +1,65 @@
-import {Component} from '@angular/core';
-import {calculateAge, getIeltsScoreTypeString} from "../../../../../../../model/candidate";
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {
+  calculateAge,
+  Candidate,
+  CandidateIntakeData,
+  CandidateVisa,
+  CandidateVisaJobCheck,
+  getIeltsScoreTypeString
+} from "../../../../../../../model/candidate";
 import {OccupationService} from "../../../../../../../services/occupation.service";
 import {CandidateOccupationService} from "../../../../../../../services/candidate-occupation.service";
 import {CandidateOccupation} from "../../../../../../../model/candidate-occupation";
 import {CandidateEducationService} from "../../../../../../../services/candidate-education.service";
+import {CandidateEducation} from "../../../../../../../model/candidate-education";
+import {describeFamilyInDestination} from "../../../../../../../model/candidate-destination";
 import {Occupation} from "../../../../../../../model/occupation";
-import {VisaJobCheckBase} from "../../../../../../util/visa/visaJobCheckBase";
+import {NgbAccordion} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-visa-job-check-au',
   templateUrl: './visa-job-check-au.component.html',
   styleUrls: ['./visa-job-check-au.component.scss']
 })
-export class VisaJobCheckAuComponent extends VisaJobCheckBase {
+export class VisaJobCheckAuComponent implements OnInit, AfterViewInit {
+  @Input() selectedJobCheck: CandidateVisaJobCheck;
+  @Input() candidate: Candidate;
+  @Input() candidateIntakeData: CandidateIntakeData;
+  @Input() visaCheckRecord: CandidateVisa;
+
+  @ViewChild('visaJobAus') visaJobAus: NgbAccordion;
+
+  candOccupations: CandidateOccupation[];
+  candQualifications: CandidateEducation[];
   occupations: Occupation[];
   yrsExp: CandidateOccupation;
+  familyInAus: string;
   candidateAge: number;
 
   error: string;
-  loading: boolean;
 
-  constructor(candidateEducationService: CandidateEducationService,
-              candidateOccupationService: CandidateOccupationService,
-              private occupationService: OccupationService) {
-    super(candidateEducationService, candidateOccupationService);
-  }
+  constructor(private candidateEducationService: CandidateEducationService,
+              private candidateOccupationService: CandidateOccupationService,
+              private occupationService: OccupationService) {}
 
   ngOnInit() {
-    super.ngOnInit();
+    // Get the candidate occupations
+    this.candidateOccupationService.get(this.candidate.id).subscribe(
+      (response) => {
+        this.candOccupations = response;
+      }, (error) => {
+        this.error = error;
+      }
+    )
+    // Get the candidate qualifications
+    this.candidateEducationService.list(this.candidate.id).subscribe(
+        (response) => {
+        this.candQualifications = response;
+      }, (error) => {
+        this.error = error;
+      }
+    )
+
     // Get the list of all occupations
     this.occupationService.listOccupations().subscribe(
       (results) => {
@@ -35,8 +67,29 @@ export class VisaJobCheckAuComponent extends VisaJobCheckBase {
       }
     )
 
+    this.familyInAus = describeFamilyInDestination(this.visaCheckRecord?.country.id, this.candidateIntakeData);
     const dobDate = new Date(this.candidate.dob);
     this.candidateAge = calculateAge(dobDate);
+  }
+
+  ngAfterViewInit() {
+    this.visaJobAus.expandAll();
+  }
+
+
+  get currentYear(): string {
+    return new Date().getFullYear().toString();
+  }
+
+  get birthYear(): string {
+    return this.candidate?.dob.toString().slice(0, 4);
+  }
+
+  get selectedOccupations(): CandidateOccupation {
+    if (this.candOccupations) {
+      this.yrsExp = this.candOccupations?.find(occ => occ.occupation.id === this.selectedJobCheck?.occupation?.id);
+      return this.yrsExp;
+    }
   }
 
   get ieltsScoreType(): string {
--- ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/ca/job/visa-job-check-ca.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/ca/job/visa-job-check-ca.component.ts
@@ -1,28 +1,92 @@
-import {Component} from '@angular/core';
-import {IeltsStatus} from "../../../../../../../model/candidate";
-import {VisaJobCheckBase} from "../../../../../../util/visa/visaJobCheckBase";
+import {AfterViewInit, Component, Input, OnInit, ViewChild} from '@angular/core';
+import {
+  Candidate,
+  CandidateIntakeData,
+  CandidateVisa,
+  CandidateVisaJobCheck,
+  getDestinationPathwayInfoLink,
+  IeltsStatus
+} from "../../../../../../../model/candidate";
+import {describeFamilyInDestination} from "../../../../../../../model/candidate-destination";
+import {CandidateEducationService} from "../../../../../../../services/candidate-education.service";
+import {CandidateOccupationService} from "../../../../../../../services/candidate-occupation.service";
+import {CandidateOccupation} from "../../../../../../../model/candidate-occupation";
+import {CandidateEducation} from "../../../../../../../model/candidate-education";
+import {NgbAccordion} from "@ng-bootstrap/ng-bootstrap";
+import {CandidateVisaJobService} from "../../../../../../../services/candidate-visa-job.service";
 
 @Component({
   selector: 'app-visa-job-check-ca',
   templateUrl: './visa-job-check-ca.component.html',
   styleUrls: ['./visa-job-check-ca.component.scss']
 })
-export class VisaJobCheckCaComponent extends VisaJobCheckBase {
+export class VisaJobCheckCaComponent implements OnInit, AfterViewInit {
+  @Input() selectedJobCheck: CandidateVisaJobCheck;
+  @Input() candidate: Candidate;
+  @Input() candidateIntakeData: CandidateIntakeData;
+  @Input() visaCheckRecord: CandidateVisa;
+
+  @ViewChild('visaJobCanada') visaJobCanada: NgbAccordion;
+
+  candOccupations: CandidateOccupation[];
+  candQualifications: CandidateEducation[];
+
+  familyInCanada: string;
   partnerIeltsString: string;
   pathwaysInfoLink: string;
 
   error: string;
   loading: boolean;
 
+  constructor(private candidateEducationService: CandidateEducationService,
+              private candidateOccupationService: CandidateOccupationService,
+              private candidateVisaJobService: CandidateVisaJobService) {}
+
   ngOnInit(): void {
-    super.ngOnInit();
-    // Set the partner IELTS score
+    // Get the candidate occupations
+    this.candidateOccupationService.get(this.candidate.id).subscribe(
+      (response) => {
+        this.candOccupations = response;
+      }, (error) => {
+        this.error = error;
+      }
+    )
+    // Get the candidate qualifications
+    this.candidateEducationService.list(this.candidate.id).subscribe(
+      (response) => {
+        this.candQualifications = response;
+      }, (error) => {
+        this.error = error;
+      }
+    )
+
+    // Process & fetch values that need to be displayed.
+    this.familyInCanada = describeFamilyInDestination(this.visaCheckRecord?.country.id, this.candidateIntakeData);
     if (this.candidateIntakeData?.partnerIelts) {
       this.partnerIeltsString = IeltsStatus[this.candidateIntakeData?.partnerIelts] +
         (this.candidateIntakeData?.partnerIeltsScore ? ', Score: ' + this.candidateIntakeData.partnerIeltsScore : null);
     } else {
       this.partnerIeltsString = null;
     }
+    this.pathwaysInfoLink = getDestinationPathwayInfoLink(this.visaCheckRecord.country.id);
+  }
+
+  ngAfterViewInit() {
+    this.visaJobCanada.expandAll();
+  }
+
+  requestSfCaseRelocationInfoUpdate() {
+    this.error = null;
+    this.loading = true;
+    this.candidateVisaJobService.updateSfCaseRelocationInfo(
+        this.selectedJobCheck.id).subscribe(
+        boolean => {
+          this.loading = false;
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        });
   }
 }
 
