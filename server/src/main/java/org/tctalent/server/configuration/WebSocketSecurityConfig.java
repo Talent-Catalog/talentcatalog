efe9473759e398371e771b2270baf0b45ab67447
seraco.io <code@seraco.io>
2024-05-28 15:48:52 +1000

    Fix format after IntelliJ update and lost setting.

--- server/src/main/java/org/tctalent/server/configuration/WebSocketSecurityConfig.java
+++ server/src/main/java/org/tctalent/server/configuration/WebSocketSecurityConfig.java
@@ -16,14 +16,16 @@
 
 package org.tctalent.server.configuration;
 
+import static org.springframework.messaging.simp.SimpMessageType.CONNECT;
+import static org.springframework.messaging.simp.SimpMessageType.DISCONNECT;
+import static org.springframework.messaging.simp.SimpMessageType.UNSUBSCRIBE;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
 import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
-
-import static org.springframework.messaging.simp.SimpMessageType.*;
 
 /**
  * Websocket security set up.
