00250c8c922470d8006bc51a369876c41cb6a3f3
EhsanEhrari <ehrarie@gmail.com>
2024-05-28 10:32:44 +0430

    VisaRejectComponent

--- ui/admin-portal/src/app/components/candidates/intake/visa-reject/visa-reject.component.spec.ts
+++ ui/admin-portal/src/app/components/candidates/intake/visa-reject/visa-reject.component.spec.ts
@@ -13,29 +13,77 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
-
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {NgSelectModule} from '@ng-select/ng-select';
 import {VisaRejectComponent} from './visa-reject.component';
+import {CandidateService} from '../../../../services/candidate.service';
+import {YesNoUnsure} from '../../../../model/candidate';
+import {of} from 'rxjs';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {AutosaveStatusComponent} from "../../../util/autosave-status/autosave-status.component";
 
 describe('VisaRejectComponent', () => {
   let component: VisaRejectComponent;
   let fixture: ComponentFixture<VisaRejectComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ VisaRejectComponent ]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ VisaRejectComponent, AutosaveStatusComponent ],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, NgSelectModule ],
+      providers: [
+        { provide: CandidateService }
+      ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(VisaRejectComponent);
     component = fixture.componentInstance;
+    component.candidateIntakeData = {
+      visaReject: YesNoUnsure.Yes,
+      visaRejectNotes: 'Rejected due to incomplete documentation'
+    };
+    component.editable = true;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should initialize the form controls with the correct default values', () => {
+    expect(component.form.get('visaReject').value).toBe(YesNoUnsure.Yes);
+    expect(component.form.get('visaRejectNotes').value).toBe('Rejected due to incomplete documentation');
+  });
+
+  it('should enable the form controls when editable is true', () => {
+    expect(component.form.get('visaReject').enabled).toBeTrue();
+    expect(component.form.get('visaRejectNotes').enabled).toBeTrue();
+  });
+
+  it('should disable the form controls when editable is false', () => {
+    component.editable = false;
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    expect(component.form.get('visaReject').disabled).toBeTrue();
+    expect(component.form.get('visaRejectNotes').disabled).toBeTrue();
+  });
+
+  it('should display visaRejectNotes field when visaReject is Yes or Unsure', () => {
+    component.form.get('visaReject').setValue('Yes');
+    fixture.detectChanges();
+    let notesField = fixture.nativeElement.querySelector('#visaRejectNotes');
+    expect(notesField).not.toBeNull();
+
+    component.form.get('visaReject').setValue('Unsure');
+    fixture.detectChanges();
+    notesField = fixture.nativeElement.querySelector('#visaRejectNotes');
+    expect(notesField).not.toBeNull();
+  });
+
+  it('should hide visaRejectNotes field when visaReject is No', () => {
+    component.form.get('visaReject').setValue('No');
+    fixture.detectChanges();
+    const notesField = fixture.nativeElement.querySelector('#visaRejectNotes');
+    expect(notesField).toBeNull();
   });
 });
