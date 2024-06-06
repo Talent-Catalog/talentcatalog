adb0c47da91be7f141d40688d608352ac9ae1a11
EhsanEhrari <ehrarie@gmail.com>
2024-06-06 10:48:23 +0430

    RiskAssessmentComponent

--- ui/admin-portal/src/app/components/candidates/visa/risk-assessment/risk-assessment.component.spec.ts
+++ ui/admin-portal/src/app/components/candidates/visa/risk-assessment/risk-assessment.component.spec.ts
@@ -1,17 +1,42 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {RiskAssessmentComponent} from './risk-assessment.component';
-
-describe('RiskAssessmentComponent', () => {
+/*
+ * Copyright (c) 2024 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { RiskAssessmentComponent } from './risk-assessment.component';
+import { CandidateVisaCheckService } from '../../../../services/candidate-visa-check.service';
+import {AutosaveStatusComponent} from "../../../util/autosave-status/autosave-status.component";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {NgSelectModule} from "@ng-select/ng-select";
+fdescribe('RiskAssessmentComponent', () => {
   let component: RiskAssessmentComponent;
   let fixture: ComponentFixture<RiskAssessmentComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ RiskAssessmentComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule,ReactiveFormsModule, NgSelectModule],
+      declarations: [RiskAssessmentComponent,AutosaveStatusComponent],
+      providers: [
+        FormBuilder,
+        {
+          provide: CandidateVisaCheckService,
+          useValue: jasmine.createSpyObj('CandidateVisaCheckService', ['someMethod'])
+        }
+      ]
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RiskAssessmentComponent);
@@ -21,5 +46,10 @@ describe('RiskAssessmentComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('hasNotes should return true when visaOverallRisk is "High"', () => {
+    component.form.controls['visaOverallRisk'].setValue('High');
+    expect(component.hasNotes).toBeTrue();
   });
 });
