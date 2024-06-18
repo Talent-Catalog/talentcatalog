b5de66c1d37c994091c992a480d984141265d30b
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-06-18 11:42:12 +1000

    Create intake service to handle form stepper process

---
+++ ui/candidate-portal/src/app/services/intake.service.ts
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2024 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {Injectable} from '@angular/core';
+import {RegistrationStep} from '../components/register/registration-step';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IntakeService {
+
+  private subscription: Subscription;
+
+  public steps: RegistrationStep[] = [
+    {
+      key: 'account',
+      title: 'Welcome to Talent Catalog!',
+      section: 0
+    },
+    {
+      key: 'intRecruitment',
+      title: 'Interest in International Recruitment',
+      section: 1
+    },
+    {
+      key: 'destinations',
+      title: 'Destinations',
+      section: 2
+    },
+    {
+      key: 'occupation',
+      title: 'Tell us about your occupation',
+      section: 3
+    },
+    {
+      key: 'experience',
+      title: 'Tell us about your working history',
+      section: 4
+    },
+    {
+      key: 'education',
+      title: 'Tell us about your education',
+      section: 5
+    },
+    {
+      key: 'language',
+      title: 'What languages do you speak?',
+      section: 6
+    },
+    {
+      key: 'certifications',
+      title: 'Do you have any other professional certifications?',
+      section: 7
+    },
+    {
+      key: 'additional',
+      title: 'How did you hear about us?',
+      section: 8
+    },
+    {
+      key: 'upload',
+      title: 'Do you have any files to upload?',
+      section: 9
+    },
+    {
+      key: 'complete',
+      title: '',
+      hideHeader: true,
+      section: 9
+    }
+  ];
+  public totalSections: number = Math.max(...this.steps.map(s => s.section));
+  public currentStepKey: string;
+  public currentStep: RegistrationStep;
+  public currentStepIndex: number;
+  registering: boolean = false;
+
+  constructor(private router: Router,
+              private route: ActivatedRoute) { }
+
+  // Observe the query params in the url to determine which step to display
+  start() {
+    // Set step back to 0 before starting a new registration.
+    this.currentStepIndex = 0;
+    this.currentStep = this.steps[this.currentStepIndex];
+    this.currentStepKey = this.currentStep.key;
+    if (!this.subscription || this.subscription.closed) {
+      this.registering = true;
+      this.subscription = this.route.queryParams.subscribe(
+        params => {
+          if (params['step']) {
+            this.openStep(params['step']);
+          } else {
+            this.routeToStep('landing');
+          }
+        }
+      );
+    }
+  }
+
+  // Stop observing the url changes
+  stop() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+      this.registering = false;
+    }
+  }
+
+  openStep(stepKey: string) {
+    stepKey = stepKey || 'landing';
+    this.currentStepIndex = this.steps.findIndex(step => step.key === stepKey);
+    this.setStep();
+  }
+
+  back() {
+    if (!this.registering) {return;}
+    this.currentStepIndex--;
+    this.setStep();
+  }
+
+  next() {
+    if (!this.registering) {return;}
+    this.currentStepIndex++;
+    this.setStep();
+  }
+
+  setStep() {
+    if (this.currentStepIndex < 1 || this.currentStepIndex > this.steps.length - 1) {
+      this.currentStepIndex = 0;
+    }
+    this.currentStep = this.steps[this.currentStepIndex];
+    this.currentStepKey = this.currentStep.key;
+    this.routeToStep(this.currentStepKey);
+  }
+
+  routeToStep(key: string) {
+    this.router.navigate([], {queryParams: {step: key}, queryParamsHandling: "merge"});
+  }
+}
