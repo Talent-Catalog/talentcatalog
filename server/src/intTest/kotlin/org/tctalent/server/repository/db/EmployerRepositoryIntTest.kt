d81dc47d059c85ec70646a9b88e424f1939c7efc
seraco.io <code@seraco.io>
2024-06-18 17:08:46 +1000

    TEST: New test.

---
+++ server/src/intTest/kotlin/org/tctalent/server/repository/db/EmployerRepositoryIntTest.kt
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2024 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.repository.db
+
+import kotlin.jvm.optionals.getOrNull
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+import org.springframework.beans.factory.annotation.Autowired
+import org.tctalent.server.model.db.Employer
+import org.tctalent.server.repository.db.integrationhelp.BaseDBIntegrationTest
+import kotlin.test.assertNull
+
+class EmployerRepositoryIntTest : BaseDBIntegrationTest() {
+  @Autowired lateinit var repository: EmployerRepository
+  private val testDescription = "The description"
+  private val salesforceId = "salesforceId"
+
+  @Test
+  fun `find first by sf id`() {
+    assertTrue { isContainerInitialized() }
+
+    val employer = Employer().apply {
+      description = testDescription
+      sfId = salesforceId
+    }
+    repository.save(employer)
+    assertTrue { employer.id > 0 }
+
+    val savedEmployee = repository.findFirstBySfId(salesforceId).getOrNull()
+    assertNotNull(savedEmployee)
+    assertTrue { savedEmployee.description == testDescription }
+  }
+
+  @Test
+  fun `find by sf id fail`() {
+    assertTrue { isContainerInitialized() }
+
+    val employer = Employer().apply { description = testDescription }
+    repository.save(employer)
+    assertTrue { employer.id > 0 }
+
+    val savedEmployee = repository.findFirstBySfId(salesforceId + "00").getOrNull()
+    assertNull(savedEmployee)
+  }
+}
