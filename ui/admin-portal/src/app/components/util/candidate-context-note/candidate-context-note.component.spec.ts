6c99a6bfb3bc1a0a2de7d71bdb4d1578511fba79
EhsanEhrari <ehrarie@gmail.com>
2024-06-27 09:25:06 +0430

    CandidateContextNoteComponent

--- ui/admin-portal/src/app/components/util/candidate-context-note/candidate-context-note.component.spec.ts
+++ ui/admin-portal/src/app/components/util/candidate-context-note/candidate-context-note.component.spec.ts
@@ -13,29 +13,87 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
+import {CandidateContextNoteComponent} from "./candidate-context-note.component";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {ReactiveFormsModule} from "@angular/forms";
+import {CandidateSourceService} from "../../../services/candidate-source.service";
+import {MockCandidate} from "../../../MockData/MockCandidate";
+import {MockCandidateSource} from "../../../MockData/MockCandidateSource";
+import {By} from "@angular/platform-browser";
+import {AutosaveStatusComponent} from "../autosave-status/autosave-status.component";
+import {of} from "rxjs";
 
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-
-import {CandidateContextNoteComponent} from './candidate-context-note.component';
-
-describe('CandidateContextNoteComponent', () => {
+fdescribe('CandidateContextNoteComponent', () => {
   let component: CandidateContextNoteComponent;
   let fixture: ComponentFixture<CandidateContextNoteComponent>;
+  let candidateSourceService: jasmine.SpyObj<CandidateSourceService>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CandidateContextNoteComponent ]
-    })
-    .compileComponents();
-  }));
+  const mockCandidate = new MockCandidate();
+  const mockCandidateSource = new MockCandidateSource();
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    const candidateSourceSpy = jasmine.createSpyObj('CandidateSourceService', ['updateContextNote']);
+
+    await TestBed.configureTestingModule({
+      declarations: [CandidateContextNoteComponent, AutosaveStatusComponent],
+      imports: [ReactiveFormsModule],
+      providers: [
+        { provide: CandidateSourceService, useValue: candidateSourceSpy }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CandidateContextNoteComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    candidateSourceService = TestBed.inject(CandidateSourceService) as jasmine.SpyObj<CandidateSourceService>;
   });
 
+  beforeEach(() => {
+    component.candidate = mockCandidate;
+    component.candidateSource = mockCandidateSource;
+    candidateSourceService.updateContextNote.and.returnValue(of());
+    fixture.detectChanges();
+  });
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize form with candidate context note', () => {
+    expect(component.form.value.contextNote).toBe('');
+  });
+
+  it('should update form value when candidate input changes', () => {
+    component.candidate = { ...mockCandidate, contextNote: 'Updated context note' };
+    component.ngOnChanges({
+      candidate: {
+        currentValue: component.candidate,
+        previousValue: mockCandidate,
+        firstChange: false,
+        isFirstChange: () => false
+      }
+    });
+    fixture.detectChanges();
+    expect(component.form.value.contextNote).toBe('Updated context note');
+  });
+
+  it('should call updateContextNote on save', () => {
+    component.form.controls['contextNote'].setValue('New context note');
+    component.doSave(component.form.value).subscribe();
+    expect(candidateSourceService.updateContextNote).toHaveBeenCalledWith(
+      mockCandidateSource, { candidateId: 1, contextNote: 'New context note' }
+    );
+  });
+
+  it('should update candidate context note on successful save', () => {
+    component.form.controls['contextNote'].setValue('New context note');
+    component.doSave(component.form.value);
+    component.onSuccessfulSave();
+    expect(component.candidate.contextNote).toBe('New context note');
+  });
+
+  it('should display error message when there is an error', () => {
+    component.error = 'An error occurred';
+    fixture.detectChanges();
+    const errorMsg = fixture.debugElement.query(By.css('.alert-danger')).nativeElement;
+    expect(errorMsg.textContent).toContain('An error occurred');
   });
 });
