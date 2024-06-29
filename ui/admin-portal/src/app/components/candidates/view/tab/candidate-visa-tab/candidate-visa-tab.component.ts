c9d7c8bd22a8aa9b3c0e29d54c5541188e756937
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-06-29 11:43:21 +1000

    Added UK to notes

--- ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/candidate-visa-tab.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/tab/candidate-visa-tab/candidate-visa-tab.component.ts
@@ -71,7 +71,7 @@ export class CandidateVisaTabComponent implements OnInit {
     this.countryService.listTBBDestinations().subscribe((results) => {
       /**
        * todo: Remove/alter this filter once no longer needed or find other solution.
-       * It is a temporary filter to only display the TBB destinations (Australia & Canada) that have functioning visa checks.
+       * It is a temporary filter to only display the TBB destinations (Australia, Canada & UK) that have functioning visa checks.
        */
       this.tbbDestinations = results.filter(c => c.id == 6191 || c.id == 6216 || c.id == 6179);
     })
