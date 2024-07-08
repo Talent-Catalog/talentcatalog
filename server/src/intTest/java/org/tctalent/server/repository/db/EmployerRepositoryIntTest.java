ac4e5fc255df6510b2431620073f24de171a18fe
andrewtodd <andrew@seraco.io>
2024-07-08 20:09:24 +1000

    New test suite.

--- server/src/intTest/java/org/tctalent/server/repository/db/EmployerRepositoryIntTest.java
+++ server/src/intTest/java/org/tctalent/server/repository/db/EmployerRepositoryIntTest.java
@@ -19,7 +19,6 @@ package org.tctalent.server.repository.db;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
