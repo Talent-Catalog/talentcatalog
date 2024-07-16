3fb9ebfde837dd9ae217cb84cabdc11c93e1f986
EhsanEhrari <ehrarie@gmail.com>
2024-07-16 12:16:07 +0430

    Create job-prep-item.spec.ts

---
+++ ui/admin-portal/src/app/model/job-prep-item.spec.ts
@@ -0,0 +1,127 @@
+/*
+ * Copyright (c) 2024 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {
+  JobPrepDueDate,
+  JobPrepJD,
+  JobPrepJobSummary,
+  JobPrepJOI,
+  JobPrepSuggestedCandidates,
+  JobPrepSuggestedSearches
+} from "./job-prep-item";
+import {Job} from "./job";
+import {MockJob} from "../MockData/MockJob";
+
+fdescribe('Job Preparation Items', () => {
+  let job: Job;
+
+  beforeEach(() => {
+    job = MockJob;
+  });
+
+  describe('JobPrepDueDate', () => {
+    let item: JobPrepDueDate;
+
+    beforeEach(() => {
+      item = new JobPrepDueDate();
+      item.job = job;
+    });
+
+    it('should return true if submission due date is set', () => {
+      expect(item.isCompleted()).toBe(true);
+    });
+  });
+
+  describe('JobPrepJD', () => {
+    let item: JobPrepJD;
+
+    beforeEach(() => {
+      item = new JobPrepJD();
+      item.job = job;
+    });
+
+    it('should return true if job description (JD) is provided', () => {
+      expect(item.isCompleted()).toBe(true);
+    });
+
+    it('should return false if job description (JD) link is not provided', () => {
+      job.submissionList.fileJdLink = '';
+      expect(item.isCompleted()).toBe(false);
+    });
+  });
+
+  describe('JobPrepJobSummary', () => {
+    let item: JobPrepJobSummary;
+
+    beforeEach(() => {
+      item = new JobPrepJobSummary();
+      item.job = job;
+    });
+
+    it('should return true if job summary is provided', () => {
+      expect(item.isCompleted()).toBe(true);
+    });
+
+    it('should return false if job summary is not provided', () => {
+      job.jobSummary = '';
+      expect(item.isCompleted()).toBe(false);
+    });
+  });
+
+  describe('JobPrepJOI', () => {
+    let item: JobPrepJOI;
+
+    beforeEach(() => {
+      item = new JobPrepJOI();
+      item.job = job;
+    });
+
+    it('should return true if job opportunity intake (JOI) is provided', () => {
+      expect(item.isCompleted()).toBe(true);
+    });
+
+    it('should return false if employer cost commitment is not provided', () => {
+      job.jobOppIntake.employerCostCommitment = '';
+      expect(item.isCompleted()).toBe(false);
+    });
+  });
+
+  describe('JobPrepSuggestedCandidates', () => {
+    let item: JobPrepSuggestedCandidates;
+
+    beforeEach(() => {
+      item = new JobPrepSuggestedCandidates();
+    });
+
+    it('should return false by default (empty is true)', () => {
+      expect(item.isCompleted()).toBe(false);
+    });
+  });
+  
+  describe('JobPrepSuggestedSearches', () => {
+    let item: JobPrepSuggestedSearches;
+
+    beforeEach(() => {
+      item = new JobPrepSuggestedSearches();
+      item.job = job;
+    });
+
+    it('should return false if suggested searches are not provided', () => {
+      job.suggestedSearches = [];
+      expect(item.isCompleted()).toBe(false);
+    });
+  });
+});
