58a20f1545b37bb37a1f45d53ef214cf59ed7b48
John Cameron <john@cameronfoundation.org>
2024-07-19 13:42:48 +1000

    Structure standard query method pattern.

--- server/src/main/java/org/tctalent/server/service/db/impl/CandidateStatsServiceImpl.java
+++ server/src/main/java/org/tctalent/server/service/db/impl/CandidateStatsServiceImpl.java
@@ -25,99 +25,138 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
+import org.tctalent.server.logging.LogBuilder;
 import org.tctalent.server.model.db.DataRow;
 import org.tctalent.server.model.db.Gender;
 import org.tctalent.server.service.db.CandidateStatsService;
 
+/**
+ * Methods performing our standard candidate stats.
+ * <p/>
+ * Note that I have been forced to go to native queries for these more complex queries.
+ * The non-native queries seem a bit buggy.
+ * Anyway - I couldn't get them working. Simpler to use normal SQL. JC.
+ */
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class CandidateStatsServiceImpl implements CandidateStatsService {
-    
+
     @PersistenceContext
     private EntityManager entityManager;
-    
+
+    //Standard SQL building blocks for our stats.
+    private static final String candidatesCondition =
+        " and candidate.id in (:candidateIds)";
+    private static final String sourceCountriesCondition =
+        " and candidate.country_id in (:sourceCountryIds)";
+    private static final String notTestCandidateCondition =
+        " and candidate.id NOT IN (select candidate_id from candidate_saved_list" +
+            " where saved_list_id = "
+            + "(select id from saved_list where name = 'TestCandidates' and global = true))";
+    private static final String countingStandardFilter =
+        " users.status = 'active' and candidate.status != 'draft'" + notTestCandidateCondition;
+    private static final String dateConditionFilter =
+        " and users.created_date >= (:dateFrom) and users.created_date <= (:dateTo)";
+    private static final String excludeIneligible = " and candidate.status != 'ineligible'";
+
     @Override
     public List<DataRow> computeBirthYearStats(
         @Nullable Gender gender, @Nullable LocalDate dateFrom, @Nullable LocalDate dateTo,
-        @Nullable Set<Long> candidateIds, @Nullable List<Long> sourceCountryIds, 
+        @Nullable Set<Long> candidateIds, @Nullable List<Long> sourceCountryIds,
         @Nullable String constraintPredicate) {
 
-        //Default null dates
-        if (dateFrom == null) {
-            dateFrom = LocalDate.parse("2000-01-01");
-        }
-        if (dateTo == null) {
-            dateTo = LocalDate.now();
-        }
-
-        String candidatesCondition = " and candidate.id in (:candidateIds)";
-        String sourceCountriesCondition = " and candidate.country_id in (:sourceCountryIds)";
-        String notTestCandidateCondition =
-            " and candidate.id NOT IN (select candidate_id from candidate_saved_list" +
-                " where saved_list_id = (select id from saved_list where name = 'TestCandidates' and global = true))";
-        String countingStandardFilter =
-            " users.status = 'active' and candidate.status != 'draft'" + notTestCandidateCondition;
-        String dateConditionFilter = " and users.created_date >= (:dateFrom) and users.created_date <= (:dateTo)";
-
-        String excludeIneligible = " and candidate.status != 'ineligible'";
-
-        //Note that I have been forced to go to native queries for these more
-        //complex queries. The non-native queries seem a bit buggy.
-        //Anyway - I couldn't get them working. Simpler to use normal SQL. JC.
-        
         String countByBirthYearSelectSQL =
     """
             select cast(extract(year from dob) as bigint) as year, count(distinct candidate) as PeopleCount
                  from candidate left join users on candidate.user_id = users.id
                  where gender like :gender
     """
-        //Ignore null birthdate and birth year that is not plausible  
-        + " and dob is not null and extract(year from dob) > 1940 and"
-        + countingStandardFilter + dateConditionFilter;
-        if (sourceCountryIds != null && !sourceCountryIds.isEmpty()) {
-            countByBirthYearSelectSQL += sourceCountriesCondition;
-        }
-        if (constraintPredicate != null) {
-            countByBirthYearSelectSQL += " and " + constraintPredicate;
-        }
-        if (candidateIds != null) {
-            countByBirthYearSelectSQL += candidatesCondition;
-        }
+        //Ignore null birthdate and only look at plausible birth years
+        + " and dob is not null and extract(year from dob) > 1940 and";
 
-        //Stats that are not based on predefined candidate ids or constraintPredicate, 
-        // should exclude ineligible.
-        //(With candidate ids, it is up to the associated list or search to decide whether to
-        // exclude ineligible)
-        if (candidateIds == null && constraintPredicate == null) {
-            countByBirthYearSelectSQL += excludeIneligible;
-        }
+        countByBirthYearSelectSQL +=
+            standardConstraints(candidateIds, sourceCountryIds, constraintPredicate);
+
         String countByBirthYearGroupBySQL = " group by year order by year asc";
         String sql = countByBirthYearSelectSQL + countByBirthYearGroupBySQL;
-        Query query = entityManager.createNativeQuery(sql);
-        
-        
-        query.setParameter("gender", genderStr(gender));
-        query.setParameter("dateFrom", dateFrom);
-        query.setParameter("dateTo", dateTo);
-        if (sourceCountryIds != null && !sourceCountryIds.isEmpty()) {
-            query.setParameter("sourceCountryIds", sourceCountryIds);
-        }
-        if (candidateIds != null) {
-            query.setParameter("candidateIds", candidateIds);
-        }
-        final List resultList = query.getResultList();
 
-        return toRows(resultList);
+        LogBuilder.builder(log).action("computeBirthYearStats")
+            .message("Query: " + sql).logInfo();
+
+        Query query = entityManager.createNativeQuery(sql);
+
+        query.setParameter("gender", genderStr(gender));
+
+        setStandardQueryParameters(query,
+            dateFrom, dateTo, candidateIds, sourceCountryIds);
+
+        return runQuery(query);
     }
 
     private static String countryStr(String country) {
         return country == null ? "%" : country;
     }
 
+    private static LocalDate defaultDateFrom(LocalDate dateFrom) {
+       return dateFrom == null ? LocalDate.parse("2000-01-01") : dateFrom;
+    }
+
+    private static LocalDate defaultDateTo(LocalDate dateTo) {
+        return dateTo == null ? LocalDate.now() : dateTo;
+    }
+
     private static String genderStr(Gender gender) {
         return gender == null ? "%" : gender.toString();
+    }
+
+    private static List<DataRow> runQuery(Query query) {
+        final List resultList = query.getResultList();
+        return toRows(resultList);
+    }
+
+    private static void setStandardQueryParameters(Query query,
+        @Nullable LocalDate dateFrom, @Nullable LocalDate dateTo,
+        @Nullable Set<Long> candidateIds, @Nullable List<Long> sourceCountryIds) {
+
+        query.setParameter("dateFrom", defaultDateFrom(dateFrom));
+        query.setParameter("dateTo", defaultDateTo(dateTo));
+        if (sourceCountryIds != null && !sourceCountryIds.isEmpty()) {
+            query.setParameter("sourceCountryIds", sourceCountryIds);
+        }
+        if (candidateIds != null) {
+            query.setParameter("candidateIds", candidateIds);
+        }
+
+    }
+
+    private static String standardConstraints(
+        @Nullable Set<Long> candidateIds, @Nullable List<Long> sourceCountryIds,
+        @Nullable String constraintPredicate) {
+
+        String s = countingStandardFilter + dateConditionFilter;
+        if (sourceCountryIds != null && !sourceCountryIds.isEmpty()) {
+            s += sourceCountriesCondition;
+        }
+        if (constraintPredicate != null) {
+            s += " and " + constraintPredicate;
+        }
+        if (candidateIds != null) {
+            s += candidatesCondition;
+        }
+
+        //Stats that are not based on predefined candidate ids or constraintPredicate,
+        // should exclude ineligible.
+        //(With candidate ids, it is up to the associated list or search to decide whether to
+        // exclude ineligible)
+        if (candidateIds == null && constraintPredicate == null) {
+            s += excludeIneligible;
+        }
+
+        return s;
     }
 
     private static List<DataRow> toRows(List<Object[]> objects) {
