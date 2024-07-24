d6b0f8ea0821ea75761a8a2d5085630a67c8e06d
EhsanEhrari <ehrarie@gmail.com>
2024-07-24 14:08:40 +0430

    Creation of CandidateExamCardComponent

---
+++ ui/candidate-portal/src/app/components/common/candidate-exam-card/candidate-exam-card.component.ts
@@ -0,0 +1,35 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
+import {CandidateExam} from '../../../model/candidate-exam';
+import {Exam} from "../../../model/candidate";
+
+@Component({
+  selector: 'app-candidate-exam-card',
+  templateUrl: './candidate-exam-card.component.html',
+  styleUrls: ['./candidate-exam-card.component.scss']
+})
+export class CandidateExamCardComponent implements OnChanges {
+
+  @Input() candidateExam: CandidateExam;
+  @Input() candidateExams: CandidateExam[];
+  @Input() disabled: boolean = false;
+  @Output() onDelete = new EventEmitter<number>();
+
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges) {
+
+  }
+  delete() {
+    this.onDelete.emit();
+  }
+  getExamName(exam: string): string {
+    return Exam[exam as keyof typeof Exam] || 'Unknown';
+  }
+}
