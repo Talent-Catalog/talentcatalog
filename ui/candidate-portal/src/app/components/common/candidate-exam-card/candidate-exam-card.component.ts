15149c32f2512518bfc7983ca8f7ebb87aa737ab
EhsanEhrari <ehrarie@gmail.com>
2024-07-25 11:05:19 +0430

    update exam card years field

--- ui/candidate-portal/src/app/components/common/candidate-exam-card/candidate-exam-card.component.ts
+++ ui/candidate-portal/src/app/components/common/candidate-exam-card/candidate-exam-card.component.ts
@@ -4,28 +4,34 @@ import {
   Input,
   Output,
   OnChanges,
-  SimpleChanges
+  SimpleChanges, OnInit
 } from '@angular/core';
 import {CandidateExam} from '../../../model/candidate';
 import {Exam} from "../../../model/candidate";
+import {generateYearArray} from "../../../util/year-helper";
 
 @Component({
   selector: 'app-candidate-exam-card',
   templateUrl: './candidate-exam-card.component.html',
   styleUrls: ['./candidate-exam-card.component.scss']
 })
-export class CandidateExamCardComponent implements OnChanges {
+export class CandidateExamCardComponent implements OnChanges,OnInit {
 
   @Input() candidateExam: CandidateExam;
   @Input() candidateExams: CandidateExam[];
   @Input() disabled: boolean = false;
   @Output() onDelete = new EventEmitter<number>();
-
+  years:number[];
   constructor() {}
+
+  ngOnInit() {
+    this.years=generateYearArray(1950,true);
+  }
 
   ngOnChanges(changes: SimpleChanges) {
 
   }
+
   delete() {
     this.onDelete.emit();
   }
