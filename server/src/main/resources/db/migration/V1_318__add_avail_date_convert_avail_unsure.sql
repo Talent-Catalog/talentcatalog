21bb2b837e06b6af1556ec17dcfa5a2bc9d7ae98
John Cameron <john@cameronfoundation.org>
2024-07-31 18:56:47 +1000

    Append to notes of converted Unsure

--- server/src/main/resources/db/migration/V1_318__add_avail_date_convert_avail_unsure.sql
+++ server/src/main/resources/db/migration/V1_318__add_avail_date_convert_avail_unsure.sql
@@ -17,6 +17,7 @@
 alter table candidate add column avail_date date;
 
 update candidate set avail_immediate = 'No',
-                     avail_immediate_notes = 'Candidate is not sure about international opportunities at the moment'
+                     avail_immediate_notes =
+                         concat(avail_immediate_notes, '-Candidate is not sure about international opportunities at the moment')
 where avail_immediate = 'Unsure';
 
