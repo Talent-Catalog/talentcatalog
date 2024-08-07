356c4801e33a570b19fa3cc2610e904d9ed1987d
sadatmalik <sadat.malik@me.com>
2024-08-07 14:00:39 +0100

    apply cache evicting repo interface to employer repo

--- server/src/main/java/org/tctalent/server/repository/db/EmployerRepository.java
+++ server/src/main/java/org/tctalent/server/repository/db/EmployerRepository.java
@@ -16,64 +16,12 @@
 
 package org.tctalent.server.repository.db;
 
-import java.util.List;
 import java.util.Optional;
-import org.jetbrains.annotations.NotNull;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.tctalent.server.model.db.Employer;
 
-public interface EmployerRepository extends JpaRepository<Employer, Long>,
+public interface EmployerRepository extends CacheEvictingRepository<Employer, Long>,
     JpaSpecificationExecutor<Employer> {
-
-    @NotNull
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    <T extends Employer> T save(@NotNull T employer);
-
-    @NotNull
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    <T extends Employer> List<T> saveAll(@NotNull Iterable<T> employers);
-
-    @NotNull
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    <T extends Employer> T saveAndFlush(@NotNull T employer);
-
-    @NotNull
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    <T extends Employer> List<T> saveAllAndFlush(@NotNull Iterable<T> employers);
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void delete(@NotNull Employer employer);
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void deleteById(@NotNull Long id);
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void deleteAll();
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void deleteAll(@NotNull Iterable<? extends Employer> employers);
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void deleteAllInBatch();
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void deleteAllInBatch(@NotNull Iterable<Employer> employers);
-
-    @Override
-    @CacheEvict(value = "users", allEntries = true)
-    void deleteAllByIdInBatch(@NotNull Iterable<Long> ids);
 
     /**
      * Look up employer by sfId.
