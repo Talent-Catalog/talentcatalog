9eb6feba23720905cd7695633ab1e21d596c7562
sadatmalik <sadat.malik@me.com>
2024-08-12 22:16:25 +0100

    add job opp ID and case ID fields to log builder

--- server/src/main/java/org/tctalent/server/logging/LogBuilder.java
+++ server/src/main/java/org/tctalent/server/logging/LogBuilder.java
@@ -144,6 +144,35 @@ public class LogBuilder {
   }
 
   /**
+   * Adds the job opportunity ID to the log fields if the job opportunity ID is not null.
+   *
+   * @param jobOppId the job opportunity ID
+   * @return the current {@code LogBuilder} instance for chaining
+   */
+  public LogBuilder jobOppId(Long jobOppId) {
+    if (jobOppId == null) {
+      return this;
+    }
+    addField(LogField.JOB_OPP_ID, jobOppId.toString());
+    return this;
+  }
+
+  /**
+   * Adds the candidate opportunity ID (case ID) to the log fields if the candidate opportunity ID
+   * is not null.
+   *
+   * @param caseId the candidate opportunity ID
+   * @return the current {@code LogBuilder} instance for chaining
+   */
+  public LogBuilder caseId(Long caseId) {
+    if (caseId == null) {
+      return this;
+    }
+    addField(LogField.CASE_ID, caseId.toString());
+    return this;
+  }
+
+  /**
    * Adds the action to the log fields if the action is not null.
    *
    * @param action the action
