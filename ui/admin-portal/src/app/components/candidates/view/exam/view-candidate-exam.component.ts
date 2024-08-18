169ae0d9a9db97cbfdfaf0c26caa5e1eb5dd7906
EhsanEhrari <ehrarie@gmail.com>
2024-08-18 15:32:07 +0430

    Update view-candidate-exam.component.ts

--- ui/admin-portal/src/app/components/candidates/view/exam/view-candidate-exam.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/exam/view-candidate-exam.component.ts
@@ -1,11 +1,25 @@
+/*
+ * Copyright (c) 2024 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
 import {Component, Input, OnInit, SimpleChanges} from '@angular/core';
 import {Candidate, CandidateExam} from "../../../../model/candidate";
 import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {CandidateExamService} from "../../../../services/candidate-exam.service";
-import {
-  CreateCandidateCertificationComponent
-} from "../certification/create/create-candidate-certification.component";
 import {CreateCandidateExamComponent} from "./create/create-candidate-exam.component";
+import {ConfirmationComponent} from "../../../util/confirm/confirmation.component";
 
 @Component({
   selector: 'app-view-candidate-exam',
@@ -65,4 +79,30 @@ export class ViewCandidateExamComponent implements OnInit {
     .catch(() => { /* Isn't possible */ });
 
   }
+
+  deleteCandidateExam(candidateExam: CandidateExam) {
+    const deleteCandidateExamModal = this.modalService.open(ConfirmationComponent, {
+      centered: true,
+      backdrop: 'static'
+    });
+    deleteCandidateExamModal.componentInstance.message = 'Are you sure you want to delete this exam?';
+
+    deleteCandidateExamModal.result
+    .then((result) => {
+      if (result === true) {
+        this.candidateExamService.delete(candidateExam.id).subscribe(
+          (user) => {
+            this.loading = false;
+            this.doSearch();
+          },
+          (error) => {
+            this.error = error;
+            this.loading = false;
+          });
+        this.doSearch();
+      }
+    })
+    .catch(() => { /* Isn't possible */ });
+  }
+
 }
