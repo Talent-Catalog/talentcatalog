7657a8c3c989f9e40a08367731abdec617938edb
EhsanEhrari <ehrarie@gmail.com>
2024-08-19 15:19:41 +0430

    Update edit-candidate-exam.component.ts

--- ui/admin-portal/src/app/components/candidates/view/exam/edit/edit-candidate-exam.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/exam/edit/edit-candidate-exam.component.ts
@@ -1,17 +1,24 @@
+/*
+ * Copyright (c) 2024 Talent Beyond Boundaries.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
 import { Component, OnInit } from '@angular/core';
-import {CandidateCertification} from "../../../../../model/candidate-certification";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {
-  CandidateCertificationService,
-  UpdateCandidateCertificationRequest
-} from "../../../../../services/candidate-certification.service";
-import {CountryService} from "../../../../../services/country.service";
 import {CandidateExam, Exam} from "../../../../../model/candidate";
-import {
-  CandidateExamService,
-  CreateCandidateExamRequest, UpdateCandidateExamRequest
-} from "../../../../../services/candidate-exam.service";
+import {CandidateExamService, UpdateCandidateExamRequest} from "../../../../../services/candidate-exam.service";
 import {generateYearArray} from "../../../../../util/year-helper";
 import {EnumOption, enumOptions} from "../../../../../util/enum";
 
