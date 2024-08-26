8bdbdfbbfc468fefad9c507d7cddbd7e7268e966
camerojo <john@cameronfoundation.org>
2024-08-26 11:12:29 +1000

    Add family location to flyway

--- server/src/main/resources/db/migration/V1_320__move_and_create_destination_family_fields.sql
+++ server/src/main/resources/db/migration/V1_320__move_and_create_destination_family_fields.sql
@@ -14,8 +14,8 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-insert into candidate_visa_check (candidate_id, country_id, destination_family)
-select candidate_id, country_id, family from candidate_destination cd
+insert into candidate_visa_check (candidate_id, country_id, destination_family, destination_family_location)
+select candidate_id, country_id, family, location from candidate_destination cd
 where id not in (select cd.id from candidate_destination cd
                                        join candidate c on cd.candidate_id = c.id
                                        join candidate_visa_check cvc on c.id = cvc.candidate_id
