ea77205c7f621410f20c84f568826e6be08af24c
samschlicht <samschlicht@gmail.com>
2024-08-27 13:25:07 +0100

    - Improved error handling

--- server/src/main/java/org/tctalent/server/api/admin/SystemAdminApi.java
+++ server/src/main/java/org/tctalent/server/api/admin/SystemAdminApi.java
@@ -2811,14 +2811,34 @@ public class SystemAdminApi {
      */
     @Transactional
     @GetMapping("reassign-candidates/list-{listId}-to-partner-{partnerId}")
-    public void reassignCandidates(
+    public ResponseEntity<?> reassignCandidates(
         @PathVariable("listId") int listId,
         @PathVariable("partnerId") int partnerId
     ) {
-        // SavedListService can't be injected into CandidateService due to circular dependency, so
-        // we're obtaining the Saved List from the given ID here.
-        SavedList savedList = savedListService.get(listId);
-        candidateService.reassignSavedListCandidates(savedList, partnerId);
+        try {
+            // SavedListService can't be injected into CandidateService due to circular dependency, so
+            // we're obtaining the Saved List from the given ID here.
+            SavedList savedList = savedListService.get(listId);
+            candidateService.reassignSavedListCandidates(savedList, partnerId);
+
+            LogBuilder.builder(log)
+                .action("Reassign candidates")
+                .message("Reassignment of candidates from list with ID " + listId +
+                    " to partner with ID " + partnerId + " complete.")
+                .logInfo();
+
+            return ResponseEntity.ok().build(); // Return 200 OK - front-end will display 'Done'
+
+        } catch(Exception e) {
+            LogBuilder.builder(log)
+                .action("Reassign candidates")
+                .message("Reassignment of candidates from list with ID " + listId +
+                    " to partner with ID " + partnerId + " failed.")
+                .logError(e);
+
+            // Return 500 Internal Server Error including error in body for display on frontend
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+        }
     }
 
 }
