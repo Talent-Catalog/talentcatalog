e0b9f75137d6407fc47ead17a91fcedca407e2e4
john <jadcpub@gmail.com>
2024-10-14 11:06:04 +1100

    Add unit test

--- ui/admin-portal/src/app/components/list/find-list/find-list.component.spec.ts
+++ ui/admin-portal/src/app/components/list/find-list/find-list.component.spec.ts
@@ -1,25 +1,57 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {FindListComponent} from './find-list.component';
+import {NgbTypeaheadSelectItemEvent} from "@ng-bootstrap/ng-bootstrap";
+import {CandidateSourceService} from "../../../services/candidate-source.service";
+import {of} from "rxjs";
+import {MockCandidateSource} from "../../../MockData/MockCandidateSource";
 
 describe('FindListComponent', () => {
   let component: FindListComponent;
   let fixture: ComponentFixture<FindListComponent>;
+  let candidateSourceService: jasmine.SpyObj<CandidateSourceService>;
+  let mockCandidateSource: MockCandidateSource = new MockCandidateSource();
 
   beforeEach(async () => {
+    const candidateSourceServiceSpy = jasmine.createSpyObj('CandidateSourceService', ['searchPaged', 'get']);
+
     await TestBed.configureTestingModule({
-      declarations: [ FindListComponent ]
+      declarations: [ FindListComponent ],
+      providers: [
+        { provide: CandidateSourceService, useValue: candidateSourceServiceSpy }
+      ]
+
     })
     .compileComponents();
+
+    candidateSourceService = TestBed.inject(CandidateSourceService) as jasmine.SpyObj<CandidateSourceService>;
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FindListComponent);
     component = fixture.componentInstance;
+    candidateSourceService.get.and.returnValue(of(mockCandidateSource));
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should initialize correctly', () => {
+    component.id = 1;
+    component.ngOnChanges({});
+    expect(component.currentSelection).toBe(mockCandidateSource);
+  });
+
+  it('should emit job selection correctly', (done) => {
+    component.selectionMade.subscribe(selectedSource => {
+      expect(selectedSource).toEqual(mockCandidateSource);
+      done();
+    });
+
+    component.selectResult({ item: mockCandidateSource } as NgbTypeaheadSelectItemEvent);
   });
 });
