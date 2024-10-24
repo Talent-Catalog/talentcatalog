89840c253abb315611b7a9bea543d2dab6b3b1a7
camerojo <john@cameronfoundation.org>
2024-10-24 11:48:01 +1100

    Added documentation

--- ui/admin-portal/src/app/services/unsaved-changes.guard.ts
+++ ui/admin-portal/src/app/services/unsaved-changes.guard.ts
@@ -2,6 +2,11 @@ import {Injectable} from '@angular/core';
 import {ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 
+/**
+ * The Angular CanDeactivate Guard determines whether we can navigate away from a route. This custom guard
+ * allows us to implement a custom canExit condition, which must be passed to navigate away.
+ * See doc: https://www.tektutorialshub.com/angular/angular-candeactivate-guard/
+ */
 export interface BlockUnsavedChanges {
   canExit: () => Observable<boolean> | Promise<boolean> | boolean;
 }
