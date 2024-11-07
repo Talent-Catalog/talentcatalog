8a1323b5eac6be6977dbeeec36f461f6d75af3dc
john <jadcpub@gmail.com>
2024-11-07 15:39:29 +1100

    Replaced javax.transaction with jakarta

--- server/src/test/java/org/tctalent/server/service/db/util/TextExtractHelperTest.java
+++ server/src/test/java/org/tctalent/server/service/db/util/TextExtractHelperTest.java
@@ -26,7 +26,7 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
