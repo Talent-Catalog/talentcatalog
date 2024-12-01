65872449584328ec6be8cc1ff11ba2110d7f036a
john <jadcpub@gmail.com>
2024-12-01 16:27:44 +1100

    Ran ng add @angular/pwa in candidate-portal directory

--- ui/candidate-portal/src/app/app.module.ts
+++ ui/candidate-portal/src/app/app.module.ts
@@ -15,7 +15,7 @@
  */
 
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {NgModule, isDevMode} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './components/app.component';
 import {LandingComponent} from './components/landing/landing.component';
@@ -221,6 +221,7 @@ import {
 import {
   DestinationComponent
 } from "./components/register/destinations/destination/destination.component";
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 //This is not used now - but is left here to show how the standard translation loading works.
 //See https://github.com/ngx-translate/core#configuration
@@ -330,7 +331,13 @@ export function HttpLoaderFactory(http: HttpClient) {
     NgSelectModule,
     NgxWigModule,
     QuillModule.forRoot(),
-    PickerModule
+    PickerModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {provide: RedirectGuard},
