f283c3a6e7482fa63cd356183cc7167b39ac60e2
Muhanned Noman <Muhanned.Noman@gmail.com>
2024-12-05 10:02:33 +0300

    fix: rename file

--- server/src/main/resources/db/migration/V1_325__add_dulingo_coupon_task.sql
+++ server/src/main/resources/db/migration/V1_325__add_dulingo_coupon_task.sql
@@ -16,5 +16,6 @@
 
 insert into task (admin, created_by, created_date, days_to_complete, description, name, optional, task_type,
                   display_name)
-values (false, 25043, now(), 14, 'We are offering you a free Duolingo Test.', 'duolingoTest', false, 'Task',
+values (false, (select id from users where username = 'SystemAdmin'), now(), 14,
+        'We are offering you a free Duolingo Test.', 'duolingoTest', false, 'Task',
         'Take the Duolingo English Test for free!');
