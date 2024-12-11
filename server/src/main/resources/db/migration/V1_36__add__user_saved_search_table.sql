aa4993bab505d6370ccad9fb8fbea996c38a8635
John Cameron <john@cameronfoundation.org>
2024-12-11 16:12:06 +1100

    Remove copyright text from Flyways

--- server/src/main/resources/db/migration/V1_36__add__user_saved_search_table.sql
+++ server/src/main/resources/db/migration/V1_36__add__user_saved_search_table.sql
@@ -5,21 +5,6 @@ create table user_saved_search
    primary key (user_id, saved_search_id)
 );
 
-/*
- * Copyright (c) 2024 Talent Catalog.
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU Affero General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
- * for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see https://www.gnu.org/licenses/.
- */
 
 -- Note that the primary key can serve as the index for user_id.
 -- See https://stackoverflow.com/questions/3048154/indexes-and-multi-column-primary-keys
