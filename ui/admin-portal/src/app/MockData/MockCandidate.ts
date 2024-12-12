4963a205b5ab2fd206722cef96a08d0fb077c2fe
Caroline Cameron <caroline.iris.cameron@gmail.com>
2024-12-12 14:08:36 +1100

    Fix up broken test

--- ui/admin-portal/src/app/MockData/MockCandidate.ts
+++ ui/admin-portal/src/app/MockData/MockCandidate.ts
@@ -94,6 +94,10 @@ export class MockCandidate implements Candidate {
   unhcrStatus: UnhcrStatus = UnhcrStatus.RegisteredAsylum;
   ieltsScore: string = "7.5";
   numberDependants: number = 2;
+  relocatedAddress: string = "123 Sesame Street"
+  relocatedCity: string = "Melbourne"
+  relocatedState: string = "Victoria"
+  relocatedCountry: any = { id: 1, name: "Australia", status: "active", translatedName: "Australia" }
   candidateExams: any[] = [
     { id: 1, exam: Exam.IELTSGen, score: "7.5", year: 2020 }
   ];
@@ -329,10 +333,6 @@ export class MockCandidate implements Candidate {
       notes: "I do not like Toronto."
     },
   ];
-  relocatedAddress: "123 Sesame Street"
-  relocatedCity: "Melbourne"
-  relocatedState: "Victoria"
-  relocatedCountry: { id: 1, name: "Australia", status: "active", translatedName: "Australia" }
 
 
   constructor() {}
