bb2fb46ba16f5980b5a861204b54c01ddcb08c50
EhsanEhrari <ehrarie@gmail.com>
2025-01-19 16:12:45 +0430

    Update candidate-mini-intake-tab.component.ts

--- ui/admin-portal/src/app/components/candidates/view/tab/candidate-mini-intake-tab/candidate-mini-intake-tab.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/tab/candidate-mini-intake-tab/candidate-mini-intake-tab.component.ts
@@ -39,6 +39,7 @@ import {EditCandidateContactComponent} from "../../contact/edit/edit-candidate-c
 })
 export class CandidateMiniIntakeTabComponent extends IntakeComponentTabBase {
   clickedOldIntake: boolean;
+  examLabels: { [key: string]: string } = {};
 
   constructor(candidateService: CandidateService,
               countryService: CountryService,
@@ -73,7 +74,46 @@ export class CandidateMiniIntakeTabComponent extends IntakeComponentTabBase {
   isPalestinian(): boolean {
     return this.countryService.isPalestine(this.candidate?.nationality)
   }
+  getExamInfo(score: string) {
+    let className = 'text-mute';
+    let tooltip = 'Pending. Score is not provided or invalid.';
 
+    if (score === null || score === undefined || isNaN(parseFloat(score))) {
+      // Handle null, undefined, or non-numeric scores
+      return { className, tooltip };
+    }
+
+    const numericScore = parseFloat(score);
+
+    if (numericScore < 60) {
+      className = 'text-danger';
+      tooltip = 'Below requirement. Score is < 60.';
+    } else if (numericScore >= 60 && numericScore < 90) {
+      className = 'text-warning';
+      tooltip = 'Needs verification against the language requirement. Score is between 60 and 89.';
+    } else {
+      className = 'text-success';
+      tooltip = 'Meets language requirements. Score is 90 or higher.';
+    }
+
+    return { className, tooltip };
+  }
+
+  getExamLabel(exam: any): string {
+    const mostRecent = this.getMostRecentDetOfficialExam(this.candidateIntakeData?.candidateExams);
+    const highestScore = this.getHighestScoreDetOfficialExam(this.candidateIntakeData?.candidateExams);
+
+    if (exam === mostRecent && exam === highestScore) {
+      this.examLabels[exam.id] = 'Best & Newest Score';
+    } else if (exam === mostRecent) {
+      this.examLabels[exam.id] = 'Newest Score';
+    } else if (exam === highestScore) {
+      this.examLabels[exam.id] = 'Best Score';
+    } else {
+      this.examLabels[exam.id] = 'DET Official';
+    }
+    return this.examLabels[exam.id];
+  }
   addExamRecord(e: MouseEvent) {
     e.stopPropagation();
     this.saving = true;
