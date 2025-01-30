87de021f8636c85038b215120ffd6bd9a8256dc3
Muhanned Noman <Muhanned.Noman@gmail.com>
2025-01-31 00:16:12 +0300

    Fix: rollback last change

--- ui/admin-portal/src/app/components/search/define-search/define-search.component.spec.ts
+++ ui/admin-portal/src/app/components/search/define-search/define-search.component.spec.ts
@@ -37,7 +37,7 @@ import {
 import {SavedSearchService} from "../../../services/saved-search.service";
 import {AuthorizationService} from "../../../services/authorization.service";
 
-describe('DefineSearchComponent', () => {
+fdescribe('DefineSearchComponent', () => {
   let component: DefineSearchComponent;
   let fixture: ComponentFixture<DefineSearchComponent>;
   let searchQueryServiceSpy: jasmine.SpyObj<SearchQueryService>;
