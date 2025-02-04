1c0d6db0a2b3aa3d8a6067a9a095a34a193a6ac3
John Cameron <john@cameronfoundation.org>
2025-02-04 11:58:45 +1100

    Fixed Flyway typo

--- server/src/main/resources/db/migration/V1_337__add_candidate_muted_and_all_notifications.sql
+++ server/src/main/resources/db/migration/V1_337__add_candidate_muted_and_all_notifications.sql
@@ -15,5 +15,5 @@
  */
 
 alter table candidate add muted boolean default false not null;
-alter table candidate add allNotifications boolean default false not null;
+alter table candidate add all_notifications boolean default false not null;
 
