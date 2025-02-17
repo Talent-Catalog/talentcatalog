cf4d053c44258111b995cf6037fb95e0a81ee656
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2025-02-16 21:15:56 -0800

    Create DuolingoExtraFieldsRequest.java

---
+++ server/src/main/java/org/tctalent/server/request/duolingo/DuolingoExtraFieldsRequest.java
@@ -0,0 +1,37 @@
+package org.tctalent.server.request.duolingo;/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DuolingoExtraFieldsRequest {
+  private String certificateUrl;
+  private String interviewUrl;
+  private String verificationDate;
+  private int percentScore;
+  private int scale;
+  private int literacySubscore;
+  private int conversationSubscore;
+  private int comprehensionSubscore;
+  private int productionSubscore;
+  private Long candidateExamId;
+
+  public DuolingoExtraFieldsRequest(String certificateUrl, String interviewUrl, String verificationDate, int percentScore, int scale, int literacySubscore, int conversationSubscore,
+      int comprehensionSubscore, int productionSubscore, Long id) {
+  }
+}
