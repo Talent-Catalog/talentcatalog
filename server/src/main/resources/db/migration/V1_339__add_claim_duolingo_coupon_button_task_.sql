fb677dd36a69127789a6d389709d5b872ab62f8b
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2025-02-27 12:11:36 +0430

    Update V1_339__add_claim_duolingo_coupon_button_task_.sql

--- server/src/main/resources/db/migration/V1_339__add_claim_duolingo_coupon_button_task_.sql
+++ server/src/main/resources/db/migration/V1_339__add_claim_duolingo_coupon_button_task_.sql
@@ -1,4 +1,4 @@
 insert into task (admin, created_by, created_date, days_to_complete, description, name, optional, task_type, display_name)
 values (false, (select id from users where username = 'SystemAdmin'), now(), 14,
-        'Track if the user clicked on Claim Coupon button.', 'claimCouponButton', false, 'Task',
+        'Claim your free coupon by clicking the ''Claim Coupon'' button.', 'claimCouponButton', false, 'Task',
         'Claim the Duolingo Coupon');
