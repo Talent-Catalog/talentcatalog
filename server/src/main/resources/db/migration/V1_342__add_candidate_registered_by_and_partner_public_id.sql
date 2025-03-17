569fa58bbc888c245fdb70e02a1844e9db68b931
John Cameron <john@cameronfoundation.org>
2025-03-17 13:38:42 +1100

    add partner public_id and index

--- server/src/main/resources/db/migration/V1_342__add_candidate_registered_by_and_partner_public_id.sql
+++ server/src/main/resources/db/migration/V1_342__add_candidate_registered_by_and_partner_public_id.sql
@@ -1,5 +1,8 @@
 alter table candidate add registered_by bigint references partner;
-
 create index candidate_registered_by_idx on candidate(registered_by);
+
+alter table partner add public_id varchar(22);
+create index partner_public_id_idx on partner(public_id);
+
 
 
