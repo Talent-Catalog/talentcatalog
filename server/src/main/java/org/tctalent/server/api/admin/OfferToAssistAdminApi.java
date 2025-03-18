1b05ab0086d8f326ce3c58275b340b4ac62704a1
Caroline Cameron <caroline.iris.cameron@gmail.com>
2025-03-18 14:32:27 +1100

    Undo indent

--- server/src/main/java/org/tctalent/server/api/admin/OfferToAssistAdminApi.java
+++ server/src/main/java/org/tctalent/server/api/admin/OfferToAssistAdminApi.java
@@ -60,9 +60,9 @@ public class OfferToAssistAdminApi {
         //Create response
         OfferToAssistCandidates201Response response =
             new OfferToAssistCandidates201Response().toBuilder()
-                    .offerId(ota.getPublicId())
-                    .message("Your offer has been successfully recorded.")
-                    .build();
+                .offerId(ota.getPublicId())
+                .message("Your offer has been successfully recorded.")
+                .build();
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
