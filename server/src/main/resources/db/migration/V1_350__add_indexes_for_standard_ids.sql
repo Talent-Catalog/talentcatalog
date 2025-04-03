5dc92a26ae68b3f457dd0e514d8620e24e7c31d5
john <jadcpub@gmail.com>
2025-04-03 13:28:52 +1100

    Add standard code indexes

---
+++ server/src/main/resources/db/migration/V1_350__add_indexes_for_standard_ids.sql
@@ -0,0 +1,3 @@
+create index candidate_iso_code_idx on country(iso_code);
+create index language_iso_code_idx on language(iso_code);
+create index occupation_isco08_code_idx on occupation(isco08_code);
