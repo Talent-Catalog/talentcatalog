0748e5e82b446389c8ed807ab8f002a816683817
Caroline Cameron <caroline.iris.cameron@gmail.com>
2025-04-29 15:29:21 +1000

    Starting to write some tests

--- ui/admin-portal/src/app/directives/read-only-inputs.directive.spec.ts
+++ ui/admin-portal/src/app/directives/read-only-inputs.directive.spec.ts
@@ -15,10 +15,65 @@
  */
 
 import {ReadOnlyInputsDirective} from './read-only-inputs.directive';
+import {Component, DebugElement} from "@angular/core";
+import {ComponentFixture, TestBed, waitForAsync} from "@angular/core/testing";
+import {FormsModule, NgControl} from "@angular/forms";
+import {By} from "@angular/platform-browser";
+import {NgSelectModule} from "@ng-select/ng-select";
 
-describe('ReadOnlyInputsDirective', () => {
+@Component({
+  template: `
+    <div [appReadOnlyInputs]="readOnly">
+      <input type="text" />
+      <ng-select [items]="['a', 'b', 'c']"></ng-select>
+      <textarea></textarea>
+    </div>
+
+  `
+})
+class TestComponent {
+  readOnly = true;
+}
+
+fdescribe('ReadOnlyInputsDirective', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
+  let inputEl: DebugElement;
+  let ngSelectEl: DebugElement;
+  let textareaEl: DebugElement;
+  let directiveEl: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [FormsModule, NgSelectModule],
+      declarations: [TestComponent, ReadOnlyInputsDirective],
+      providers: [
+        {
+          provide: NgControl,
+        }
+      ]
+    })
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+    inputEl = fixture.debugElement.query(By.css('input'));
+    ngSelectEl = fixture.debugElement.query(By.css('ng-select'));
+    fixture.detectChanges();
+  }));
+
   it('should create an instance', () => {
-    const directive = new ReadOnlyInputsDirective();
+    const directive = new ReadOnlyInputsDirective(null, null);
     expect(directive).toBeTruthy();
   });
+
+  it('should disable a text input if read only', () => {
+    fixture.componentInstance.readOnly = true;
+    fixture.detectChanges();
+    expect(inputEl.nativeElement.hasAttribute('disabled')).toBeTrue();
+  });
+
+  // it('should not disable a text input if not read only', () => {
+  //   fixture.componentInstance.readOnly = false;
+  //   fixture.detectChanges();
+  //   expect(inputEl.nativeElement.hasAttribute('disabled')).toBeFalse();
+  // });
 });
