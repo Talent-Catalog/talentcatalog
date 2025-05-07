4bbdfcebc400a9e3b8ef079185f5a486c19ac8d1
samschlicht <samschlicht@gmail.com>
2025-05-07 13:41:27 +1000

    JobServiceImplTest created w update Job name test

---
+++ server/src/test/java/org/tctalent/server/service/db/impl/JobServiceImplTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License 
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.service.db.impl;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+
+import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.tctalent.server.model.db.Employer;
+import org.tctalent.server.model.db.SalesforceJobOpp;
+import org.tctalent.server.model.db.User;
+import org.tctalent.server.repository.db.SalesforceJobOppRepository;
+import org.tctalent.server.request.job.UpdateJobRequest;
+import org.tctalent.server.service.db.SalesforceService;
+import org.tctalent.server.service.db.SavedListService;
+import org.tctalent.server.service.db.SavedSearchService;
+import org.tctalent.server.service.db.UserService;
+
+@ExtendWith(MockitoExtension.class)
+class JobServiceImplTest {
+
+    SalesforceJobOpp job;
+    UpdateJobRequest updateJobRequest;
+
+    private static final String JOB_SF_ID = "LKJH66446GGFDFSA";
+    private static final String JOB_NAME = "test job";
+    private static final String JOB_NEXT_STEP = "do something";
+
+    @Mock User user;
+    @Mock UserService userService;
+    @Mock SalesforceJobOppRepository salesforceJobOppRepository;
+    @Mock Employer employerEntity;
+    @Mock SalesforceService sfService;
+    @Mock SavedSearchService savedSearchService;
+    @Mock SavedListService savedListService;
+
+    @InjectMocks
+    JobServiceImpl jobService;
+
+    @BeforeEach
+    void setUp() {
+        job = new SalesforceJobOpp();
+        job.setId(1L);
+        job.setName(JOB_NAME);
+        job.setSfId(JOB_SF_ID);
+        job.setNextStep(JOB_NEXT_STEP);
+        job.setEmployerEntity(employerEntity);
+        updateJobRequest = new UpdateJobRequest();
+    }
+
+    @Test
+    @DisplayName("updateJob sets new name on SF and TC when changed")
+    void updateJobSetsNewNameWhenChanged() {
+        final String newName = "new name";
+        updateJobRequest.setJobName(newName);
+
+        given(userService.getLoggedInUser()).willReturn(user);
+        given(salesforceJobOppRepository.findById(anyLong())).willReturn(Optional.of(job));
+
+        jobService.updateJob(99L, updateJobRequest);
+
+        then(sfService).should().updateEmployerOpportunityName(eq(JOB_SF_ID), eq(newName));
+        Assertions.assertEquals(job.getName(), newName);
+        then(savedSearchService).should().updateSuggestedSearchesNames(eq(job), eq(JOB_NAME));
+        then(savedListService).should().updateAssociatedListsNames(eq(job));
+    }
+}
