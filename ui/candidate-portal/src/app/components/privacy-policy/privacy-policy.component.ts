eba2f960e9d5d7f65ba83a5b9a55590b312985a2
john <jadcpub@gmail.com>
2025-05-16 14:17:34 +0100

    Add PrivacyPolicyComponent

---
+++ ui/candidate-portal/src/app/components/privacy-policy/privacy-policy.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-privacy-policy',
+  templateUrl: './privacy-policy.component.html',
+  styleUrls: ['./privacy-policy.component.scss']
+})
+export class PrivacyPolicyComponent {
+
+}
