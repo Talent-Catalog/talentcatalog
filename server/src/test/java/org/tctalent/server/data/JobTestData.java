ac13b42470a3b0004905edf1aa62c0bf88814ada
samschlicht <samschlicht@gmail.com>
2025-05-28 20:52:43 +1000

    Added data package and refactored existing tests in JobServiceImplTest to use it

---
+++ server/src/test/java/org/tctalent/server/data/JobTestData.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.data;
+
+import java.time.LocalDate;
+import org.tctalent.server.model.db.Employer;
+import org.tctalent.server.model.db.JobOpportunityStage;
+import org.tctalent.server.model.db.SalesforceJobOpp;
+
+public class JobTestData {
+
+    public static SalesforceJobOpp getJob() {
+        SalesforceJobOpp job = new SalesforceJobOpp();
+        job.setSfId("LKJH66446GGFDFSA");
+        job.setAccountId("JKDHUT0000JJJGGG");
+        job.setName("Test Job");
+        job.setNextStep("Do something");
+        job.setNextStepDueDate(LocalDate.of(1901, 1, 1));
+        job.setStage(JobOpportunityStage.candidateSearch);
+        job.setEmployerEntity(getEmployer());
+        return job;
+    }
+
+    static Employer getEmployer() {
+        Employer employer = new Employer();
+        employer.setName("Test Employer");
+        employer.setSfId("LKJH66446GGFDFSA");
+        return employer;
+    }
+
+}
--- server/src/test/java/org/tctalent/server/service/db/impl/JobServiceImplTest.java
+++ server/src/test/java/org/tctalent/server/service/db/impl/JobServiceImplTest.java
@@ -30,7 +30,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.tctalent.server.model.db.Employer;
+import org.tctalent.server.data.JobTestData;
 import org.tctalent.server.model.db.JobOpportunityStage;
 import org.tctalent.server.model.db.SalesforceJobOpp;
 import org.tctalent.server.model.db.User;
@@ -47,55 +47,42 @@ import org.tctalent.server.service.db.UserService;
 class JobServiceImplTest {
 
     private SalesforceJobOpp job;
-    private UpdateJobRequest updateJobRequest;
+    private UpdateJobRequest emptyUpdateJobRequest;
 
-    private static final String JOB_SF_ID = "LKJH66446GGFDFSA";
-    private static final String JOB_NAME = "test job";
-    private static final String JOB_NEXT_STEP = "do something";
-    private static final LocalDate JOB_NEXT_STEP_DUE_DATE =
-        LocalDate.of(1901, 1, 1);
-    private static final JobOpportunityStage JOB_STAGE = JobOpportunityStage.candidateSearch;
-
+    // TODO: import real user from test data once UserService tests merged
     @Mock private User user;
     @Mock private UserService userService;
     @Mock private SalesforceJobOppRepository salesforceJobOppRepository;
-    @Mock private Employer employerEntity;
     @Mock private SalesforceService sfService;
     @Mock private SavedSearchService savedSearchService;
     @Mock private SavedListService savedListService;
-    @Mock private OppNotificationService oppNotificationService;
     @Mock private NextStepProcessingService nextStepProcessingService;
+    @Mock private OppNotificationService oppNotificationService;
 
     @InjectMocks
     private JobServiceImpl jobService;
 
     @BeforeEach
     void setUp() {
-        job = new SalesforceJobOpp();
-        job.setId(1L);
-        job.setName(JOB_NAME);
-        job.setSfId(JOB_SF_ID);
-        job.setNextStep(JOB_NEXT_STEP);
-        job.setNextStepDueDate(JOB_NEXT_STEP_DUE_DATE);
-        job.setEmployerEntity(employerEntity);
-        job.setStage(JOB_STAGE);
-        updateJobRequest = new UpdateJobRequest();
+        job = JobTestData.getJob();
+        emptyUpdateJobRequest = new UpdateJobRequest();
     }
 
     @Test
     @DisplayName("updateJob sets new name on SF AND TC when provided in request")
     void updateJobSetsNewNameWhenChanged() {
         final String newName = "new name";
-        updateJobRequest.setJobName(newName);
+        final String oldName = job.getName();
+        emptyUpdateJobRequest.setJobName(newName);
 
         given(userService.getLoggedInUser()).willReturn(user);
         given(salesforceJobOppRepository.findById(anyLong())).willReturn(Optional.of(job));
 
-        jobService.updateJob(99L, updateJobRequest);
+        jobService.updateJob(99L, emptyUpdateJobRequest);
 
-        then(sfService).should().updateEmployerOpportunityName(JOB_SF_ID, newName);
+        then(sfService).should().updateEmployerOpportunityName(job.getSfId(), newName);
         Assertions.assertEquals(job.getName(), newName);
-        then(savedSearchService).should().updateSuggestedSearchesNames(job, JOB_NAME);
+        then(savedSearchService).should().updateSuggestedSearchesNames(job, oldName);
         then(savedListService).should().updateAssociatedListsNames(job);
     }
 
@@ -103,12 +90,12 @@ class JobServiceImplTest {
     @DisplayName("updateJob sets Stage on SF AND TC when provided in request")
     void updateJobSetsStageOnSfAndTcWhenProvided() {
         final JobOpportunityStage newStage = JobOpportunityStage.jobOffer;
-        updateJobRequest.setStage(newStage);
+        emptyUpdateJobRequest.setStage(newStage);
 
         given(userService.getLoggedInUser()).willReturn(user);
         given(salesforceJobOppRepository.findById(anyLong())).willReturn(Optional.of(job));
 
-        jobService.updateJob(99L, updateJobRequest);
+        jobService.updateJob(99L, emptyUpdateJobRequest);
 
         then(sfService).should()
             .updateEmployerOpportunityStage(job, newStage, null, null);
@@ -119,13 +106,13 @@ class JobServiceImplTest {
     @DisplayName("updateJob sets Next Step on SF AND TC when provided in request")
     void updateJobSetsNextStepOnSfAndTcWhenProvided() {
         final String newNextStep = "do something";
-        updateJobRequest.setNextStep(newNextStep);
+        emptyUpdateJobRequest.setNextStep(newNextStep);
 
         given(userService.getLoggedInUser()).willReturn(user);
         given(salesforceJobOppRepository.findById(anyLong())).willReturn(Optional.of(job));
         given(nextStepProcessingService.processNextStep(job, newNextStep)).willReturn(newNextStep);
 
-        jobService.updateJob(99L, updateJobRequest);
+        jobService.updateJob(99L, emptyUpdateJobRequest);
 
         then(sfService).should()
             .updateEmployerOpportunityStage(job, null, newNextStep, null);
@@ -136,12 +123,12 @@ class JobServiceImplTest {
     @DisplayName("updateJob sets Next Step Due Date on SF AND TC when provided in request")
     void updateJobSetsNextStepDueDateOnSfAndTcWhenProvided() {
         final LocalDate newNextStepDueDate = LocalDate.of(1970, 1,1);
-        updateJobRequest.setNextStepDueDate(newNextStepDueDate);
+        emptyUpdateJobRequest.setNextStepDueDate(newNextStepDueDate);
 
         given(userService.getLoggedInUser()).willReturn(user);
         given(salesforceJobOppRepository.findById(anyLong())).willReturn(Optional.of(job));
 
-        jobService.updateJob(99L, updateJobRequest);
+        jobService.updateJob(99L, emptyUpdateJobRequest);
 
         then(sfService).should()
             .updateEmployerOpportunityStage(job, null, null, newNextStepDueDate);
@@ -152,18 +139,22 @@ class JobServiceImplTest {
     @DisplayName("updateJob - when SF-pertinent request values all null, no SF update "
         + "and corresponding TC Job values unchanged")
     void updateJobNoUnnecessaryUpdate() {
-        updateJobRequest.setEvergreen(true); // Set something to evade potential process skip
+        emptyUpdateJobRequest.setEvergreen(true); // Minor update (not pertinent to SF)
+        final String currentNextStep = job.getNextStep();
+        final LocalDate currentDueDate = job.getNextStepDueDate();
+        final String currentName = job.getName();
+        final JobOpportunityStage currentStage = job.getStage();
 
         given(userService.getLoggedInUser()).willReturn(user);
         given(salesforceJobOppRepository.findById(anyLong())).willReturn(Optional.of(job));
 
-        jobService.updateJob(99L, updateJobRequest);
+        jobService.updateJob(99L, emptyUpdateJobRequest);
 
         then(sfService).shouldHaveNoInteractions();
-        Assertions.assertEquals(job.getNextStep(), JOB_NEXT_STEP);
-        Assertions.assertEquals(job.getNextStepDueDate(), JOB_NEXT_STEP_DUE_DATE);
-        Assertions.assertEquals(job.getName(), JOB_NAME);
-        Assertions.assertEquals(job.getStage(), JOB_STAGE);
+        Assertions.assertEquals(job.getNextStep(), currentNextStep);
+        Assertions.assertEquals(job.getNextStepDueDate(), currentDueDate);
+        Assertions.assertEquals(job.getName(), currentName);
+        Assertions.assertEquals(job.getStage(), currentStage);
     }
 
 }
