80a3acc2f1029fdedc82eb38ca2a2c11c62bec37
Caroline Cameron <caroline.iris.cameron@gmail.com>
2025-06-27 13:43:57 +1000

    Refacting the tables component

--- ui/admin-portal/src/app/shared/components/table/display-table.component.ts
+++ ui/admin-portal/src/app/shared/components/table/display-table.component.ts
@@ -1,6 +1,5 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {PageInfo} from "../pagination/tc-pagination.component";
-import {SearchResults} from "../../../model/search-results";
 
 export type TableType = 'Basic' | 'Striped' | 'Dropdown';
 
@@ -12,8 +11,6 @@ export type TableType = 'Basic' | 'Striped' | 'Dropdown';
 export class DisplayTableComponent {
 
   @Input() name: string;
-  @Input() columns: string[] = ["Name", "Stage", "Created", "Due"];
-  @Input() searchResults: SearchResults<any>;
   @Input() type: TableType = 'Basic';
   // Variables for pagination
   @Input() pageNumber;
