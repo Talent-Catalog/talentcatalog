4b7c1730848869047a6940dfb4406833b1ab2de9
samschlicht <samschlicht@gmail.com>
2025-06-30 12:30:26 +1000

    Pushing progress - more tests

--- server/src/test/java/org/tctalent/server/service/db/impl/CandidateAttachmentsServiceImplTest.java
+++ server/src/test/java/org/tctalent/server/service/db/impl/CandidateAttachmentsServiceImplTest.java
@@ -18,29 +18,45 @@ package org.tctalent.server.service.db.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+import static org.tctalent.server.data.CandidateTestData.getCandidate;
+import static org.tctalent.server.data.UserTestData.getAdminUser;
 
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
+import org.tctalent.server.exception.InvalidRequestException;
 import org.tctalent.server.exception.InvalidSessionException;
+import org.tctalent.server.exception.NoSuchObjectException;
+import org.tctalent.server.model.db.AttachmentType;
+import org.tctalent.server.model.db.Candidate;
 import org.tctalent.server.model.db.CandidateAttachment;
+import org.tctalent.server.model.db.User;
 import org.tctalent.server.model.db.task.UploadType;
 import org.tctalent.server.repository.db.CandidateAttachmentRepository;
+import org.tctalent.server.repository.db.CandidateRepository;
 import org.tctalent.server.request.PagedSearchRequest;
+import org.tctalent.server.request.attachment.CreateCandidateAttachmentRequest;
 import org.tctalent.server.request.attachment.ListByUploadTypeRequest;
 import org.tctalent.server.request.attachment.SearchCandidateAttachmentsRequest;
 import org.tctalent.server.security.AuthService;
+import org.tctalent.server.service.db.CandidateService;
 
 @ExtendWith(MockitoExtension.class)
 public class CandidateAttachmentsServiceImplTest {
@@ -48,11 +64,20 @@ public class CandidateAttachmentsServiceImplTest {
     private CandidateAttachment attachment;
     private Page<CandidateAttachment> attachmentPage;
     private List<CandidateAttachment> attachmentList;
+    private CreateCandidateAttachmentRequest createRequest;
 
-    private final static long candidateId = 1L;
+    private final static long candidateId = 99L;
+    private final static Candidate candidate = getCandidate();
+    private final static User adminUser = getAdminUser();
+    private final static String name = "name";
+    private final static String location = "www.link.com";
 
     private @Mock CandidateAttachmentRepository candidateAttachmentRepository;
     private @Mock AuthService authService;
+    private @Mock CandidateRepository candidateRepository;
+    private @Mock CandidateService candidateService;
+
+    private @Captor ArgumentCaptor<CandidateAttachment> attachmentCaptor;
 
     @InjectMocks
     CandidateAttachmentsServiceImpl candidateAttachmentsService;
@@ -62,6 +87,10 @@ public class CandidateAttachmentsServiceImplTest {
         attachment = new CandidateAttachment();
         attachmentPage = new PageImpl<>(List.of(attachment, attachment));
         attachmentList = List.of(attachment, attachment);
+        createRequest = new CreateCandidateAttachmentRequest();
+        createRequest.setCandidateId(candidateId);
+        createRequest.setLocation(location);
+        createRequest.setName(name);
     }
 
     @Test
@@ -130,6 +159,110 @@ public class CandidateAttachmentsServiceImplTest {
 
         assertEquals(attachmentList,
             candidateAttachmentsService.listCandidateAttachmentsForLoggedInCandidate());
+    }
+
+    @Test
+    @DisplayName("should return list of cvs when candidate found")
+    void listCandidateCvs_shouldReturnListOfCvs_whenCandidateFound() {
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.of(candidate));
+        given(candidateAttachmentRepository.findByCandidateIdAndCv(anyLong(), eq(true)))
+            .willReturn(attachmentList);
+
+        assertEquals(attachmentList, candidateAttachmentsService.listCandidateCvs(candidateId));
+    }
+
+    @Test
+    @DisplayName("should throw when candidate not found")
+    void listCandidateCvs_shouldThrow_whenCandidateNotFound() {
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.empty());
+
+        assertThrows(NoSuchObjectException.class,
+            () -> candidateAttachmentsService.listCandidateCvs(candidateId));
+    }
+
+    @Test
+    @DisplayName("should return list of attachments when candidate found")
+    void listCandidateAttachments_shouldReturnListOfAttachments_whenCandidateFound() {
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.of(candidate));
+        given(candidateAttachmentRepository.findByCandidateId(anyLong())).willReturn(attachmentList);
+
+        assertEquals(attachmentList, candidateAttachmentsService.listCandidateAttachments(candidateId));
+    }
+
+    @Test
+    @DisplayName("should throw when candidate not found")
+    void listCandidateAttachments_shouldThrow_whenCandidateNotFound() {
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.empty());
+
+        assertThrows(NoSuchObjectException.class,
+            () -> candidateAttachmentsService.listCandidateAttachments(candidateId));
+    }
+
+    @Test
+    @DisplayName("should throw when no logged in user")
+    void createCandidateAttachment_shouldThrow_whenNoLoggedInUser() {
+        given(authService.getLoggedInUser()).willReturn(Optional.empty());
+
+        assertThrows(InvalidSessionException.class,
+            () -> candidateAttachmentsService.createCandidateAttachment(createRequest));
+    }
+
+    @Test
+    @DisplayName("should throw when candidate not found")
+    void createCandidateAttachment_shouldThrow_whenCandidateNotFound() {
+        given(authService.getLoggedInUser()).willReturn(Optional.of(adminUser));
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.empty());
+
+        assertThrows(NoSuchObjectException.class,
+            () -> candidateAttachmentsService.createCandidateAttachment(createRequest));
+    }
+
+    @Test
+    @DisplayName("should throw when no candidate id in request")
+    void createCandidateAttachment_shouldThrow_whenNoCandidateIdInRequest() {
+        createRequest.setCandidateId(null);
+        given(authService.getLoggedInUser()).willReturn(Optional.of(adminUser));
+
+        assertThrows(InvalidRequestException.class,
+            () -> candidateAttachmentsService.createCandidateAttachment(createRequest));
+    }
+
+    @Test
+    @DisplayName("should create candidate attachment for link")
+    void createCandidateAttachment_shouldCreateCandidateAttachmentForLink() {
+        createRequest.setType(AttachmentType.link);
+
+        given(authService.getLoggedInUser()).willReturn(Optional.of(adminUser));
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.of(candidate));
+
+        candidateAttachmentsService.createCandidateAttachment(createRequest);
+
+        verify(candidateService).save(candidate, true);
+        verify(candidateAttachmentRepository).save(attachmentCaptor.capture());
+        CandidateAttachment attachment = attachmentCaptor.getValue();
+        assertEquals(name, attachment.getName());
+        assertEquals(location, attachment.getLocation());
+        assertEquals(AttachmentType.link, attachment.getType());
+    }
+
+    @Test
+    @DisplayName("should create candidate attachment for google file")
+    void createCandidateAttachment_shouldCreateCandidateAttachmentForGoogleFile() {
+        createRequest.setType(AttachmentType.googlefile);
+        createRequest.setCv(true);
+
+        given(authService.getLoggedInUser()).willReturn(Optional.of(adminUser));
+        given(candidateRepository.findById(candidateId)).willReturn(Optional.of(candidate));
+
+        candidateAttachmentsService.createCandidateAttachment(createRequest);
+
+        verify(candidateService).save(candidate, true);
+        verify(candidateAttachmentRepository).save(attachmentCaptor.capture());
+        CandidateAttachment attachment = attachmentCaptor.getValue();
+        assertEquals(name, attachment.getName());
+        assertEquals(location, attachment.getLocation());
+        assertEquals(AttachmentType.googlefile, attachment.getType());
+        assertTrue(attachment.isCv());
     }
 
 }
