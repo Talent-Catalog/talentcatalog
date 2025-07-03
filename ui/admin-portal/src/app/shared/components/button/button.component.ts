4b2c7de4be83bbdf63ea5bb71496afa7a651e249
hiba-machfej <hiba.mashfij@gmail.com>
2025-07-03 15:09:05 +0300

    update the component selector

--- ui/admin-portal/src/app/shared/components/button/button.component.ts
+++ ui/admin-portal/src/app/shared/components/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'tc-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
