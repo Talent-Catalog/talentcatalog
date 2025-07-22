58b91ec1c50445494f9f8067a6e2d527900f0d78
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2025-07-22 09:45:07 +0430

    Create edit-max-education-level.component.ts

---
+++ ui/admin-portal/src/app/components/candidates/view/education/edit-max-education-level/edit-max-education-level.component.ts
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+import {Component, OnInit} from '@angular/core';
+import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {EducationLevel} from "../../../../../model/education-level";
+import {EducationLevelService} from "../../../../../services/education-level.service";
+
+@Component({
+  selector: 'app-edit-max-education-level',
+  templateUrl: './edit-max-education-level.component.html',
+  styleUrls: ['./edit-max-education-level.component.scss']
+})
+export class EditMaxEducationLevelComponent implements OnInit {
+  educationLevels: EducationLevel[] = [];
+  selectedLevel: EducationLevel;
+  currentLevel: EducationLevel;
+
+  constructor(
+    private educationLevelService: EducationLevelService,
+    public activeModal: NgbActiveModal
+  ) {
+  }
+
+  ngOnInit() {
+    this.educationLevelService.listEducationLevels().subscribe(levels => {
+      this.educationLevels = levels;
+      this.selectedLevel = this.educationLevels.find(level => level.id === this.currentLevel?.id);
+    });
+  }
+
+  save() {
+    this.activeModal.close(this.selectedLevel);
+  }
+}
