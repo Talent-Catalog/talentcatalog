00aeed1d558a824400f741d2559b9e3f1316711c
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2025-07-22 09:11:46 +0430

    Update view-candidate-education.component.ts

--- ui/admin-portal/src/app/components/candidates/view/education/view-candidate-education.component.ts
+++ ui/admin-portal/src/app/components/candidates/view/education/view-candidate-education.component.ts
@@ -23,6 +23,7 @@ import {EditCandidateEducationComponent} from "./edit/edit-candidate-education.c
 import {CreateCandidateEducationComponent} from "./create/create-candidate-education.component";
 import {ConfirmationComponent} from "../../../util/confirm/confirmation.component";
 import {CandidateService} from "../../../../services/candidate.service";
+import {EditMaxEducationLevelComponent} from "./view-candidate-education-level/edit-candidate-max-education-level";
 
 @Component({
   selector: 'app-view-candidate-education',
@@ -46,6 +47,27 @@ export class ViewCandidateEducationComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  editMaxEducationLevel() {
+    const modalRef = this.modalService.open(EditMaxEducationLevelComponent, {
+      centered: true,
+      backdrop: 'static'
+    });
+
+    modalRef.componentInstance.currentLevel = this.candidate.maxEducationLevel;
+    modalRef.result.then((newLevel) => {
+        if (newLevel && newLevel.id) {
+          const candidatePayload = {
+            maxEducationLevel: newLevel.id
+          };
+
+          this.candidateService.updateMaxEducationLevel(this.candidate.id, candidatePayload).subscribe(() => {
+            this.candidateService.updateCandidate(); // refresh candidate
+          });
+        }
+    }).catch(() => { /* dismissed */ });
+  }
+
 
   editCandidateEducation(candidateEducation: CandidateEducation) {
     const editCandidateEducationModal = this.modalService.open(EditCandidateEducationComponent, {
