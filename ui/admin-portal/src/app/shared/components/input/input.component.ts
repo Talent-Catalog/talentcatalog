06d6d984ee978fe691f851e4f43af601b8f28039
hiba-machfej <hiba.mashfij@gmail.com>
2025-08-11 14:01:54 +0300

    update the input

--- ui/admin-portal/src/app/shared/components/input/input.component.ts
+++ ui/admin-portal/src/app/shared/components/input/input.component.ts
@@ -21,32 +21,57 @@ export class InputComponent implements ControlValueAccessor, OnInit {
   @Input() placeholder: string = '';
   @Input() disabled = false;
   @Input() invalid: boolean = false;
-  @Input() value: string = '';
   @Input() defaultValue: string = '';
 
   @Output() valueChange = new EventEmitter<string>();
 
-  // The reference for creating custom inputs using ControlValueAccessor https://blog.bitsrc.io/how-ive-created-custom-inputs-in-angular-16-43f4c2d37d07
-  onChange = (val: any) => {};
-  onTouched = () => {};
+  private _value: string = '';
+
+  get value(): string {
+    return this._value;
+  }
+
+  set value(val: string) {
+    if (val !== this._value) {
+      this._value = val;
+      this.onChange(val);
+      this.valueChange.emit(val);
+    }
+  }
+
+  private onChange = (val: any) => {};
+  private onTouched = () => {};
 
   ngOnInit() {
-    if (this.defaultValue && !this.value) {
+    // Only set default value if no value has been set by the form control
+    if (this.defaultValue && !this._value) {
       this.value = this.defaultValue;
     }
   }
 
+  // ControlValueAccessor implementation
   writeValue(val: any): void {
-    this.value = val ?? '';
+    this._value = val ?? '';
   }
-  registerOnChange(fn: any): void { this.onChange = fn; }
-  registerOnTouched(fn: any): void { this.onTouched = fn; }
-  setDisabledState?(isDisabled: boolean): void { this.disabled = isDisabled; }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
 
   handleInput(event: Event) {
     const newValue = (event.target as HTMLInputElement).value;
     this.value = newValue;
-    this.onChange(newValue);
-    this.valueChange.emit(newValue);
+  }
+
+  handleBlur() {
+    this.onTouched();
   }
 }
