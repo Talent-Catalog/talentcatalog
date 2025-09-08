cbb1a47d0d7c57f385f684eff56772fc48916253
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2025-09-08 11:10:37 +0430

    Update terms-info.service.spec.ts

--- ui/admin-portal/src/app/services/terms-info.service.spec.ts
+++ ui/admin-portal/src/app/services/terms-info.service.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { TermsInfoService } from './terms-info.service';
+import {TermsInfoService} from './terms-info.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('TermsInfoService', () => {
   let service: TermsInfoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TermsInfoService]
+    });
     service = TestBed.inject(TermsInfoService);
   });
 
