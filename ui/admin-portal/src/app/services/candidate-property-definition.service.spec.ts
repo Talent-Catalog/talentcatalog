8efafe478b6d4956b6c24661aab121ac5590144b
John Cameron <john@cameronfoundation.org>
2025-09-15 12:44:52 +1000

    Fix Angular test

--- ui/admin-portal/src/app/services/candidate-property-definition.service.spec.ts
+++ ui/admin-portal/src/app/services/candidate-property-definition.service.spec.ts
@@ -1,16 +1,48 @@
 import {TestBed} from '@angular/core/testing';
 
 import {CandidatePropertyDefinitionService} from './candidate-property-definition.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {SpringDataCandidatePropertyDefinitionsPage} from "../model/candidate-property-definition";
+import {environment} from "../../environments/environment";
 
 describe('CandidatePropertyDefinitionService', () => {
   let service: CandidatePropertyDefinitionService;
+  let httpMock: HttpTestingController;
+  const apiUrl = environment.halApiUrl + '/candidate-property-definitions';
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(CandidatePropertyDefinitionService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a page of property definitions', () => {
+    const mockDefinitions: SpringDataCandidatePropertyDefinitionsPage = {
+      _embedded: {
+        candidatePropertyDefinitions: []
+      },
+      page: {
+        size: 20,
+        totalElements: 1,
+        totalPages: 1,
+        number: 0
+      }
+    };
+
+    service.get().subscribe((definitions) => {
+      expect(definitions).toEqual(mockDefinitions);
+    })
+
+
+    const req = httpMock.expectOne(`${apiUrl}?page=0&size=20`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockDefinitions);
+
+  })
 });
