8d765455daca96db5c55e0c054134739a9108ac8
sadatmalik <sadat.malik@me.com>
2025-09-16 17:45:56 +0100

    task registry test

---
+++ server/src/test/java/org/tctalent/server/candidateservices/domain/policy/TaskPolicyRegistryTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.candidateservices.domain.policy;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskPolicyRegistryTest {
+
+  @Autowired
+  TaskPolicyRegistry registry;
+
+  @Test
+  void loadsDuolingo() {
+    assertNotNull(registry.forProvider("DUOLINGO"));
+  }
+}
