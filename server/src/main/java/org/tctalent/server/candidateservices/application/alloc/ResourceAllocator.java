3e5fb3c2fdf1ad7bc6db913ec4569213dc32f35e
sadatmalik <sadat.malik@me.com>
2025-09-16 17:40:08 +0100

    resource allocator and inventory allocator impl

---
+++ server/src/main/java/org/tctalent/server/candidateservices/application/alloc/ResourceAllocator.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.candidateservices.application.alloc;
+
+import java.util.List;
+import org.tctalent.server.candidateservices.domain.model.ServiceResource;
+import org.tctalent.server.model.db.Candidate;
+
+public interface ResourceAllocator {
+
+  ServiceResource allocateFor(Candidate candidate);
+
+  List<ServiceResource> getAvailableResources();
+}
