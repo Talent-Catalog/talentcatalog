2be601b31855d5b0b90b23e054a98a26ea34b981
sadatmalik <sadat.malik@me.com>
2025-09-16 17:42:59 +0100

    task policies

---
+++ server/src/main/java/org/tctalent/server/candidateservices/domain/policy/TaskPolicy.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2025 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.candidateservices.domain.policy;
+
+import java.util.List;
+import org.tctalent.server.candidateservices.domain.events.ServiceAssignedEvent;
+import org.tctalent.server.candidateservices.domain.events.ServiceExpiredEvent;
+import org.tctalent.server.candidateservices.domain.events.ServiceRedeemedEvent;
+
+public interface TaskPolicy {
+
+  String provider(); // todo - use PD
+
+  List<String> tasksOnAssigned(ServiceAssignedEvent e);
+
+  default void handleOnRedeemed(ServiceRedeemedEvent e) {}
+
+  default void handleOnExpired(ServiceExpiredEvent e) {}
+
+}
