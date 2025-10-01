b73cf63fe3a291107f27ae22ae6b92b479a9e5e8
samschlicht <samschlicht@gmail.com>
2025-10-01 19:48:11 +1000

    Fixed broken tests

--- ui/admin-portal/src/app/components/candidates/show/show-candidates.component.spec.ts
+++ ui/admin-portal/src/app/components/candidates/show/show-candidates.component.spec.ts
@@ -310,7 +310,7 @@ describe('ShowCandidatesComponent', () => {
   it('should toggle description visibility when link is clicked', () => {
     fixture.detectChanges();
 
-    const toggleLink = fixture.debugElement.query(By.css('.link'));
+    const toggleLink = fixture.debugElement.query(By.css('.toggle-description'));
     expect(component.showDescription).toBeFalse();
 
     toggleLink.triggerEventHandler('click', null);
@@ -319,7 +319,7 @@ describe('ShowCandidatesComponent', () => {
     expect(component.showDescription).toBeTrue();
 
     const icon = toggleLink.query(By.css('i'));
-    expect(icon.nativeElement.classList).toContain('fa-chevron-down');
+    expect(icon.nativeElement.classList).toContain('fa-chevron-up');
   });
 
 
