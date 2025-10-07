e1156722c6f922ae6924415e8ff98fb75740bc64
Caroline Cameron <caroline.iris.cameron@gmail.com>
2025-10-07 14:39:44 +1100

    Refactor the job prep items so that the publish button stays in the card

--- ui/admin-portal/src/app/components/job/view/preparation-items/view-job-preparation-items/view-job-preparation-items.component.ts
+++ ui/admin-portal/src/app/components/job/view/preparation-items/view-job-preparation-items/view-job-preparation-items.component.ts
@@ -26,6 +26,7 @@ export class ViewJobPreparationItemsComponent implements OnInit {
   @Input() jobPrepItems: JobPrepItem[];
   @Input() editable: boolean;
   @Output() itemSelected = new EventEmitter();
+  @Output() onPublish = new EventEmitter();
 
   selectedItem: JobPrepItem;
   error: any;
@@ -42,5 +43,9 @@ export class ViewJobPreparationItemsComponent implements OnInit {
   isSelected(item: JobPrepItem): boolean {
     let res = item === this.selectedItem;
     return res;
+  }
+
+  publish() {
+    this.onPublish.emit();
   }
 }
