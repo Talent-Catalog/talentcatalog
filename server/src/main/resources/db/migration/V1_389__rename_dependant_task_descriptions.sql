c4a9d0a5ae3cf9660b84cb9d5c4a822f85c91db2
John Cameron <john@cameronfoundation.org>
2025-11-21 15:37:54 +1100

    Update Dependant task descriptions to talk about dependants rather than family members

---
+++ server/src/main/resources/db/migration/V1_389__rename_dependant_task_descriptions.sql
@@ -0,0 +1,7 @@
+UPDATE task
+SET description = 'Please enter details of your eligible relocating dependants.'
+WHERE name = 'dependantsTravelDocFormTask';
+
+UPDATE task
+SET description = 'Submit refugee status evidence for each relocating dependant.'
+WHERE name = 'dependantsRefugeeStatusInfoDocFormTask';
