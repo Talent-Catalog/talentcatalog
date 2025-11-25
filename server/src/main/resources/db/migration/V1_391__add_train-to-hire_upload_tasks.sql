37223e5a970252b583999b6df3c807e24fe9a94d
samschlicht <samschlicht@gmail.com>
2025-11-25 20:20:03 +1100

    correct v391

--- server/src/main/resources/db/migration/V1_391__add_train-to-hire_upload_tasks.sql
+++ server/src/main/resources/db/migration/V1_391__add_train-to-hire_upload_tasks.sql
@@ -2,24 +2,24 @@
 -- Relocating candidate travel document
 insert into task (name, display_name, task_type, description, days_to_complete, created_by,
                   created_date)
-values ('italyTrainToHireTravelDocUpload', 'Travel Document Upload', 'Upload', 'placeholder',
+values ('italyTrainToHireTravelDocUpload', 'Travel Document Upload', 'UploadTask', 'placeholder',
         7, (select id from users where username = 'SystemAdmin'), now());
 
 -- Relocating candidate dependant travel doc upload
 insert into task (name, display_name, task_type, description, days_to_complete, created_by,
                   created_date)
-values ('italyTrainToHireDependantTravelDocUpload', 'Dependant Travel Document Upload', 'Upload',
+values ('italyTrainToHireDependantTravelDocUpload', 'Dependant Travel Document Upload', 'UploadTask',
         'placeholder', 7, (select id from users where username = 'SystemAdmin'), now());
 
 -- REFUGEE STATUS DOCUMENT UPLOAD TASKS
 -- Relocating candidate refugee status document
 insert into task (name, display_name, task_type, description, days_to_complete, created_by,
                   created_date)
-values ('italyTrainToHireRefugeeStatusDocUpload', 'Refugee Status Document Upload', 'Upload',
+values ('italyTrainToHireRefugeeStatusDocUpload', 'Refugee Status Document Upload', 'UploadTask',
         'placeholder', 7, (select id from users where username = 'SystemAdmin'), now());
 
 -- Relocating candidate dependant refugee status document
 insert into task (name, display_name, task_type, description, days_to_complete, created_by,
                   created_date)
 values ('italyTrainToHireDependantRefugeeStatusDocUpload', 'Dependant Refugee Status Document Upload',
-        'Upload', 'placeholder', 7, (select id from users where username = 'SystemAdmin'), now());
+        'UploadTask', 'placeholder', 7, (select id from users where username = 'SystemAdmin'), now());
