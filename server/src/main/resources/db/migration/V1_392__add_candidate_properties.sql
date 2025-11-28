8d19b3f9df7c48d0380e232b216ef9b45ee1963e
John Cameron <john@cameronfoundation.org>
2025-11-28 13:37:57 +1100

    Add new comments fields.
    Apply naming convention for properties

---
+++ server/src/main/resources/db/migration/V1_392__add_candidate_properties.sql
@@ -0,0 +1,60 @@
+insert into candidate_property_definition (name, label, definition, type)
+values
+    ('TTH_IT$TRAVEL_DOC_COMMENT', 'TTH IT: Travel document comment',
+        'Optional comment related to Italian Train to Hire travel document information.',NULL),
+    ('TTH_IT$REFUGEE_STATUS_COMMENT', 'TTH IT: Refugee status comment',
+        'Optional comment related to Italian Train to Hire Refugee status information.',NULL);
+
+update candidate_property_definition
+set name = 'TTH_IT$TRAVEL_DOC_TYPE',
+    label = 'TTH IT: Travel document type',
+    definition = 'Type of travel document, e.g., Passport, Refugee Travel Doc, National ID (Italian Train to Hire).'
+where name = 'TRAVEL_DOC_TYPE';
+
+update candidate_property_definition
+set name = 'TTH_IT$TRAVEL_DOC_NUMBER',
+    label = 'TTH IT: Travel document number',
+    definition = 'Unique number printed on the travel document (Italian Train to Hire).'
+where name = 'TRAVEL_DOC_NUMBER';
+
+update candidate_property_definition
+set name = 'TTH_IT$TRAVEL_DOC_ISSUED_BY',
+    label = 'TTH IT: Travel document issued by',
+    definition = 'Authority or country that issued the travel document (Italian Train to Hire).'
+where name = 'TRAVEL_DOC_ISSUED_BY';
+
+update candidate_property_definition
+set name = 'TTH_IT$TRAVEL_DOC_ISSUE_DATE',
+    label = 'TTH IT: Travel document issue date',
+    definition = 'Date when the travel document was issued (Italian Train to Hire).'
+where name = 'TRAVEL_DOC_ISSUE_DATE';
+
+update candidate_property_definition
+set name = 'TTH_IT$TRAVEL_DOC_EXPIRY_DATE',
+    label = 'TTH IT: Travel document expiry date',
+    definition = 'Date when the travel document expires (Italian Train to Hire).'
+where name = 'TRAVEL_DOC_EXPIRY_DATE';
+
+update candidate_property_definition
+set name = 'TTH_IT$REFUGEE_STATUS',
+    label = 'TTH IT: Refugee status',
+    definition = 'Refugee status according to UNHCR or host country determination (Italian Train to Hire).'
+where name = 'REFUGEE_STATUS';
+
+update candidate_property_definition
+set name = 'TTH_IT$REFUGEE_STATUS_EVIDENCE_DOCUMENT_TYPE',
+    label = 'TTH IT: Refugee status evidence document type',
+    definition = 'Type of Refugee Status evidence document (UNHCR Certificate, Host Country ID, etc.) (Italian Train to Hire).'
+where name = 'REFUGEE_STATUS_EVIDENCE_DOCUMENT_TYPE';
+
+update candidate_property_definition
+set name = 'TTH_IT$REFUGEE_STATUS_EVIDENCE_DOCUMENT_NUMBER',
+    label = 'TTH IT: Refugee status evidence document number',
+    definition = 'Number printed on the Refugee Status evidence document (Italian Train to Hire).'
+where name = 'REFUGEE_STATUS_EVIDENCE_DOCUMENT_NUMBER';
+
+update candidate_property_definition
+set name = 'TTH_IT$DEPENDANTS_INFO',
+    label = 'TTH IT: Dependants info',
+    definition = 'Information about dependants of a candidate (Italian Train to Hire).'
+where name = 'DEPENDANTS_INFO';
