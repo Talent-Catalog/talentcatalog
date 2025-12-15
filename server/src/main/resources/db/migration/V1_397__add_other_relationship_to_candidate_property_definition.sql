d3e2bd7a78b0a0c4539d9bad6229de75fb1be49d
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2025-12-15 14:32:24 +0430

    Create V1_397__add_other_relationship_to_candidate_property_definition.sql

---
+++ server/src/main/resources/db/migration/V1_397__add_other_relationship_to_candidate_property_definition.sql
@@ -0,0 +1,8 @@
+insert into candidate_property_definition (name,
+                                           label,
+                                           definition,
+                                           type)
+values ('TTH_IT$RELATIONSHIP_TO_PRIMARY_APPLICANT',
+        'TTH IT: Other relationship to primary applicant',
+        'Other relationship of a dependant to the primary applicant.',
+        null);
