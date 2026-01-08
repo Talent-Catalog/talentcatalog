b4ed708b3444994c310931e5877a677f931738b5
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2026-01-08 10:27:26 +0430

    Restructuring the package to support both Scala and Java

--- performance-tests/src/gatling/scala/org/talentcatalog/scenarios/NewSearchScreenScenario.scala
+++ performance-tests/src/gatling/scala/org/talentcatalog/scenarios/NewSearchScreenScenario.scala
@@ -5,12 +5,12 @@
  * the terms of the GNU Affero General Public License as published by the Free
  * Software Foundation, either version 3 of the License, or any later version.
  *
- * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
  * for more details.
  *
- * You should have received a copy of the GNU Affero General Public License 
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
@@ -18,20 +18,19 @@ package org.talentcatalog.scenarios
 
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
-import org.talentcatalog.queries.EsLoadQuery.{esLoadCountQuery, esLoadPagedQuery}
+import org.talentcatalog.queries.NewSearchScreenQuery.newSearchScreenQuery
 
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-object EsLoadScenario {
+object NewSearchScreenScenario {
 
-  val scenarioName: String = "EsLoadSystemApi"
+  val scenarioName: String = "NewSearchScreen"
 
-  val esLoadScenario: ScenarioBuilder =
+  val newSearchScreenScenario: ScenarioBuilder =
     scenario(scenarioName)
       .during(5 minutes) {
-        exec(esLoadPagedQuery())
-          .exec(esLoadCountQuery())
+        exec(newSearchScreenQuery())
       }
 
 }
