08ce8805fd6a9c613469e0cb52af6534febdf150
sadatmalik <sadat.malik@me.com>
2026-01-28 19:07:24 +0000

    update configuration in opc-prod main.tf

--- infra/aws/terraform/opc-prod/main.tf
+++ infra/aws/terraform/opc-prod/main.tf
@@ -59,78 +59,64 @@ module "tc-plus-prod" {
 
   # Spring application configuration
 
-  # todo -- SM: The below service configuration will be addressed in a follow-up PR / PRs
-
-  # todo create s3 access and required buckets
-  aws_access_key = "<REPLACE_ME>" # for s3 -- shouldn't be hardcoded todo
-  aws_secret_key = "<REPLACE_ME>" # for s3 -- shouldn't be hardcoded todo
-  s3_bucket = "<REPLACE_ME>" # for s3 -- switch to OPC todo
-
-  # todo missing duolingo api secret -- shouldn't be hardcoded
-
-  # todo retire elasticsearch
-  es_password = "<REPLACE_ME>" # shouldn't be hardcoded -- remove elasticsearch todo
-  es_url = "<REPLACE_ME>" # -- remove elasticsearch todo
-  es_username = "<REPLACE_ME>" # -- remove elasticsearch todo
-
-  email_default = "<REPLACE_ME>" # todo confirm if this is used / needed - it is not configured in the AWS staging task definition
-  email_password = "<REPLACE_ME>" # shouldn't be hardcoded todo
-  email_test_override = "john@cameronfoundation.org" # todo change this to a shared address
-
-  email_user = "<REPLACE_ME>" # todo confirm if this is used / needed - it is not configured in the AWS staging task definition
-
-  environment = "opc-prod"
-
-  # todo replicate in OPC workspace
-  drive_id = "<REPLACE_ME>"
-  drive_rootfolder =  "<REPLACE_ME>"
-  drive_list_folders_id = "<REPLACE_ME>"
-  drive_list_folders_root_id = "<REPLACE_ME>"
-  drive_private_key = "<REPLACE_ME>"
-  drive_private_key_id = "<REPLACE_ME>"
-
-  gradle_home = "/usr/local/gradle"
-  java_home = "/usr/lib/jvm/java"
-  jwt_secret = "<REPLACE_ME>" # todo do not hardcode
-  m2 = "/usr/local/apache-maven/bin"
-  m2_home = "/usr/local/apache-maven"
-
-  # todo preset_api_token, preset_secret (do not hardcode), preset_workspace_id
-  # todo redis_host, redis_port
-
-  server_port = "8080"
-  server_url = "https://plus.tctalent.org/"
-
-  # todo either create for OPC -- or decouple TC+ from SF ?
-  sf_base_classic_url = "<REPLACE_ME>"
-  sf_base_lightning_url = "<REPLACE_ME>"
-  sf_base_login_url = "<REPLACE_ME>"
-  sf_consumer_key = "<REPLACE_ME>"
-  sf_private_key = "<REPLACE_ME>"
-  sf_user = "<REPLACE_ME>"
-
-  slack_token = "<REPLACE_ME>" # todo need one for OPC but in any case shouldn't be hardcoded
-  spring_client_url = "-" # todo confirm if this is used or needed, there is no value configured for it in the stating service task definition
-  spring_datasource_password = "<REPLACE_ME>" # shouldn't be hardcoded -- todo
-  spring_datasource_url = "jdbc:postgresql://tbbtalent-prod.cy7icd7y1lyr.us-east-1.rds.amazonaws.com:5432/tctalent" # todo after RDS DB is created
-  spring_datasource_username = "tctalent"
-  spring_db_pool_max = "50"
-  spring_db_pool_min = "20"
-  spring_servlet_max_file_size = "10MB"
-  spring_servlet_max_request_size = "10MB"
-
-  # todo tc_api_key, tc_api_url
-
-  tc_cors_urls = "https://plus.tctalent.org,https://*.d2jx6ziu0w8kq9.amplifyapp.com,https://*.d1bt868vpd541m.amplifyapp.com" # todo amplify urls
-
-  #  todo tc_destinations
-
-  tc_db_copy_config = "data.sharing/tcCopies.xml" # todo -- can this be retired?
-
-  # todo -- tc_skills_extraction_api_url : https://skills.plus.tctalent.org
-  translation_password = "<REPLACE_ME>" # can't be hardcoded for repository -- find another way todo
-
-  web_admin = "https://test.plus.tctalent.org/admin-portal"
-  web_portal = "https://test.plus.tctalent.org/candidate-portal"
+  # AWS S3 configuration: Terraform creates SSM parameters with placeholder values.
+  # Update manually after terraform apply (see README.md for AWS CLI commands):
+  # - /tc-plus/opc-prod/AWS_CREDENTIALS_ACCESSKEY (String)
+  # - /tc-plus/opc-prod/AWS_CREDENTIALS_SECRETKEY (SecureString)
+  # - /tc-plus/opc-prod/AWS_S3_BUCKETNAME (String)
+  # - /tc-plus/opc-prod/DUOLINGO_API_APISECRET (SecureString)
+  # - /tc-plus/opc-prod/ELASTICSEARCH_PASSWORD (SecureString) - todo: retire elasticsearch
+  # - /tc-plus/opc-prod/ELASTICSEARCH_URL (String) - todo: retire elasticsearch
+  # - /tc-plus/opc-prod/ELASTICSEARCH_USERNAME (String) - todo: retire elasticsearch
+  # - /tc-plus/opc-prod/EMAIL_DEFAULTEMAIL (String) - todo: confirm if used/needed
+  # - /tc-plus/opc-prod/EMAIL_PASSWORD (SecureString)
+  # - /tc-plus/opc-prod/EMAIL_TESTOVERRIDEEMAIL (String) - todo: change to shared address
+  # - /tc-plus/opc-prod/EMAIL_USER (String) - todo: confirm if used/needed
+  # - /tc-plus/opc-prod/ENVIRONMENT (String)
+  # - /tc-plus/opc-prod/GOOGLE_DRIVE_CANDIDATEDATADRIVEID (SecureString) - todo: replicate in OPC workspace
+  # - /tc-plus/opc-prod/GOOGLE_DRIVE_CANDIDATEROOTFOLDERID (SecureString) - todo: replicate in OPC workspace
+  # - /tc-plus/opc-prod/GOOGLE_DRIVE_LISTFOLDERSDRIVEID (SecureString) - todo: replicate in OPC workspace
+  # - /tc-plus/opc-prod/GOOGLE_DRIVE_LISTFOLDERSROOTID (SecureString) - todo: replicate in OPC workspace
+  # - /tc-plus/opc-prod/GOOGLE_DRIVE_PRIVATEKEY (SecureString) - todo: replicate in OPC workspace
+  # - /tc-plus/opc-prod/GOOGLE_DRIVE_PRIVATEKEYID (SecureString) - todo: replicate in OPC workspace
+  # - /tc-plus/opc-prod/GRADLE_HOME (String)
+  # - /tc-plus/opc-prod/JAVA_HOME (String)
+  # - /tc-plus/opc-prod/JWT_SECRET (SecureString)
+  # - /tc-plus/opc-prod/LOGBUILDER_INCLUDE_CPU_UTILIZATION (String)
+  # - /tc-plus/opc-prod/LOGBUILDER_INCLUDE_MEMORY_UTILIZATION (String)
+  # - /tc-plus/opc-prod/M2 (String)
+  # - /tc-plus/opc-prod/M2_HOME (String)
+  # - /tc-plus/opc-prod/PRESET_API_TOKEN (SecureString)
+  # - /tc-plus/opc-prod/PRESET_SECRET (SecureString)
+  # - /tc-plus/opc-prod/PRESET_WORKSPACE_ID (SecureString)
+  # - /tc-plus/opc-prod/REDIS_HOST (String) - todo: the cache should be created by terraform
+  # - /tc-plus/opc-prod/REDIS_PORT (String) - todo: the cache should be created by terraform
+  # - /tc-plus/opc-prod/SERVER_PORT (String)
+  # - /tc-plus/opc-prod/SERVER_URL (String)
+  # - /tc-plus/opc-prod/SF_BASE_CLASSIC_URL (String) - todo: either create for OPC or decouple TC+ from SF
+  # - /tc-plus/opc-prod/SF_BASE_LIGHTNING_URL (String) - todo: either create for OPC or decouple TC+ from SF
+  # - /tc-plus/opc-prod/SF_BASE_LOGIN_URL (String) - todo: either create for OPC or decouple TC+ from SF
+  # - /tc-plus/opc-prod/SF_CONSUMER_KEY (SecureString) - todo: either create for OPC or decouple TC+ from SF
+  # - /tc-plus/opc-prod/SF_PRIVATE_KEY (SecureString) - todo: either create for OPC or decouple TC+ from SF
+  # - /tc-plus/opc-prod/SF_USER (SecureString) - todo: either create for OPC or decouple TC+ from SF
+  # - /tc-plus/opc-prod/SLACK_TOKEN (SecureString) - todo: need one for OPC
+  # - /tc-plus/opc-prod/SPRING_BOOT_ADMIN_CLIENT_URL (String) - todo: confirm if used/needed
+  # - /tc-plus/opc-prod/SPRING_DATASOURCE_PASSWORD (SecureString)
+  # - /tc-plus/opc-prod/SPRING_DATASOURCE_URL (String) - uses RDS endpoint when db_enable=true
+  # - /tc-plus/opc-prod/SPRING_DATASOURCE_USERNAME (String)
+  # - /tc-plus/opc-prod/SPRING_DBPOOL_MAX (String)
+  # - /tc-plus/opc-prod/SPRING_DBPOOL_MIN (String)
+  # - /tc-plus/opc-prod/SPRING_SERVLET_MULTIPART_MAX_FILE_SIZE (String)
+  # - /tc-plus/opc-prod/SPRING_SERVLET_MULTIPART_MAX_REQUEST_SIZE (String)
+  # - /tc-plus/opc-prod/TC_API_KEY (SecureString)
+  # - /tc-plus/opc-prod/TC_API_URL (String)
+  # - /tc-plus/opc-prod/TC_CORS_URLS (String) - todo: amplify urls
+  # - /tc-plus/opc-prod/TC_DESTINATIONS (String)
+  # - /tc-plus/opc-prod/TC_PARTNER_DBCOPY_CONFIG (String) - todo: can this be retired?
+  # - /tc-plus/opc-prod/TC_SKILLS_EXTRACTION_API_URL (String)
+  # - /tc-plus/opc-prod/TRANSLATION_PASSWORD (SecureString)
+  # - /tc-plus/opc-prod/WEB_ADMIN (String)
+  # - /tc-plus/opc-prod/WEB_PORTAL (String)
+  # Note: lifecycle.ignore_changes prevents Terraform from overwriting manual updates
 
 }
