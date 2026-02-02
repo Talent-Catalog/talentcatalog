c5c5efe581bd86e5303e7508f6d48499a36a7761
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2026-02-02 15:07:09 +0430

    Update GatlingRunner.java

--- performance-tests/src/gatling/java/org/talentcatalog/perf/GatlingRunner.java
+++ performance-tests/src/gatling/java/org/talentcatalog/perf/GatlingRunner.java
@@ -1,14 +1,31 @@
+/*
+ * Copyright (c) 2026 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
 package org.talentcatalog.perf;
 
 import io.gatling.app.Gatling;
 import io.gatling.core.config.GatlingPropertiesBuilder;
 import java.util.Optional;
+import scala.collection.mutable.Map;
 
 /**
  * Programmatic entry point to run Gatling simulations from an IDE (IntelliJ "Run"/green arrow)
  * or any JVM launcher (java command, Gradle/Maven exec, etc.).
  *
- * <p>This runner starts Gatling via {@link Gatling#fromMap(scala.collection.immutable.Map)} using
+ * <p>This runner starts Gatling via {@link Gatling#fromMap(Map)}} using
  * {@link GatlingPropertiesBuilder}, so you can run simulations without relying on Gradle/Maven
  * Gatling tasks.</p>
  *
