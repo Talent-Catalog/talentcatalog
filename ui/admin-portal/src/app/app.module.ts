bdb116b734c89d1f76d23f2656e794a1e3d755fc
sadatmalik <sadat.malik@me.com>
2026-03-13 14:45:11 +0000

    move coupon import components to CasiManagement and add routing for candidate services

--- ui/admin-portal/src/app/app.module.ts
+++ ui/admin-portal/src/app/app.module.ts
@@ -799,7 +799,7 @@ import {
 } from './components/candidates/potential-duplicates/duplicates-detail/duplicates-detail.component';
 import {
   ImportDuolingoCouponsComponent
-} from "./components/settings/import-duolingo-coupons/import-duolingo-coupons.component";
+} from "./components/casi-management/import-duolingo-coupons/import-duolingo-coupons.component";
 import {
   DetScoreValidationComponent
 } from './components/util/det-score-validation/det-score-validation.component';
@@ -818,7 +818,7 @@ import {ErrorDisplayComponent} from './components/util/error-display/error-displ
 import {IntelligenceComponent} from './components/intelligence/intelligence.component';
 import {
   OfferToAssistComponent
-} from './components/settings/offer-to-assist/offer-to-assist.component';
+} from './components/casi-management/offer-to-assist/offer-to-assist.component';
 import {
   IntlPhoneInputComponent
 } from './components/util/intl-phone-input/intl-phone-input.component';
@@ -840,10 +840,12 @@ import {
 } from './components/candidates/view/tab/candidate-cv-text-tab/candidate-cv-text-tab.component';
 import {
   ImportLinkedinPremiumCouponsComponent
-} from "./components/settings/import-linkedin-premium-coupons/import-linkedin-premium-coupons.component";
+} from "./components/casi-management/import-linkedin-premium-coupons/import-linkedin-premium-coupons.component";
 import {
   ImportReferenceVouchersComponent
-} from "./components/settings/import-reference-vouchers/import-reference-vouchers.component";
+} from "./components/casi-management/import-reference-vouchers/import-reference-vouchers.component";
+import {CasiManagementComponent} from "./components/casi-management/casi-management.component";
+import {CsvPreviewComponent} from "./components/casi-management/csv-preview/csv-preview.component";
 
 @NgModule({
   declarations: [
@@ -1183,6 +1185,8 @@ import {
     PartnerDpaComponent,
     SafeHtmlPipe,
     CandidateCvTextTabComponent,
+    CasiManagementComponent,
+    CsvPreviewComponent,
     ImportLinkedinPremiumCouponsComponent,
     ImportReferenceVouchersComponent
   ],
