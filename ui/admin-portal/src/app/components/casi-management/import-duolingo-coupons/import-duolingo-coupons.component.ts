ba4a63284c35312ae06a68dccb8e5a9949f35077
sadatmalik <sadat.malik@me.com>
2026-03-13 14:42:35 +0000

    use base csv importer in ImportDuolingoCouponsComponent

---
+++ ui/admin-portal/src/app/components/casi-management/import-duolingo-coupons/import-duolingo-coupons.component.ts
@@ -0,0 +1,65 @@
+import {Component, Input} from '@angular/core';
+import {DuolingoCouponService} from "../../../services/duolingo-coupon.service";
+import {User} from "../../../model/user";
+import {BaseCsvImportComponent} from "../base-csv-import.component";
+
+@Component({
+  selector: 'app-import-duolingo-coupons',
+  templateUrl: './import-duolingo-coupons.component.html',
+  styleUrls: ['./import-duolingo-coupons.component.scss'],
+})
+export class ImportDuolingoCouponsComponent extends BaseCsvImportComponent {
+  availableProctoredCouponsCount = 0;
+  requiredColumns = ['Coupon Code', 'Expiration Date', 'Date Sent', 'Coupon Status'];
+  @Input() loggedInUser!: User;
+
+  constructor(private duolingoCouponService: DuolingoCouponService) {
+    super();
+  }
+
+  protected doImport(): void {
+    if (!this.selectedFile) {
+      this.error = 'Please select a file to import.';
+      this.working = false;
+      return;
+    }
+
+    this.duolingoCouponService.importCoupons(this.selectedFile).subscribe({
+      next: () => {
+        this.working = false;
+        this.loadAvailableCount();
+        this.csvImported = true;
+        alert('CSV data imported successfully!');
+      },
+      error: (error) => {
+        this.working = false;
+        this.error = 'Failed to import the CSV file. Please try again.';
+        console.error('Import error:', error);
+      },
+    });
+  }
+
+  protected filterColumns(
+    headers: string[],
+    data: string[][]
+  ): { headers: string[]; data: string[][] } {
+    const filteredHeaders = headers.filter(
+      (_, index) => index !== 1 && index !== headers.length - 1
+    );
+    const filteredData = data.map((row) =>
+      row.filter((_, index) => index !== 1 && index !== row.length - 1)
+    );
+    return {headers: filteredHeaders, data: filteredData};
+  }
+
+  protected loadAvailableCount(): void {
+    this.duolingoCouponService.countAvailableProctoredCoupons().subscribe(
+      (response) => {
+        this.availableProctoredCouponsCount = response.count;
+      },
+      (error) => {
+        console.error('Error fetching available proctored coupons count', error);
+      }
+    );
+  }
+}
