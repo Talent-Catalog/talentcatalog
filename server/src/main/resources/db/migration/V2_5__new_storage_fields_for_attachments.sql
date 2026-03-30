231f6ad9201edc40f46557ec33ffb1b52188e986
camerojo <john@cameronfoundation.org>
2026-03-30 17:54:45 +1100

    Add index on publicId

--- server/src/main/resources/db/migration/V2_5__new_storage_fields_for_attachments.sql
+++ server/src/main/resources/db/migration/V2_5__new_storage_fields_for_attachments.sql
@@ -5,4 +5,7 @@ alter table candidate_attachment add column if not exists public_id text;
 alter table candidate_attachment add column if not exists sha256_hex text;
 alter table candidate_attachment add column if not exists storage_key text;
 
+create index if not exists candidate_attachment_public_id_idx on candidate_attachment(public_id);
+
+
 
