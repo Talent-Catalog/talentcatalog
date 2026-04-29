cb37d4da8edcd655fd87a26d8da9b07d43b01465
sadatmalik <sadat.malik@me.com>
2026-04-29 15:06:23 +0100

    Add integration tests for candidate data auditing across various entities

---
+++ server/src/test/java/org/tctalent/server/integration/audit/CandidateDataAuditingIntegrationTest.java
@@ -0,0 +1,412 @@
+/*
+ * Copyright (c) 2026 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.integration.audit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+import java.time.OffsetDateTime;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.transaction.annotation.Transactional;
+import org.tctalent.server.integration.helper.TestDataFactory;
+import org.tctalent.server.model.db.AbstractCandidateDataDomainObject;
+import org.tctalent.server.model.db.AttachmentType;
+import org.tctalent.server.model.db.Auditable;
+import org.tctalent.server.model.db.Candidate;
+import org.tctalent.server.model.db.CandidateAttachment;
+import org.tctalent.server.model.db.CandidateCertification;
+import org.tctalent.server.model.db.CandidateDependant;
+import org.tctalent.server.model.db.CandidateDestination;
+import org.tctalent.server.model.db.CandidateEducation;
+import org.tctalent.server.model.db.CandidateExam;
+import org.tctalent.server.model.db.CandidateJobExperience;
+import org.tctalent.server.model.db.CandidateLanguage;
+import org.tctalent.server.model.db.CandidateOccupation;
+import org.tctalent.server.model.db.Country;
+import org.tctalent.server.model.db.DependantRelations;
+import org.tctalent.server.model.db.EducationType;
+import org.tctalent.server.model.db.Exam;
+import org.tctalent.server.model.db.Language;
+import org.tctalent.server.model.db.LanguageLevel;
+import org.tctalent.server.model.db.Occupation;
+import org.tctalent.server.model.db.Role;
+import org.tctalent.server.model.db.Status;
+import org.tctalent.server.model.db.User;
+import org.tctalent.server.model.db.YesNoUnsure;
+import org.tctalent.server.repository.db.CandidateAttachmentRepository;
+import org.tctalent.server.repository.db.CandidateCertificationRepository;
+import org.tctalent.server.repository.db.CandidateDependantRepository;
+import org.tctalent.server.repository.db.CandidateDestinationRepository;
+import org.tctalent.server.repository.db.CandidateEducationRepository;
+import org.tctalent.server.repository.db.CandidateExamRepository;
+import org.tctalent.server.repository.db.CandidateJobExperienceRepository;
+import org.tctalent.server.repository.db.CandidateLanguageRepository;
+import org.tctalent.server.repository.db.CandidateOccupationRepository;
+import org.tctalent.server.repository.db.CandidateRepository;
+import org.tctalent.server.repository.db.CountryRepository;
+import org.tctalent.server.repository.db.LanguageLevelRepository;
+import org.tctalent.server.repository.db.LanguageRepository;
+import org.tctalent.server.repository.db.OccupationRepository;
+import org.tctalent.server.repository.db.UserRepository;
+import org.tctalent.server.security.TcUserDetails;
+import org.tctalent.server.service.db.UserService;
+
+@SpringBootTest
+@Transactional
+class CandidateDataAuditingIntegrationTest {
+
+    @Autowired private UserService userService;
+    @Autowired private UserRepository userRepository;
+    @Autowired private CandidateRepository candidateRepository;
+    @Autowired private CandidateAttachmentRepository candidateAttachmentRepository;
+    @Autowired private CandidateOccupationRepository candidateOccupationRepository;
+    @Autowired private CandidateCertificationRepository candidateCertificationRepository;
+    @Autowired private CandidateDestinationRepository candidateDestinationRepository;
+    @Autowired private CandidateEducationRepository candidateEducationRepository;
+    @Autowired private CandidateExamRepository candidateExamRepository;
+    @Autowired private CandidateJobExperienceRepository candidateJobExperienceRepository;
+    @Autowired private CandidateLanguageRepository candidateLanguageRepository;
+    @Autowired private CandidateDependantRepository candidateDependantRepository;
+    @Autowired private OccupationRepository occupationRepository;
+    @Autowired private CountryRepository countryRepository;
+    @Autowired private LanguageRepository languageRepository;
+    @Autowired private LanguageLevelRepository languageLevelRepository;
+
+    private User systemAdmin;
+
+    @BeforeEach
+    void setUp() {
+        systemAdmin = userService.getSystemAdminUser();
+    }
+
+    @AfterEach
+    void tearDown() {
+        SecurityContextHolder.clearContext();
+    }
+
+    @Test
+    void candidateOccupationAuditsAdminFlow() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("occ-admin", Role.user));
+        Occupation occupation = getAnyOccupation();
+
+        CandidateOccupation candidateOccupation = new CandidateOccupation();
+        candidateOccupation.setCandidate(candidate);
+        candidateOccupation.setOccupation(occupation);
+        candidateOccupation.setYearsExperience(4L);
+        candidateOccupation = candidateOccupationRepository.saveAndFlush(candidateOccupation);
+
+        assertAuditFields(candidateOccupation, systemAdmin);
+        assertAuditUpdate(candidateOccupation, occupationEntity -> occupationEntity.setYearsExperience(6L));
+    }
+
+    @Test
+    void candidateOccupationAuditsCandidateFlow() {
+        User candidateUser = createUser("occ-candidate", Role.user);
+        authenticateAs(candidateUser);
+        Candidate candidate = createCandidateFor(candidateUser);
+        Occupation occupation = getAnyOccupation();
+
+        CandidateOccupation candidateOccupation = new CandidateOccupation();
+        candidateOccupation.setCandidate(candidate);
+        candidateOccupation.setOccupation(occupation);
+        candidateOccupation.setYearsExperience(2L);
+        candidateOccupation = candidateOccupationRepository.saveAndFlush(candidateOccupation);
+
+        assertAuditFields(candidateOccupation, candidateUser);
+        assertAuditUpdate(candidateOccupation, occupationEntity -> occupationEntity.setYearsExperience(3L));
+    }
+
+    @Test
+    void candidateAttachmentAuditsAdminFlow() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("att-admin", Role.user));
+
+        CandidateAttachment attachment = new CandidateAttachment();
+        attachment.setCandidate(candidate);
+        attachment.setType(AttachmentType.link);
+        attachment.setName("portfolio");
+        attachment.setUrl("https://example.com/portfolio");
+        attachment = candidateAttachmentRepository.saveAndFlush(attachment);
+
+        assertAuditFields(attachment, systemAdmin);
+        assertAuditUpdate(attachment, attachmentEntity -> attachmentEntity.setName("portfolio-updated"));
+    }
+
+    @Test
+    void candidateAttachmentAuditsCandidateFlow() {
+        User candidateUser = createUser("att-candidate", Role.user);
+        authenticateAs(candidateUser);
+        Candidate candidate = createCandidateFor(candidateUser);
+
+        CandidateAttachment attachment = new CandidateAttachment();
+        attachment.setCandidate(candidate);
+        attachment.setType(AttachmentType.link);
+        attachment.setName("profile-link");
+        attachment.setUrl("https://example.com/profile");
+        attachment = candidateAttachmentRepository.saveAndFlush(attachment);
+
+        assertAuditFields(attachment, candidateUser);
+        assertAuditUpdate(attachment, attachmentEntity -> attachmentEntity.setName("profile-link-updated"));
+    }
+
+    @Test
+    void candidateCertificationAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("cert", Role.user));
+
+        CandidateCertification certification = new CandidateCertification();
+        certification.setCandidate(candidate);
+        certification.setName("Nursing Certificate");
+        certification.setInstitution("Institution");
+        certification.setDateCompleted(LocalDate.now().minusYears(1));
+        certification = candidateCertificationRepository.saveAndFlush(certification);
+
+        assertAuditFields(certification, systemAdmin);
+        assertAuditUpdate(certification, entity -> entity.setInstitution("Updated Institution"));
+    }
+
+    @Test
+    void candidateDestinationAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("dest", Role.user));
+
+        CandidateDestination destination = new CandidateDestination();
+        destination.setCandidate(candidate);
+        destination.setCountry(getAnyCountry());
+        destination.setInterest(YesNoUnsure.Yes);
+        destination.setNotes("Initial notes");
+        destination = candidateDestinationRepository.saveAndFlush(destination);
+
+        assertAuditFields(destination, systemAdmin);
+        assertAuditUpdate(destination, entity -> entity.setNotes("Updated notes"));
+    }
+
+    @Test
+    void candidateEducationAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("edu", Role.user));
+
+        CandidateEducation education = new CandidateEducation();
+        education.setCandidate(candidate);
+        education.setCountry(getAnyCountry());
+        education.setEducationType(EducationType.Bachelor);
+        education.setInstitution("University");
+        education.setCourseName("Computer Science");
+        education.setYearCompleted(2022);
+        education = candidateEducationRepository.saveAndFlush(education);
+
+        assertAuditFields(education, systemAdmin);
+        assertAuditUpdate(education, entity -> entity.setInstitution("Updated University"));
+    }
+
+    @Test
+    void candidateExamAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("exam", Role.user));
+
+        CandidateExam exam = new CandidateExam();
+        exam.setCandidate(candidate);
+        exam.setExam(Exam.IELTSAca);
+        exam.setScore("7.5");
+        exam.setYear(2023L);
+        exam = candidateExamRepository.saveAndFlush(exam);
+
+        assertAuditFields(exam, systemAdmin);
+        assertAuditUpdate(exam, entity -> entity.setScore("8.0"));
+    }
+
+    @Test
+    void candidateJobExperienceAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("job-exp", Role.user));
+        CandidateOccupation occupation = new CandidateOccupation();
+        occupation.setCandidate(candidate);
+        occupation.setOccupation(getAnyOccupation());
+        occupation.setYearsExperience(5L);
+        occupation = candidateOccupationRepository.saveAndFlush(occupation);
+
+        CandidateJobExperience experience = new CandidateJobExperience();
+        experience.setCandidate(candidate);
+        experience.setCandidateOccupation(occupation);
+        experience.setCountry(getAnyCountry());
+        experience.setCompanyName("Clinic");
+        experience.setRole("Nurse");
+        experience.setStartDate(LocalDate.now().minusYears(2));
+        experience.setEndDate(LocalDate.now().minusMonths(1));
+        experience = candidateJobExperienceRepository.saveAndFlush(experience);
+
+        assertAuditFields(experience, systemAdmin);
+        assertAuditUpdate(experience, entity -> entity.setCompanyName("Updated Clinic"));
+    }
+
+    @Test
+    void candidateLanguageAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("language", Role.user));
+        Language language = getAnyLanguage();
+        LanguageLevel level = getAnyLanguageLevel();
+
+        CandidateLanguage candidateLanguage = new CandidateLanguage();
+        candidateLanguage.setCandidate(candidate);
+        candidateLanguage.setLanguage(language);
+        candidateLanguage.setSpokenLevel(level);
+        candidateLanguage.setWrittenLevel(level);
+        candidateLanguage = candidateLanguageRepository.saveAndFlush(candidateLanguage);
+
+        assertAuditFields(candidateLanguage, systemAdmin);
+        assertAuditUpdate(candidateLanguage, entity -> entity.setWrittenLevel(getAnyLanguageLevel()));
+    }
+
+    @Test
+    void candidateDependantAudits() {
+        authenticateAs(systemAdmin);
+        Candidate candidate = createCandidateFor(createUser("dependant", Role.user));
+
+        CandidateDependant dependant = new CandidateDependant();
+        dependant.setCandidate(candidate);
+        dependant.setName("Jane");
+        dependant.setRelation(DependantRelations.Child);
+        dependant.setDob(LocalDate.now().minusYears(8));
+        dependant = candidateDependantRepository.saveAndFlush(dependant);
+
+        assertAuditFields(dependant, systemAdmin);
+        assertAuditUpdate(dependant, entity -> entity.setName("Jane Updated"));
+    }
+
+    private void assertAuditFields(Auditable auditable, User expectedUser) {
+        assertNotNull(auditable.getCreatedBy());
+        assertNotNull(auditable.getCreatedDate());
+        assertNotNull(auditable.getUpdatedBy());
+        assertNotNull(auditable.getUpdatedDate());
+        assertEquals(expectedUser.getId(), auditable.getCreatedBy().getId());
+        assertEquals(expectedUser.getId(), auditable.getUpdatedBy().getId());
+    }
+
+    private <T extends AbstractCandidateDataDomainObject<Long>> void assertAuditUpdate(
+        T entity,
+        java.util.function.Consumer<T> mutator
+    ) {
+        OffsetDateTime createdDate = entity.getCreatedDate();
+        Long createdById = entity.getCreatedBy().getId();
+        OffsetDateTime previousUpdatedDate = entity.getUpdatedDate();
+        mutator.accept(entity);
+        entity = saveByType(entity);
+        assertEquals(createdDate, entity.getCreatedDate());
+        assertEquals(createdById, entity.getCreatedBy().getId());
+        assertNotNull(entity.getUpdatedDate());
+        org.junit.jupiter.api.Assertions.assertFalse(entity.getUpdatedDate().isBefore(previousUpdatedDate));
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends AbstractCandidateDataDomainObject<Long>> T saveByType(T entity) {
+        if (entity instanceof CandidateAttachment attachment) {
+            return (T) candidateAttachmentRepository.saveAndFlush(attachment);
+        }
+        if (entity instanceof CandidateOccupation occupation) {
+            return (T) candidateOccupationRepository.saveAndFlush(occupation);
+        }
+        if (entity instanceof CandidateCertification certification) {
+            return (T) candidateCertificationRepository.saveAndFlush(certification);
+        }
+        if (entity instanceof CandidateDestination destination) {
+            return (T) candidateDestinationRepository.saveAndFlush(destination);
+        }
+        if (entity instanceof CandidateEducation education) {
+            return (T) candidateEducationRepository.saveAndFlush(education);
+        }
+        if (entity instanceof CandidateExam exam) {
+            return (T) candidateExamRepository.saveAndFlush(exam);
+        }
+        if (entity instanceof CandidateJobExperience experience) {
+            return (T) candidateJobExperienceRepository.saveAndFlush(experience);
+        }
+        if (entity instanceof CandidateLanguage language) {
+            return (T) candidateLanguageRepository.saveAndFlush(language);
+        }
+        if (entity instanceof CandidateDependant dependant) {
+            return (T) candidateDependantRepository.saveAndFlush(dependant);
+        }
+        throw new IllegalArgumentException("Unsupported entity type: " + entity.getClass().getName());
+    }
+
+    private Candidate createCandidateFor(User user) {
+        Candidate candidate = TestDataFactory.createCandidate();
+        candidate.setUser(user);
+        candidate.setCreatedBy(null);
+        candidate.setCreatedDate(null);
+        candidate.setUpdatedBy(null);
+        candidate.setUpdatedDate(null);
+        return candidateRepository.saveAndFlush(candidate);
+    }
+
+    private User createUser(String prefix, Role role) {
+        User user = new User();
+        String token = prefix + "-" + System.nanoTime();
+        user.setUsername(token);
+        user.setFirstName("Test");
+        user.setLastName("User");
+        user.setEmail(token + "@example.com");
+        user.setRole(role);
+        user.setStatus(Status.active);
+        user.setPasswordEnc("encoded");
+        user.setUsingMfa(false);
+        user.setPartner(systemAdmin.getPartner());
+        user.setCreatedBy(systemAdmin);
+        user.setCreatedDate(OffsetDateTime.now().minusDays(1));
+        return userRepository.saveAndFlush(user);
+    }
+
+    private void authenticateAs(User user) {
+        TcUserDetails userDetails = new TcUserDetails(user);
+        UsernamePasswordAuthenticationToken authentication =
+            new UsernamePasswordAuthenticationToken(userDetails, null, List.of());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private Occupation getAnyOccupation() {
+        return occupationRepository.findAll().stream()
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("No occupation records available for test"));
+    }
+
+    private Country getAnyCountry() {
+        return countryRepository.findAll().stream()
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("No country records available for test"));
+    }
+
+    private Language getAnyLanguage() {
+        return languageRepository.findAll().stream()
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("No language records available for test"));
+    }
+
+    private LanguageLevel getAnyLanguageLevel() {
+        return languageLevelRepository.findAll().stream()
+            .findFirst()
+            .orElseThrow(() -> new IllegalStateException("No language level records available for test"));
+    }
+}
