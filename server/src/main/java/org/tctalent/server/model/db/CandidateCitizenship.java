65f7ccfaeab94d8b1fd4bfd7768843dc0d4df497
sadatmalik <sadat.malik@me.com>
2026-05-01 11:21:44 +0100

    CandidateCitizenship extends AbstractCandidateDataDomainObject

--- server/src/main/java/org/tctalent/server/model/db/CandidateCitizenship.java
+++ server/src/main/java/org/tctalent/server/model/db/CandidateCitizenship.java
@@ -32,7 +32,7 @@ import java.time.LocalDate;
 @Table(name = "candidate_citizenship")
 @SequenceGenerator(name = "seq_gen", sequenceName = "candidate_citizenship_id_seq", allocationSize = 1)
 @NoArgsConstructor
-public class CandidateCitizenship extends AbstractDomainObject<Long> {
+public class CandidateCitizenship extends AbstractCandidateDataDomainObject<Long> {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "candidate_id")
