9153353aa964420e471df00afb491ecf5fef58cb
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2026-06-01 12:03:10 +0430

    Update CandidateService.java

--- server/src/main/java/org/tctalent/server/service/db/CandidateService.java
+++ server/src/main/java/org/tctalent/server/service/db/CandidateService.java
@@ -438,7 +438,16 @@ public interface CandidateService {
     List<DataRow> computeStatusStats(Gender gender, String country, LocalDate dateFrom, LocalDate dateTo, List<Long> sourceCountryIds);
     List<DataRow> computeStatusStats(Gender gender, String country, LocalDate dateFrom, LocalDate dateTo, Set<Long> candidateIds, List<Long> sourceCountryIds);
 
+    /**
+     * Generates a candidate CV in PDF format.
+     *
+     * @param candidate the candidate whose CV will be generated
+     * @param showName whether the candidate's name should be included in the generated CV
+     * @param showContact whether the candidate's contact details should be included in the generated CV
+     * @return a {@link Resource} containing the generated CV as a DOCX document
+     */
     Resource generateCv(Candidate candidate, Boolean showName, Boolean showContact);
+
     /**
      * Generates a candidate CV in Microsoft Word (.docx) format.
      *
@@ -448,6 +457,7 @@ public interface CandidateService {
      * @return a {@link Resource} containing the generated CV as a DOCX document
      */
     Resource generateCvDocx(Candidate candidate, Boolean showName, Boolean showContact);
+
     /**
      * Best to use this rather than {@link CandidateRepository#save}.
      * Saves the candidate to repository.
