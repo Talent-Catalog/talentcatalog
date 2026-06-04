fe8942526dc79d8ba766ccfc5852af5ab2eac1a0
sadatmalik <sadat.malik@me.com>
2026-06-04 14:31:46 +0100

    add Agreement and counterparty repositories

---
+++ server/src/main/java/org/tctalent/server/repository/db/AgreementRepository.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2026 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.repository.db;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.tctalent.server.model.db.Agreement;
+import org.tctalent.server.model.db.CounterpartyType;
+
+public interface AgreementRepository extends JpaRepository<Agreement, Long> {
+
+    List<Agreement> findByCandidateIdOrderByStartDesc(Long candidateId);
+
+    List<Agreement> findByCandidateIdAndCounterpartyTypeOrderByStartDesc(
+        Long candidateId, CounterpartyType counterpartyType);
+
+    Optional<Agreement> findFirstByCandidateIdAndCounterpartyTypeAndEndIsNullOrderByStartDesc(
+        Long candidateId, CounterpartyType counterpartyType);
+}
