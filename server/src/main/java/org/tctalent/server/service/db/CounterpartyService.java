77af78a5121e6795674ad2c0d506ec9d89bf8964
sadatmalik <sadat.malik@me.com>
2026-06-06 12:57:27 +0100

    feat: add AgreementService and CounterpartyService interfaces for managing agreements and counterparties

---
+++ server/src/main/java/org/tctalent/server/service/db/CounterpartyService.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2026 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+package org.tctalent.server.service.db;
+
+import org.springframework.lang.NonNull;
+import org.tctalent.server.model.db.Counterparty;
+import org.tctalent.server.model.db.CounterpartyType;
+
+/**
+ * Service responsible for creating/finding agreement counterparties.
+ *
+ * @author sadatmalik
+ */
+public interface CounterpartyService {
+
+    /**
+     * Finds an existing named counterparty (case-insensitive), or creates one if none exists.
+     *
+     * @param type Counterparty type
+     * @param name Counterparty display name
+     * @return Existing or newly created counterparty
+     */
+    @NonNull
+    Counterparty findOrCreateByTypeAndName(@NonNull CounterpartyType type, @NonNull String name);
+}
