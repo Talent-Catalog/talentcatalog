dd352493225745ee07895c0264f875ba5916568b
sadatmalik <sadat.malik@me.com>
2026-06-06 17:29:36 +0100

    add Agreement and related models

---
+++ ui/candidate-portal/src/app/model/agreement.ts
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2026 Talent Catalog.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://www.gnu.org/licenses/.
+ */
+
+export interface Agreement {
+  id: number;
+  start: string;
+  end: string | null;
+  termsInfoId: string;
+  counterparty: AgreementCounterparty;
+  termsInfo: AgreementTermsInfo;
+}
+
+export interface AgreementCounterparty {
+  id: number;
+  type: string;
+  name: string;
+}
+
+export interface AgreementTermsInfo {
+  id: string;
+  type: string;
+  pathToContent: string;
+  createdDate: string;
+  content: string;
+}
