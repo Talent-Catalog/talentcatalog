9e106741a66bd98482ef04ee7c0355e1b8881e8a
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2026-06-30 10:01:54 +0430

    Remove duplicated GPL header from test file
    Delete a redundant GPL license block comment from server/src/test/java/org/tctalent/server/repository/db/UserSpecificationTest.java.

--- server/src/test/java/org/tctalent/server/repository/db/UserSpecificationTest.java
+++ server/src/test/java/org/tctalent/server/repository/db/UserSpecificationTest.java
@@ -14,22 +14,6 @@
  * along with this program. If not, see https://www.gnu.org/licenses/.
  */
 
-/*
- * Copyright (c) 2026 Talent Catalog.
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License.
- * If not, see https://www.gnu.org/licenses/.
- */
-
 package org.tctalent.server.repository.db;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
