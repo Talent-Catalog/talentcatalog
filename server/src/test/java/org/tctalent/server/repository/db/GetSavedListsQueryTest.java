0370def085026de641decd981d9f87a20eb0522b
Mohammad Ehsan Ehrari <ehrarie@gmail.com>
2026-06-30 10:00:31 +0430

    Update GetSavedListsQueryTest.java

--- server/src/test/java/org/tctalent/server/repository/db/GetSavedListsQueryTest.java
+++ server/src/test/java/org/tctalent/server/repository/db/GetSavedListsQueryTest.java
@@ -16,22 +16,6 @@
 
 package org.tctalent.server.repository.db;
 
-/*
- * Copyright (c) 2026 Talent Catalog.
- *
- * This program is free software: you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free
- * Software Foundation, either version 3 of the License, or any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
- * for more details.
- *
- * You should have received a copy of the GNU General Public License.
- * If not, see https://www.gnu.org/licenses/.
- */
-
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
