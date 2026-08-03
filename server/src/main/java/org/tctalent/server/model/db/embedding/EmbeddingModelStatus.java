e71ab41d1c087b0f5d203abfb13a466dae7ec939
john <jadcpub@gmail.com>
2026-08-03 13:45:42 +1000

    Define new EmbeddingModelStatus values.

--- server/src/main/java/org/tctalent/server/model/db/embedding/EmbeddingModelStatus.java
+++ server/src/main/java/org/tctalent/server/model/db/embedding/EmbeddingModelStatus.java
@@ -19,20 +19,31 @@ package org.tctalent.server.model.db.embedding;
 /**
  * TC status of an EmbeddingModel.
  * <p>
- *     At any given time there will be one ACTIVE model. There could also be multiple
- *     RETIRED models.
+ *     At any given time there can be multiple READY models which can be used for matching.
+ *     All READY models are kept up to date as candidate data is updated.
+ * <p>
+ *     Before a model is READY it must be built. The BUILDING status indicates that the model is
+ *     being built in the background. The model is built on an alternate database table, and
+ *     when the build is complete, the model is marked READY and can be used for matching.
+ * <p>
+ *     If the build fails, the model is marked FAILED and will not be used for matching.
+ * <p>
+ *     To save resources (database space, memory, and CPU), models can be RETIRED.
+ *     Retiring a model involves dropping the database table and in memory indexes associated with
+ *     the model. A model can be in a RETIRING state before it is fully retired.
+ *     A REIRING model is no longer presented to users as a candidate for matching.
+ * <p>
  *     New models can be introduced. They are typically prepared in the background,
- *     associated with an alternate embedding table. The other statuses tracking the building
- *     and potential deployment of a new model.
- * </p>
- *
+ *     associated with a new embedding table. Those models have status BUILDING until the
+ *     build is complete. Once the build is complete, the model is marked READY and can be used
+ *     for matching.
+ * <p>
+ * See <a href="https://github.com/Talent-Catalog/talentcatalog/blob/a6ff32d5c049d90a0e09965f321500d5173902cf/server/src/main/java/org/tctalent/server/model/db/embedding/README.md">
+ *     this README</a> for more details on how models are built
+ * and managed.
  * @author John Cameron
  */
 public enum EmbeddingModelStatus {
-    /**
-     * This is the current model used for matching.
-     */
-    ACTIVE,
 
     /**
      * Model is currently building to an alternate database table.
@@ -50,14 +61,16 @@ public enum EmbeddingModelStatus {
     READY,
 
     /**
-     * The model used to be our active production model but has been replaced.
-     * Eventually tables associated with RETIRED embedding models will be dropped to save
+     * The model used to be available for matching, but it is no longer used.
+     * The tables associated with RETIRED embedding models can be dropped to save
      * space (including the in memory table indexes).
      */
     RETIRED,
 
     /**
-     * Previously active, still being kept up to date so rollback is immediate.
+     * The model is in the process of being retired. It is no longer presented to users as a
+     * candidate for matching.
+     * Once it is no longer being used, its status will be RETIRED.
      */
-    STANDBY
+    RETIRING
 }
