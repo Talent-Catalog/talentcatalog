f82704702d558393e18c3b9d7346512b053177ea
john <jadcpub@gmail.com>
2026-08-12 11:09:42 +1000

    Add lexical_weight and model_key to saved_search

--- server/src/main/resources/db/migration/V2_21__add_requirements_to_saved_search.sql
+++ server/src/main/resources/db/migration/V2_21__add_requirements_to_saved_search.sql
@@ -1,2 +1,4 @@
 ALTER TABLE saved_search
-    ADD COLUMN requirements TEXT;
+    ADD COLUMN requirements TEXT,
+    ADD COLUMN lexical_weight DOUBLE PRECISION DEFAULT 0.5 NOT NULL,
+    ADD COLUMN model_key TEXT;
